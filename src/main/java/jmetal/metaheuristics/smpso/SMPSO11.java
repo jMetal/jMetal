1011c78dc58261c33c7f6bd9029bb42c6518af08
ajnebro <ajnebro@outlook.com>
2014-04-02 00:31:50 +0200

    added SMPSO11_main

--- src/main/java/jmetal/metaheuristics/smpso/SMPSO11.java
+++ src/main/java/jmetal/metaheuristics/smpso/SMPSO11.java
@@ -107,10 +107,8 @@ public class SMPSO11 extends Algorithm {
   double C1Min_;
   double C2Max_;
   double C2Min_;
-  double WMax_;
-  double WMin_;
-  double ChVel1_;
-  double ChVel2_;
+  double W_;
+  double ChVel_;
 
   /**
    * Constructor
@@ -127,10 +125,8 @@ public class SMPSO11 extends Algorithm {
     C1Min_ = 1.5;
     C2Max_ = 2.5;
     C2Min_ = 1.5;
-    WMax_ = 0.1;
-    WMin_ = 0.1;
-    ChVel1_ = -1;
-    ChVel2_ = -1;
+    W_ = 1.0/(2.0 * Math.log(2)) ;
+    ChVel_ = -0.5;
   } // Constructor
 
   public SMPSO11(Problem problem,
@@ -146,10 +142,8 @@ public class SMPSO11 extends Algorithm {
     C1Min_ = variables.get(5);
     C2Max_ = variables.get(6);
     C2Min_ = variables.get(7);
-    WMax_ = variables.get(8);
-    WMin_ = variables.get(9);
-    ChVel1_ = variables.get(10);
-    ChVel2_ = variables.get(11);
+    W_ = variables.get(8);
+    ChVel_ = variables.get(10);
 
     hy_ = new Hypervolume();
     jmetal.qualityIndicator.util.MetricsUtil mu = new jmetal.qualityIndicator.util.MetricsUtil();
@@ -185,13 +179,8 @@ public class SMPSO11 extends Algorithm {
     r2Max_ = 1.0;
     r2Min_ = 0.0;
     C1Max_ = 2.5;
-    C1Min_ = 1.5;
-    C2Max_ = 2.5;
-    C2Min_ = 1.5;
-    WMax_ = 0.1;
-    WMin_ = 0.1;
-    ChVel1_ = -1;
-    ChVel2_ = -1;
+    W_ = 0.1;
+    ChVel_ = -0.5;
   } // Constructor
 
   /**
@@ -285,6 +274,7 @@ public class SMPSO11 extends Algorithm {
     for (int i = 0; i < swarmSize_; i++) {
     	XReal particle = new XReal(particles_.get(i)) ;
     	XReal bestParticle = new XReal(best_[i]) ;
+      XReal randomParticle = new XReal (new Solution(particles_.get(i))) ;
 
       //Select a global best_ for calculate the speed of particle i, bestGlobal
       Solution one, two;
@@ -303,15 +293,33 @@ public class SMPSO11 extends Algorithm {
       r2 = PseudoRandom.randDouble(r2Min_, r2Max_);
       C1 = PseudoRandom.randDouble(C1Min_, C1Max_);
       C2 = PseudoRandom.randDouble(C2Min_, C2Max_);
-      W = PseudoRandom.randDouble(WMin_, WMax_);
+      W = W_ ;
       //
-      wmax = WMax_;
-      wmin = WMin_;
+      XReal gravityCenter = new XReal(new Solution(particles_.get(i))) ;
 
       for (int var = 0; var < particle.getNumberOfDecisionVariables(); var++) {
         //Computing the velocity of this particle
+        double G;
+        G = particle.getValue(var) +
+                C1*r1 * (bestParticle.getValue(var) + C2*r2*bestGlobal.getValue(var) - 2 * particle.getValue(var)) / 3.0;
+        gravityCenter.setValue(var, G);
+      }
+
+      double radius = 0;
+      radius = new Distance().distanceBetweenSolutions(gravityCenter.getSolution(), particle.getSolution());
+
+      double [] random = PseudoRandom.randSphere(problem_.getNumberOfVariables()) ;
+
+      for (int var = 0; var < particle.size(); var++) {
+        randomParticle.setValue(var, gravityCenter.getValue(var) + radius*random[var]) ;
+      }
+
+      for (int var = 0; var < particle.getNumberOfDecisionVariables(); var++) {
         speed_[i][var] = velocityConstriction(constrictionCoefficient(C1, C2) *
-          (inertiaWeight(iter, miter, wmax, wmin) *
+                W_*speed_[i][var] + randomParticle.getValue(var) - particle.getValue(var), deltaMax_, deltaMin_, var, i);
+/*
+        speed_[i][var] = velocityConstriction(constrictionCoefficient(C1, C2) *
+          (W_ *
           speed_[i][var] +
           C1 * r1 * (bestParticle.getValue(var) -
           particle.getValue(var)) +
@@ -320,6 +328,7 @@ public class SMPSO11 extends Algorithm {
           deltaMin_, //[var],
           var,
           i);
+          */
       }
     }
   } // computeSpeed
@@ -336,11 +345,11 @@ public class SMPSO11 extends Algorithm {
 
         if (particle.getValue(var) < problem_.getLowerLimit(var)) {
           particle.setValue(var, problem_.getLowerLimit(var));
-          speed_[i][var] = speed_[i][var] * ChVel1_; //
+          speed_[i][var] = speed_[i][var] * ChVel_; //
         }
         if (particle.getValue(var) > problem_.getUpperLimit(var)) {
           particle.setValue(var, problem_.getUpperLimit(var));
-          speed_[i][var] = speed_[i][var] * ChVel2_; //
+          speed_[i][var] = speed_[i][var] * ChVel_; //
         }
       }
     }
