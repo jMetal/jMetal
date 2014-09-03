5e9a233c4daf92a8cef7b2b856817f5b69607219
ajnebro <ajnebro@outlook.com>
2014-09-03 16:13:01 +0200

    Renamig SimpleMutation to SimpleRandomMutation·

--- jmetal-core/src/main/java/org/uma/jmetal3/operator/SimpleRandomMutation.java
+++ jmetal-core/src/main/java/org/uma/jmetal3/operator/SimpleRandomMutation.java
@@ -3,14 +3,15 @@ package org.uma.jmetal3.operator;
 import org.uma.jmetal.core.Operator;
 import org.uma.jmetal.core.Problem;
 import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal3.core2.NumericalSolution;
 import org.uma.jmetal3.core2.Solution;
 
-public class SimpleMutation extends Operator {
+public class SimpleRandomMutation extends Operator {
   private double probability ;
   private Problem problem ;
   
   /**  Constructor */
-  public SimpleMutation(Problem problem) {
+  public SimpleRandomMutation(Problem problem) {
   	this.problem = problem ;
   	probability = 1.0/problem.getNumberOfVariables() ;
   }
@@ -19,11 +20,11 @@ public class SimpleMutation extends Operator {
   public Object execute(Object object) throws JMetalException {
     if (null == object) {
       throw new JMetalException("Null parameter") ;
-    } else if (!(object instanceof Solution)) {
+    } else if (!(object instanceof NumericalSolution)) {
       throw new JMetalException("Invalid parameter class") ;
     }
 
-    Solution solution = (Solution) object;	  
+    NumericalSolution solution = (NumericalSolution) object;	  
     
     return null;
   }
