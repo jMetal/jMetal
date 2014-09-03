2550240691e60b049c67e0169155f15c5e55fa77
ajnebro <ajnebro@outlook.com>
2014-09-03 15:55:30 +0200

    Adding a createSolution() method to SimpleRealProblem

--- jmetal-core/src/main/java/org/uma/jmetal3/core2/NumericalSolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal3/core2/NumericalSolution.java
@@ -3,7 +3,7 @@ package org.uma.jmetal3.core2;
 import org.uma.jmetal3.core.Solution;
 
 /**
- * Created by antonio on 03/09/14.
+ * Created by Antonio on 03/09/14.
  */
 public interface NumericalSolution<V extends Number> extends Solution {
   public V getLowerBound(int index) ;
--- jmetal-core/src/main/java/org/uma/jmetal3/problem/SimpleRealProblem.java
+++ jmetal-core/src/main/java/org/uma/jmetal3/problem/SimpleRealProblem.java
@@ -1,7 +1,7 @@
 package org.uma.jmetal3.problem;
 
 import org.uma.jmetal.core.Problem;
-import org.uma.jmetal.core.Solution;
+import org.uma.jmetal3.core.Solution;
 import org.uma.jmetal.util.JMetalException;
 import org.uma.jmetal3.core.impl.NumericalSolutionImpl;
 
@@ -17,8 +17,16 @@ public class SimpleRealProblem extends Problem{
     upperLimit = new double[]{1.0, 1.0, 1.0, 1.0, 1.0} ;
   }
 
-  @Override
-  public void evaluate(Solution solution) throws JMetalException {
-
+  public Solution createSolution() {
+  	Solution solution = new NumericalSolutionImpl<Double>(numberOfObjectives, numberOfVariables) ;
+  	
+  	return solution ;
+  }
+  
+	@Override
+  public void evaluate(org.uma.jmetal.core.Solution solution)
+      throws JMetalException {
+	  // TODO Auto-generated method stub
+	  
   }
 }
