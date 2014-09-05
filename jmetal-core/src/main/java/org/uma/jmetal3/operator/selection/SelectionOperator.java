540f9385546b60799fd53cd6d884d5d8c5c55959
ajnebro <ajnebro@outlook.com>
2014-09-05 09:02:31 +0200

    Added interface SelectionOperator

---
+++ jmetal-core/src/main/java/org/uma/jmetal3/operator/selection/SelectionOperator.java
@@ -0,0 +1,12 @@
+package org.uma.jmetal3.operator.selection;
+
+import org.uma.jmetal3.core.Operator;
+import org.uma.jmetal3.core.Solution;
+
+import java.util.Collection;
+
+/**
+ * Created by Antonio J. Nebro on 04/09/14.
+ */
+public interface SelectionOperator<Source extends Collection<Solution>,Result extends Solution> extends Operator<Source,Result> {
+}
