fd62d39d6ccba83e001f54a844f3e24d63526bb6
ajnebro <ajnebro@outlook.com>
2014-09-10 16:32:40 +0200

    Trying a first version of NSGAIISolution

--- jmetal-core/src/main/java/org/uma/jmetal3/metaheuristic/multiobjective/nsgaii/NSGAIISolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal3/metaheuristic/multiobjective/nsgaii/NSGAIISolution.java
@@ -1,11 +1,35 @@
 package org.uma.jmetal3.metaheuristic.multiobjective.nsgaii;
 
+import org.uma.jmetal3.core.Problem;
 import org.uma.jmetal3.core.Solution;
 import org.uma.jmetal3.encoding.attributes.CrowdingDistanceSolution;
 import org.uma.jmetal3.encoding.attributes.RankingSolution;
+import org.uma.jmetal3.encoding.impl.GenericSolutionImpl;
 
 /**
  * Created by antonio on 10/09/14.
  */
-public class NSGAIISolution<? extends Solution<?>> implements RankingSolution, CrowdingDistanceSolution {
+public class NSGAIISolution extends GenericSolutionImpl implements RankingSolution, CrowdingDistanceSolution {
+  private double crowdingDistance ;
+  private int ranking ;
+
+  @Override
+  public double getCrowdingDistance() {
+    return crowdingDistance ;
+  }
+
+  @Override
+  public int getRanking() {
+    return ranking;
+  }
+
+  @Override
+  public Solution<?> copy() {
+    
+    return null;
+  }
 }
+
+
+//public abstract class GenericSolutionImpl<T, P extends Problem> implements Solution<T>{
+// public class BinarySolutionImpl extends GenericSolutionImpl<BitSet, BinaryProblem> implements BinarySolution {
