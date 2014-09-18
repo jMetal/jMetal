c92541df9b3e196c23feb942191b4029f224960b
ajnebro <ajnebro@outlook.com>
2014-09-18 16:15:12 +0200

    Commenting AlgorithmAttributes.java #22

--- jmetal-core/src/main/java/org/uma/jmetal3/encoding/attributes/AlgorithmAttributes.java
+++ jmetal-core/src/main/java/org/uma/jmetal3/encoding/attributes/AlgorithmAttributes.java
@@ -3,7 +3,11 @@ package org.uma.jmetal3.encoding.attributes;
 import java.util.HashMap;
 
 /**
- * Created by antonio on 13/09/14.
+ * A {link Solution} has a set of components that are {@link Problem} related (variables, objective values, etc)
+ * and, optionally, a set of attributes that are {@link Algorithm} dependant (e.g., the ranking and crowding distance
+ * of NSGA-II.  The {@link AlgorithmAttibutes} interface models these kinds of attributes.
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
 public abstract class AlgorithmAttributes {
 @Override
