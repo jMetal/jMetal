978f6a4aa211b080f1091c81abb574051972bdca
ajnebro <ajnebro@outlook.com>
2014-09-18 09:21:13 +0200

    Added IntegerDoubleProblemImpl.java and problem NMMin2

--- jmetal-core/src/main/java/org/uma/jmetal3/problem/IntegerDoubleProblem.java
+++ jmetal-core/src/main/java/org/uma/jmetal3/problem/IntegerDoubleProblem.java
@@ -4,7 +4,7 @@ import org.uma.jmetal3.core.Problem;
 import org.uma.jmetal3.core.Solution;
 
 /** Interface representing problems having integer and double variables*/
-public interface IntegerDoubleProblem<S extends Solution<? extends Number>> extends Problem<S> {
+public interface IntegerDoubleProblem<S extends Solution<Number>> extends Problem<S> {
   public Number getLowerBound(int index) ;
   public Number getUpperBound(int index) ;
   public int getNumberOfIntegerVariables() ;
---
+++ jmetal-core/src/main/java/org/uma/jmetal3/problem/impl/IntegerDoubleProblemImpl.java
@@ -0,0 +1,55 @@
+package org.uma.jmetal3.problem.impl;
+
+import org.uma.jmetal3.core.Solution;
+import org.uma.jmetal3.problem.IntegerDoubleProblem;
+import org.uma.jmetal3.problem.IntegerProblem;
+
+import java.util.List;
+
+public abstract class IntegerDoubleProblemImpl<S extends Solution<Number>>
+  extends GenericProblemImpl<S>
+  implements IntegerDoubleProblem<S> {
+
+  private int numberOfIntegerVariables ;
+  private int numberOfDoubleVariables ;
+
+  private List<Number> lowerLimit ;
+  private List<Number> upperLimit ;
+
+  /* Getters */
+  public int getNumberOfDoubleVariables() {
+    return numberOfDoubleVariables;
+  }
+
+  public int getNumberOfIntegerVariables() {
+    return numberOfIntegerVariables;
+  }
+
+	@Override
+	public Number getUpperBound(int index) {
+		return upperLimit.get(index);
+	}
+
+	@Override
+	public Number getLowerBound(int index) {
+		return lowerLimit.get(index);
+	}
+
+  /* Getters */
+  /* Setters */
+  protected void setNumberOfDoubleVariables(int numberOfDoubleVariables) {
+    this.numberOfDoubleVariables = numberOfDoubleVariables;
+  }
+
+  protected void setNumberOfIntegerVariables(int numberOfIntegerVariables) {
+    this.numberOfIntegerVariables = numberOfIntegerVariables;
+  }
+
+  protected void setLowerLimit(List<Number> lowerLimit) {
+    this.lowerLimit = lowerLimit;
+  }
+
+  protected void setUpperLimit(List<Number> upperLimit) {
+    this.upperLimit = upperLimit;
+  }
+}
