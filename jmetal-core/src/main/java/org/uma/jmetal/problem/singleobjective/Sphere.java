9ddffaac8caa84f46bcd69ffd279065f263a8022
ajnebro <ajnebro@outlook.com>
2014-10-24 16:39:47 +0200

    Added Sphere problem

---
+++ jmetal-core/src/main/java/org/uma/jmetal/problem/singleobjective/Sphere.java
@@ -0,0 +1,83 @@
+//  Sphere.java
+//
+//  Author:
+//       Antonio J. Nebro <antonio@lcc.uma.es>
+//       Juan J. Durillo <durillo@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.uma.jmetal.problem.singleobjective;
+
+import org.uma.jmetal.core.Problem;
+import org.uma.jmetal.core.Solution;
+import org.uma.jmetal.encoding.DoubleSolution;
+import org.uma.jmetal.encoding.impl.DoubleSolutionImpl;
+import org.uma.jmetal.problem.ContinuousProblem;
+import org.uma.jmetal.problem.impl.ContinuousProblemImpl;
+import org.uma.jmetal.util.JMetalException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class representing a Sphere problem.
+ */
+public class Sphere extends ContinuousProblemImpl {
+  /** Constructor */
+  public Sphere(Integer numberOfVariables) throws JMetalException {
+    setNumberOfVariables(numberOfVariables);
+    setNumberOfObjectives(1);
+    setName("Sphere");
+
+    List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables()) ;
+    List<Double> upperLimit = new ArrayList<>(getNumberOfVariables()) ;
+
+    for (int i = 0; i < getNumberOfVariables(); i++) {
+      lowerLimit.add(-5.12);
+      upperLimit.add(5.12);
+    }
+
+    setLowerLimit(lowerLimit);
+    setUpperLimit(upperLimit);
+  }
+
+  @Override
+  public DoubleSolution createSolution() {
+    return new DoubleSolutionImpl(this) ;
+  }
+
+  /** Evaluate() method */
+  @Override
+  public void evaluate(DoubleSolution solution) {
+    int numberOfVariables = getNumberOfVariables() ;
+
+    double[] f = new double[getNumberOfObjectives()];
+    double[] x = new double[numberOfVariables] ;
+
+    for (int i = 0; i < numberOfVariables; i++) {
+      x[i] = solution.getVariableValue(i) ;
+    }
+
+    double sum = 0.0 ;
+    for (int var = 0; var < numberOfVariables; var++) {
+      double value = x[var];
+      sum += value * value;
+    }
+
+    solution.setObjective(0, sum);
+  }
+}
+
