e164c83f2351d24dc933130ac20f7ea2209121dc
ajnebro <ajnebro@outlook.com>
2014-10-30 18:46:58 +0100

    Created class NSGAIIV2

---
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/impl/singleobjective/geneticalgorithm/NSGAIIV2.java
@@ -0,0 +1,226 @@
+package org.uma.jmetal.algorithm.impl.singleobjective.geneticalgorithm;
+
+import org.apache.commons.math3.analysis.function.Abs;
+import org.uma.jmetal.algorithm.impl.AbstractGeneticAlgorithm;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.comparator.CrowdingDistanceComparator;
+import org.uma.jmetal.util.comparator.ObjectiveComparator;
+import org.uma.jmetal.util.solutionattribute.DensityEstimator;
+import org.uma.jmetal.util.solutionattribute.Ranking;
+import org.uma.jmetal.util.solutionattribute.impl.CrowdingDistance;
+import org.uma.jmetal.util.solutionattribute.impl.DominanceRanking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by ajnebro on 30/10/14.
+ */
+public class NSGAIIV2 extends AbstractGeneticAlgorithm<List<Solution>> {
+  private int maxIterations ;
+  private int populationSize ;
+
+  private Problem problem ;
+
+  protected Ranking ranking ;
+  protected DensityEstimator crowdingDistance;
+
+  /** Constructor */
+  private NSGAIIV2(Builder builder) {
+    problem = builder.problem ;
+    maxIterations = builder.maxIterations ;
+    populationSize = builder.populationSize ;
+
+    crossoverOperator = builder.crossoverOperator ;
+    mutationOperator = builder.mutationOperator ;
+    selectionOperator = builder.selectionOperator ;
+
+    ranking = new DominanceRanking() ;
+    crowdingDistance = new CrowdingDistance() ;
+  }
+
+  /** Builder class */
+  public static class Builder {
+    private Problem problem ;
+    private int maxIterations ;
+    private int populationSize ;
+    private CrossoverOperator crossoverOperator ;
+    private MutationOperator mutationOperator ;
+    private SelectionOperator selectionOperator ;
+
+    /** Builder constructor */
+    public Builder(Problem problem) {
+      this.problem = problem ;
+      maxIterations = 250 ;
+      populationSize = 100 ;
+    }
+
+    public Builder setMaxIterations(int maxIterations) {
+      this.maxIterations = maxIterations ;
+
+      return this ;
+    }
+
+    public Builder setPopulationSize(int populationSize) {
+      this.populationSize = populationSize ;
+
+      return this ;
+    }
+
+    public Builder setCrossoverOperator(CrossoverOperator crossoverOperator) {
+      this.crossoverOperator = crossoverOperator ;
+
+      return this ;
+    }
+
+    public Builder setMutationOperator(MutationOperator mutationOperator) {
+      this.mutationOperator = mutationOperator ;
+
+      return this ;
+    }
+
+    public Builder setSelectionOperator(SelectionOperator selectionOperator) {
+      this.selectionOperator = selectionOperator ;
+
+      return this ;
+    }
+
+    public NSGAIIV2 build() {
+      return new NSGAIIV2(this) ;
+    }
+  }
+
+  @Override
+  protected boolean isStoppingConditionReached() {
+    return getIterations() >= maxIterations;
+  }
+
+  @Override
+  protected List<Solution> createInitialPopulation() {
+    List<Solution> population = new ArrayList<>(populationSize) ;
+    for (int i = 0; i < populationSize; i++) {
+      Solution newIndividual = problem.createSolution();
+      population.add(newIndividual);
+    }
+    return population;
+  }
+
+  @Override
+  protected List<Solution> evaluatePopulation(List<Solution> population) {
+    for (Solution solution : population) {
+      problem.evaluate(solution);
+    }
+
+    return population ;
+  }
+
+  @Override
+  protected List<Solution> selection(List<Solution> population) {
+    List<Solution> matingPopulation = new ArrayList<>(population.size()) ;
+    for (int i = 0; i < populationSize; i++) {
+      Solution solution = (Solution) selectionOperator.execute(population);
+      matingPopulation.add(solution) ;
+    }
+
+    return matingPopulation;
+  }
+
+  @Override
+  protected List<Solution> reproduction(List<Solution> population) {
+    List<Solution> offspringPopulation = new ArrayList<>(populationSize);
+    for (int i = 0; i < (populationSize / 2); i++) {
+      if (!isStoppingConditionReached()) {
+        List<Solution> parents = new ArrayList<>(2);
+        parents.add((Solution) selectionOperator.execute(population));
+        parents.add((Solution) selectionOperator.execute(population));
+
+        List<Solution> offspring = (List<Solution>) crossoverOperator.execute(parents);
+
+        mutationOperator.execute(offspring.get(0));
+        mutationOperator.execute(offspring.get(1));
+
+        offspringPopulation.add(offspring.get(0));
+        offspringPopulation.add(offspring.get(1));
+      }
+    }
+    return offspringPopulation ;
+  }
+
+  @Override
+  protected List<Solution> replacement(List<Solution> population, List<Solution> offspringPopulation) {
+    List<Solution> jointPopulation = new ArrayList<>();
+    jointPopulation.addAll(population) ;
+    jointPopulation.addAll(offspringPopulation) ;
+
+    computeRanking(jointPopulation);
+    crowdingDistanceSelection();
+
+    return jointPopulation;
+  }
+
+  @Override
+  public List<Solution> getResult() {
+    return null;
+  }
+
+////////////// TODO: to be integrated smoothly
+
+  protected void addRankedSolutionsToPopulation(Ranking ranking, int rank) throws JMetalException {
+    List<Solution<?>> front ;
+
+    front = ranking.getSubfront(rank);
+
+    for (int i = 0 ; i < front.size(); i++) {
+      getPopulation().add(front.get(i));
+    }
+  }
+
+  protected void addLastRankedSolutions(Ranking ranking, int rank) throws JMetalException {
+    List<Solution> currentRankedFront = ranking.getSubfront(rank) ;
+
+    Collections.sort(currentRankedFront, new CrowdingDistanceComparator()) ;
+
+    int i = 0 ;
+    while (getPopulation().size() < populationSize) {
+      getPopulation().add(currentRankedFront.get(i)) ;
+      i++ ;
+    }
+  }
+
+  protected boolean populationIsNotFull() {
+    return getPopulation().size() < populationSize;
+  }
+
+  protected boolean subfrontFillsIntoThePopulation(Ranking ranking, int rank) {
+    return ranking.getSubfront(rank).size() < (populationSize - getPopulation().size()) ;
+  }
+
+  protected List<Solution> getNonDominatedSolutions(List<Solution> solutionSet) {
+    return ranking.computeRanking(solutionSet).getSubfront(0);
+  }
+
+  protected void computeRanking(List<Solution> solutionSet) {
+    ranking.computeRanking(solutionSet) ;
+  }
+
+  protected void crowdingDistanceSelection() {
+    getPopulation().clear();
+    int rankingIndex = 0;
+    while (populationIsNotFull()) {
+      if (subfrontFillsIntoThePopulation(ranking, rankingIndex)) {
+        addRankedSolutionsToPopulation(ranking, rankingIndex);
+        rankingIndex++;
+      } else {
+        crowdingDistance.computeDensityEstimator(ranking.getSubfront(rankingIndex));
+        addLastRankedSolutions(ranking, rankingIndex);
+      }
+    }
+  }
+}
