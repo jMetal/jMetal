d0839a4c66e525841a1484bf9d8337473f0b8de0
ajnebro <ajnebro@outlook.com>
2014-11-25 19:22:53 +0100

    Still working on ...

--- jmetal-algorithm/src/test/java/org/uma/jmetal/algorithm/singleobjective/differentialevolution/DifferentialEvolutionTest.java
+++ jmetal-algorithm/src/test/java/org/uma/jmetal/algorithm/singleobjective/differentialevolution/DifferentialEvolutionTest.java
@@ -1,11 +1,17 @@
 package org.uma.jmetal.algorithm.singleobjective.differentialevolution;
 
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
 import org.uma.jmetal.operator.impl.selection.DifferentialEvolutionSelection;
 import org.uma.jmetal.problem.DoubleProblem;
 import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.solution.impl.GenericDoubleSolution;
 import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
 import org.uma.jmetal.util.evaluator.impl.SequentialSolutionListEvaluator;
@@ -18,6 +24,11 @@ import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Antonio J. Nebro on 25/11/14.
@@ -31,6 +42,8 @@ public class DifferentialEvolutionTest {
 
   @Before
   public void startup() {
+    problem = mock(DoubleProblem.class) ;
+    /*
     problem = new DoubleProblem() {
       @Override
       public Double getLowerBound(int index) {
@@ -72,6 +85,7 @@ public class DifferentialEvolutionTest {
 
       }
     } ;
+    */
     algorithm = new DifferentialEvolution(problem, maxEvaluations, populationSize,
             new DifferentialEvolutionCrossover(),
             new DifferentialEvolutionSelection(),
@@ -139,25 +153,69 @@ public class DifferentialEvolutionTest {
 
   @Test
   public void theInitialPopulationHasInstantiatedSolutions() {
+    DoubleProblem problem = mock(DoubleProblem.class) ;
+    algorithm = new DifferentialEvolution(problem, maxEvaluations, populationSize,
+            new DifferentialEvolutionCrossover(),
+            new DifferentialEvolutionSelection(),
+            new SequentialSolutionListEvaluator()) ;
+
+    when(problem.createSolution()).thenReturn(mock(DoubleSolution.class)) ;
+
     List<DoubleSolution> population = algorithm.createInitialPopulation() ;
 
     assertNotNull(population.get(0)) ;
-    assertNotNull(population.get(populationSize-1)) ;
+    assertNotNull(population.get(populationSize - 1)) ;
+    assertNotEquals(population.get(0), population.get(populationSize-1));
   }
 
   @Test
   public void evaluationOfTheInitialPopulation() {
-    algorithm.createInitialPopulation() ;
-    List<DoubleSolution> population = algorithm.evaluatePopulation(algorithm.getPopulation()) ;
+    DoubleProblem problem = mock(DoubleProblem.class) ;
+    SolutionListEvaluator evaluator = mock(SolutionListEvaluator.class) ;
+    algorithm = new DifferentialEvolution(problem, maxEvaluations, populationSize,
+            new DifferentialEvolutionCrossover(),
+            new DifferentialEvolutionSelection(),
+            evaluator) ;
+
+    //when(problem.evaluate(any(DoubleSolution.class))).thenReturn(mock(DoubleSolution.class)) ;
+
+    /*
+    when(problem.createSolution()).thenReturn(new Answer<DoubleSolution>() {
+      DoubleSolution sol = new GenericDoubleSolution(problem) ; ;
+
+      public DoubleSolution answer(InvocationOnMock invocation) throws Throwable {
+        return sol;
+      }
+    }) ;
+    */
+
+   /*
+    Answer<DoubleSolution> answer = new Answer<DoubleSolution>() {
+      private DoubleSolution solution = new GenericDoubleSolution(problem) ;
+      @Override
+      public DoubleSolution answer(InvocationOnMock invocationOnMock) throws Throwable {
+        return mock(DoubleSolution.class);
+      }
+    } ;
+
+    when(problem.createSolution()).thenReturn(answer) ;
+    */
+
+    when(problem.createSolution()).thenReturn(mock(DoubleSolution.class)) ;
+    //when(evaluator.evaluate(Matchers.anyListOf(Solution.class),problem)).thenReturn(DoubleSolution.class);
+
+    List<DoubleSolution> population1 = algorithm.createInitialPopulation() ;
+    List<DoubleSolution> population2 = algorithm.evaluatePopulation(population1) ;
+    assertEquals(population1.size(), population2.size());
+
+    assertEquals(population1.get(0), population2.get(0));
+    assertNotEquals(population1.get(0), population2.get(1));
+    assertEquals(population1.get(populationSize-1), population2.get(populationSize-1));
   }
 
-  /*
-
-    @Override
-  protected List<DoubleSolution> evaluatePopulation(List<DoubleSolution> population) {
-    List<DoubleSolution> pop = evaluator.evaluate(population, problem) ;
-
-    return pop ;
+  @Test
+  public void selection() {
+    //List<DoubleSolution> population = algorithm.selection(algorithm.getPopulation()) ;
+    //assertEquals(population, algorithm.getPopulation()) ;
   }
-   */
 }
