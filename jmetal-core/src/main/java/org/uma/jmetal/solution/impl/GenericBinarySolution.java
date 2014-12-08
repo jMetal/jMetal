f82856c9f6026d07252dd3900646d39af65ae5ab
ajnebro <ajnebro@outlook.com>
2014-12-08 10:23:05 +0100

    Refactoring GenericBinarySolution

--- jmetal-core/src/main/java/org/uma/jmetal/solution/impl/GenericBinarySolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/impl/GenericBinarySolution.java
@@ -1,3 +1,24 @@
+//  GenericBinarySolution.java
+//
+//  Author:
+//       Antonio J. Nebro <antonio@lcc.uma.es>
+//       Juan J. Durillo <durillo@lcc.uma.es>
+//
+//  Copyright (c) 2014 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package org.uma.jmetal.solution.impl;
 
 import org.uma.jmetal.problem.BinaryProblem;
@@ -9,6 +30,7 @@ import java.util.BitSet;
 import java.util.HashMap;
 
 /**
+ * Defines an implementation of a binary solution
  * Created by Antonio J. Nebro on 03/09/14.
  */
 public class GenericBinarySolution extends AbstractGenericSolution<BitSet, BinaryProblem> implements BinarySolution {
@@ -33,9 +55,10 @@ public class GenericBinarySolution extends AbstractGenericSolution<BitSet, Binar
   public GenericBinarySolution(GenericBinarySolution solution) {
     problem = solution.problem ;
     objectives = new ArrayList<>() ;
-    for (Double obj : solution.objectives) {
-      objectives.add(new Double(obj)) ;
+    for (int i = 0; i < problem.getNumberOfObjectives(); i++) {
+      objectives.add(solution.getObjective(i)) ;
     }
+
     variables = new ArrayList<>() ;
     for (BitSet var : solution.variables) {
       variables.add((BitSet) var.clone()) ;
