612ccc0109fde16254168246be5bd1088d996e6c
ajnebro <ajnebro@outlook.com>
2014-12-11 22:50:35 +0100

    Adding license comments

--- jmetal-core/src/test/java/org/uma/jmetal/operator/impl/mutation/PolynomialMutationTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/operator/impl/mutation/PolynomialMutationTest.java
@@ -1,10 +1,3 @@
-//  PolynomialMutationTest.java
-//
-//  Author:
-//       Antonio J. Nebro <antonio@lcc.uma.es>
-//
-//  Copyright (c) 2014 Antonio J. Nebro
-//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
