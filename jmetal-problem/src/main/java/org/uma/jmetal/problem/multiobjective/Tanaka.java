c410ff216e33cbb10f6e3871695aa4b631405c15
ajnebro <ajnebro@outlook.com>
2014-12-19 18:01:06 +0100

    Added problem Tanaka

---
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/Tanaka.java
@@ -0,0 +1,94 @@
+//  Tanaka.java
+//
+//  Author:
+//       Antonio J. Nebro <antonio@lcc.uma.es>
+//       Juan J. Durillo <durillo@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.uma.jmetal.problem.multiobjective;
+
+import org.uma.jmetal.problem.ConstrainedProblem;
+import org.uma.jmetal.problem.impl.AbstractDoubleProblem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.solution.impl.GenericDoubleSolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class representing problem Tanaka
+ */
+public class Tanaka extends AbstractDoubleProblem implements ConstrainedProblem<DoubleSolution> {
+
+  /**
+   * Constructor.
+   * Creates a default instance of the problem Tanaka
+   */
+  public Tanaka() {
+    setNumberOfVariables(2);
+    setNumberOfObjectives(2);
+    setNumberOfConstraints(2);
+    setName("Tanaka") ;
+
+    List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables()) ;
+    List<Double> upperLimit = new ArrayList<>(getNumberOfVariables()) ;
+
+    for (int i = 0; i < getNumberOfVariables(); i++) {
+      lowerLimit.add(10e-5);
+      upperLimit.add(Math.PI);
+    }
+
+    setLowerLimit(lowerLimit);
+    setUpperLimit(upperLimit);
+  }
+
+  /** Evaluate() method */
+  @Override
+  public DoubleSolution createSolution() {
+    return new GenericDoubleSolution(this) ;
+  }
+
+  /** Evaluate() method */
+  @Override
+  public void evaluate(DoubleSolution solution)  {
+    solution.setObjective(0, solution.getVariableValue(0));
+    solution.setObjective(1, solution.getVariableValue(1));
+  }
+
+  /** EvaluateConstraints() method */
+  @Override
+  public void evaluateConstraints(DoubleSolution solution)  {
+    double[] constraint = new double[this.getNumberOfConstraints()];
+
+    double x1 = solution.getVariableValue(0) ;
+    double x2 = solution.getVariableValue(1) ;
+
+    constraint[0] = (x1 * x1 + x2 * x2 - 1.0 - 0.1 * Math.cos(16.0 * Math.atan(x1 / x2)));
+    constraint[1] = -2.0 * ((x1 - 0.5) * (x1 - 0.5) + (x2 - 0.5) * (x2 - 0.5) - 0.5);
+
+    int number = 0;
+    double total = 0.0;
+    for (int i = 0; i < this.getNumberOfConstraints(); i++) {
+      if (constraint[i] < 0.0) {
+        number++;
+        total += constraint[i];
+      }
+    }
+
+    solution.setOverallConstraintViolationDegree(total);
+  }
+}
