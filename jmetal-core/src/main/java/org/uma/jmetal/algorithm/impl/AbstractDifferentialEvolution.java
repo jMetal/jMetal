35b6d16d6ba0e417709942ec774eef48d76c2f81
ajnebro <ajnebro@outlook.com>
2014-12-23 12:29:23 +0100

    Commenting code

--- jmetal-core/src/main/java/org/uma/jmetal/algorithm/impl/AbstractDifferentialEvolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal/algorithm/impl/AbstractDifferentialEvolution.java
@@ -1,3 +1,17 @@
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 package org.uma.jmetal.algorithm.impl;
 
 import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
@@ -5,7 +19,9 @@ import org.uma.jmetal.operator.impl.selection.DifferentialEvolutionSelection;
 import org.uma.jmetal.solution.DoubleSolution;
 
 /**
- * Created by ajnebro on 26/10/14.
+ * Abstract class representing differential evolution (DE) algorithms
+ * @author Antonio J. Nebro
+ * @version 1.0
  */
 public abstract class AbstractDifferentialEvolution<Result> extends AbstractEvolutionaryAlgorithm<DoubleSolution, Result> {
   protected DifferentialEvolutionCrossover crossoverOperator ;
