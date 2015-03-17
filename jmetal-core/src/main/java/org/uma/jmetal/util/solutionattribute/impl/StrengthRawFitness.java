fdd76fcebb9fe82abd2c228be8d14ff6de0da16a
Juanjo <juanjod@gmail.com>
2015-03-17 16:26:15 +0100

    Eliminated duplicate code to compute the distance matrix among elements in a population. Now using exclusively the method provided by SolutionListUtils

--- jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/StrengthRawFitness.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/StrengthRawFitness.java
@@ -1,6 +1,7 @@
 package org.uma.jmetal.util.solutionattribute.impl;
 
 import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.SolutionListUtils;
 import org.uma.jmetal.util.comparator.DominanceComparator;
 import org.uma.jmetal.util.solutionattribute.DensityEstimator;
 
@@ -12,35 +13,9 @@ public class StrengthRawFitness <S extends Solution>
     extends GenericSolutionAttribute<S, Double> implements DensityEstimator<S>{
   private static final Comparator<Solution> DOMINANCE_COMPARATOR = new DominanceComparator();
 
-  private double distanceBetweenObjectives(S firstSolution, S secondSolution) {
-    double diff;
-    double distance = 0.0;
-    //euclidean distance
-    for (int nObj = 0; nObj < firstSolution.getNumberOfObjectives();nObj++){
-      diff = firstSolution.getObjective(nObj) - secondSolution.getObjective(nObj);
-      distance += Math.pow(diff,2.0);
-    } // for
-
-    return Math.sqrt(distance);
-  }
-
-  private double [][] distanceMatrix(List<S> solutionSet) {
-    //The matrix of distances
-    double [][] distance = new double [solutionSet.size()][solutionSet.size()];
-    for (int i = 0; i < solutionSet.size(); i++){
-      distance[i][i] = 0.0;
-      for (int j = i + 1; j < solutionSet.size(); j++){
-        distance[i][j] = distanceBetweenObjectives(solutionSet.get(i),solutionSet.get(j));
-        distance[j][i] = distance[i][j];
-      } // for
-    } // for
-    return distance;
-  } // distanceMatrix
-
-
   @Override
   public void computeDensityEstimator(List<S> solutionSet) {
-    double [][] distance = this.distanceMatrix(solutionSet);
+    double [][] distance = SolutionListUtils.distanceMatrix(solutionSet);
     double []   strength    = new double[solutionSet.size()];
     double []   rawFitness  = new double[solutionSet.size()];
     double kDistance                                          ;
