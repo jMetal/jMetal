80aa5a6e1c0008dcc437c9e8fe755daa83a0c3fb
ajnebro <ajnebro@outlook.com>
2015-03-27 07:04:16 +0100

    Indenting AbYSS code

--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/abyss/ABYSS.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/abyss/ABYSS.java
@@ -16,126 +16,126 @@ import java.util.List;
  * Created by cbarba on 25/3/15.
  */
 public class ABYSS extends AbstractABYSS<DoubleSolution> {
-    public ABYSS(int numberOfSubranges,int solutionSetSize, int refSet1Size, int refSet2Size, int archiveSize, int maxEvaluations,
-                 CrowdingDistanceArchive archive,     CrossoverOperator crossoverOperator,MutationLocalSearch improvement, DoubleProblem problem){
-        super(numberOfSubranges,solutionSetSize,refSet1Size,refSet2Size,archiveSize,maxEvaluations,archive,crossoverOperator,improvement,problem);
-    }
-    /**
-     * Runs of the AbYSS algorithm.
-     * as a result of the algorithm execution
-     */
-    @Override
-    public void run() {
+  public ABYSS(int numberOfSubranges,int solutionSetSize, int refSet1Size, int refSet2Size, int archiveSize, int maxEvaluations,
+      CrowdingDistanceArchive archive,     CrossoverOperator crossoverOperator,MutationLocalSearch improvement, DoubleProblem problem){
+    super(numberOfSubranges,solutionSetSize,refSet1Size,refSet2Size,archiveSize,maxEvaluations,archive,crossoverOperator,improvement,problem);
+  }
+  /**
+   * Runs of the AbYSS algorithm.
+   * as a result of the algorithm execution
+   */
+  @Override
+  public void run() {
 
-try {
+    try {
 
-    DoubleSolution solution;
-    // STEP 1. Build the initial solutionSet
-    initialListSolution();
+      DoubleSolution solution;
+      // STEP 1. Build the initial solutionSet
+      initialListSolution();
 
-    // STEP 2. Main loop
-    int newSolutions = 0;
-    while (evaluations < maxEvaluations) {
+      // STEP 2. Main loop
+      int newSolutions = 0;
+      while (evaluations < maxEvaluations) {
 
         referenceSetUpdate(true);
         newSolutions = subSetGeneration();
         while (newSolutions > 0 && evaluations < maxEvaluations ) { // New solutions are created
-            referenceSetUpdate(false);
+          referenceSetUpdate(false);
 
-            newSolutions = subSetGeneration();
+          newSolutions = subSetGeneration();
         } // while
 
         // RE-START
         if (evaluations < maxEvaluations) {
-            solutionSet.clear();
-            // Add refSet1 to SolutionSet
-            for (int i = 0; i < refSet1.size(); i++) {
-                solution = refSet1.get(i);
-                //solution.unMarked();
-                marked.setAttribute(solution,false);
-                solution = (DoubleSolution) improvementOperator.execute(solution);
-                evaluations += improvementOperator.getEvaluations();
-                solutionSet.add(solution);
-            }
-            // Remove refSet1 and refSet2
-            refSet1.clear();
-            refSet2.clear();
+          solutionSet.clear();
+          // Add refSet1 to SolutionSet
+          for (int i = 0; i < refSet1.size(); i++) {
+            solution = refSet1.get(i);
+            //solution.unMarked();
+            marked.setAttribute(solution,false);
+            solution = (DoubleSolution) improvementOperator.execute(solution);
+            evaluations += improvementOperator.getEvaluations();
+            solutionSet.add(solution);
+          }
+          // Remove refSet1 and refSet2
+          refSet1.clear();
+          refSet2.clear();
 
-            // Sort the archive and insert the best solutions
-            //CrowdingDistance<DoubleSolution> crowdingDistance = new CrowdingDistance();
-            archive.computeDistance();
-            Collections.sort(archive.getSolutionList(),crowdingDistanceComparator);
+          // Sort the archive and insert the best solutions
+          //CrowdingDistance<DoubleSolution> crowdingDistance = new CrowdingDistance();
+          archive.computeDistance();
+          Collections.sort(archive.getSolutionList(),crowdingDistanceComparator);
             /*distance.crowdingDistanceAssignment(archive_,
                     problem_.getNumberOfObjectives());
             archive_.sort(crowdingDistance_);*/
 
-            int insert = solutionSetSize / 2;
-            if (insert > archive.getMaxSize())
-                insert = archive.getMaxSize();
+          int insert = solutionSetSize / 2;
+          if (insert > archive.getMaxSize())
+            insert = archive.getMaxSize();
 
-            if (insert > (solutionSetSize - solutionSet.size()))
-                insert = solutionSetSize - solutionSet.size();
+          if (insert > (solutionSetSize - solutionSet.size()))
+            insert = solutionSetSize - solutionSet.size();
 
-            // Insert solutions
-            for (int i = 0; i < insert; i++) {
-                solution = archive.getSolutionList().get(i);
-                //solution = improvement(solution);
-                //solution.unMarked();
-                marked.setAttribute(solution,false);
-                solutionSet.add(solution);
+          // Insert solutions
+          for (int i = 0; i < insert; i++) {
+            solution = archive.getSolutionList().get(i);
+            //solution = improvement(solution);
+            //solution.unMarked();
+            marked.setAttribute(solution,false);
+            solutionSet.add(solution);
+          }
+
+          // Create the rest of solutions randomly
+          while (solutionSet.size() < solutionSetSize) {
+            solution = diversificationGeneration();
+            if(problem instanceof ConstrainedProblem){
+              ((ConstrainedProblem)problem).evaluateConstraints(solution);
             }
 
-            // Create the rest of solutions randomly
-            while (solutionSet.size() < solutionSetSize) {
-                solution = diversificationGeneration();
-                if(problem instanceof ConstrainedProblem){
-                    ((ConstrainedProblem)problem).evaluateConstraints(solution);
-                }
-
-                problem.evaluate(solution);
-                evaluations++;
-                solution = (DoubleSolution) improvementOperator.execute(solution);
-                evaluations += improvementOperator.getEvaluations();
-                //solution.unMarked();
-                marked.setAttribute(solution,false);
-                solutionSet.add(solution);
-            } // while
+            problem.evaluate(solution);
+            evaluations++;
+            solution = (DoubleSolution) improvementOperator.execute(solution);
+            evaluations += improvementOperator.getEvaluations();
+            //solution.unMarked();
+            marked.setAttribute(solution,false);
+            solutionSet.add(solution);
+          } // while
         } // if
-    } // while
-}catch(Exception e){
-e.printStackTrace();
+      } // while
+    }catch(Exception e){
+      e.printStackTrace();
     }
 
-        //archive_.printFeasibleFUN("FUN_AbYSS") ;
+    //archive_.printFeasibleFUN("FUN_AbYSS") ;
 
-        // STEP 4. Return the archive
-        //return archive_;
-    }
+    // STEP 4. Return the archive
+    //return archive_;
+  }
 
-    @Override
-    public List<? extends Solution> getResult() {
-        return archive.getSolutionList();
-    }
+  @Override
+  public List<? extends Solution> getResult() {
+    return archive.getSolutionList();
+  }
 
-    private void initialListSolution(){
-        try {
-            DoubleSolution solution;
-            for (int i = 0; i < this.solutionSetSize; i++) {
-                solution = super.diversificationGeneration();
-                problem.evaluate(solution);
-                if(problem instanceof ConstrainedProblem) {
-                    ((ConstrainedProblem)problem).evaluateConstraints(solution);
-                }
-                evaluations++;
-                solution = (DoubleSolution) improvementOperator.execute(solution);
-                marked.setAttribute(solution,false);
-                strenghtRawFitness.setAttribute(solution,0.0);
-                evaluations += improvementOperator.getEvaluations();
-                solutionSet.add(solution);
-            } // fpr
-        }catch (Exception ex){
-            ex.printStackTrace();
+  private void initialListSolution(){
+    try {
+      DoubleSolution solution;
+      for (int i = 0; i < this.solutionSetSize; i++) {
+        solution = super.diversificationGeneration();
+        problem.evaluate(solution);
+        if(problem instanceof ConstrainedProblem) {
+          ((ConstrainedProblem)problem).evaluateConstraints(solution);
         }
+        evaluations++;
+        solution = (DoubleSolution) improvementOperator.execute(solution);
+        marked.setAttribute(solution,false);
+        strenghtRawFitness.setAttribute(solution,0.0);
+        evaluations += improvementOperator.getEvaluations();
+        solutionSet.add(solution);
+      } // fpr
+    }catch (Exception ex){
+      ex.printStackTrace();
     }
+  }
 
 }
--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/abyss/ABYSSBuilder.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/abyss/ABYSSBuilder.java
@@ -11,11 +11,6 @@ import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
 import org.uma.jmetal.operator.impl.localsearch.MutationLocalSearch;
 import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
 import org.uma.jmetal.problem.DoubleProblem;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.uma.jmetal.solution.DoubleSolution;
-import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.util.AlgorithmBuilder;
 import org.uma.jmetal.util.archive.Archive;
 import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
@@ -23,119 +18,119 @@ import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
 
 public class ABYSSBuilder implements AlgorithmBuilder {
 
-    private DoubleProblem problem     ; // The problem to solve
-    private CrossoverOperator crossoverOperator   ; // Crossover operator
-    private MutationLocalSearch improvementOperator ; // Operator for improvement
-    private MutationOperator mutationOperator; // Mutation operator
-    private int numberOfSubranges; //subranges
-    private int populationSize;//Maximum size of the population
-    private int refSet1Size;//Maximum size of the reference set one
-    private int refSet2Size;//Maximum size of the reference set two
-    private int archiveSize;//Maximum size of the external archive
-    private int maxEvaluations;//Maximum number of getEvaluations to carry out
-    private CrowdingDistanceArchive archive;
-    public ABYSSBuilder(DoubleProblem problem,Archive archive){
-        this.populationSize = 20;
-        this.maxEvaluations = 25000;
-        this.archiveSize = 100;
-        this.refSet1Size = 10;
-        this.refSet2Size = 10;
-        this.numberOfSubranges = 4;
-        this.problem = problem;
-        double crossoverProbability = 0.9;
-        double distributionIndex=20.0;
-        this.crossoverOperator = new SBXCrossover(crossoverProbability,distributionIndex);
-        double mutationProbability= 1/problem.getNumberOfVariables();
-        this.mutationOperator = new PolynomialMutation(mutationProbability,distributionIndex);
-        int improvementRounds= 1;
-        this.improvementOperator = new MutationLocalSearch(improvementRounds,mutationOperator,archive.getSolutionList(),problem);
-        this.archive =(CrowdingDistanceArchive)archive;
-    }
+  private DoubleProblem problem     ; // The problem to solve
+  private CrossoverOperator crossoverOperator   ; // Crossover operator
+  private MutationLocalSearch improvementOperator ; // Operator for improvement
+  private MutationOperator mutationOperator; // Mutation operator
+  private int numberOfSubranges; //subranges
+  private int populationSize;//Maximum size of the population
+  private int refSet1Size;//Maximum size of the reference set one
+  private int refSet2Size;//Maximum size of the reference set two
+  private int archiveSize;//Maximum size of the external archive
+  private int maxEvaluations;//Maximum number of getEvaluations to carry out
+  private CrowdingDistanceArchive archive;
+  public ABYSSBuilder(DoubleProblem problem,Archive archive){
+    this.populationSize = 20;
+    this.maxEvaluations = 25000;
+    this.archiveSize = 100;
+    this.refSet1Size = 10;
+    this.refSet2Size = 10;
+    this.numberOfSubranges = 4;
+    this.problem = problem;
+    double crossoverProbability = 0.9;
+    double distributionIndex=20.0;
+    this.crossoverOperator = new SBXCrossover(crossoverProbability,distributionIndex);
+    double mutationProbability= 1/problem.getNumberOfVariables();
+    this.mutationOperator = new PolynomialMutation(mutationProbability,distributionIndex);
+    int improvementRounds= 1;
+    this.improvementOperator = new MutationLocalSearch(improvementRounds,mutationOperator,archive.getSolutionList(),problem);
+    this.archive =(CrowdingDistanceArchive)archive;
+  }
 
-    @Override
-    public Algorithm build() {
-        return new ABYSS(numberOfSubranges,populationSize,refSet1Size,refSet2Size,archiveSize,maxEvaluations,
-                archive,crossoverOperator,improvementOperator,problem);
-    }
+  @Override
+  public Algorithm build() {
+    return new ABYSS(numberOfSubranges,populationSize,refSet1Size,refSet2Size,archiveSize,maxEvaluations,
+        archive,crossoverOperator,improvementOperator,problem);
+  }
 
-    public CrossoverOperator getCrossoverOperator() {
-        return crossoverOperator;
-    }
+  public CrossoverOperator getCrossoverOperator() {
+    return crossoverOperator;
+  }
 
-    public ABYSSBuilder setCrossoverOperator(CrossoverOperator crossoverOperator) {
-        this.crossoverOperator = crossoverOperator;
-        return  this;
-    }
+  public ABYSSBuilder setCrossoverOperator(CrossoverOperator crossoverOperator) {
+    this.crossoverOperator = crossoverOperator;
+    return  this;
+  }
 
-    public MutationLocalSearch getImprovementOperator() {
-        return improvementOperator;
-    }
+  public MutationLocalSearch getImprovementOperator() {
+    return improvementOperator;
+  }
 
-    public ABYSSBuilder setImprovementOperator(MutationLocalSearch improvementOperator) {
-        this.improvementOperator = improvementOperator;
-        return  this;
-    }
+  public ABYSSBuilder setImprovementOperator(MutationLocalSearch improvementOperator) {
+    this.improvementOperator = improvementOperator;
+    return  this;
+  }
 
-    public MutationOperator getMutationOperator() {
-        return mutationOperator;
-    }
+  public MutationOperator getMutationOperator() {
+    return mutationOperator;
+  }
 
-    public ABYSSBuilder setMutationOperator(MutationOperator mutationOperator) {
-        this.mutationOperator = mutationOperator;
-        return  this;
-    }
+  public ABYSSBuilder setMutationOperator(MutationOperator mutationOperator) {
+    this.mutationOperator = mutationOperator;
+    return  this;
+  }
 
-    public int getNumberOfSubranges() {
-        return numberOfSubranges;
-    }
+  public int getNumberOfSubranges() {
+    return numberOfSubranges;
+  }
 
-    public ABYSSBuilder setNumberOfSubranges(int numberOfSubranges) {
-        this.numberOfSubranges = numberOfSubranges;
-        return  this;
-    }
+  public ABYSSBuilder setNumberOfSubranges(int numberOfSubranges) {
+    this.numberOfSubranges = numberOfSubranges;
+    return  this;
+  }
 
-    public int getPopulationSize() {
-        return populationSize;
-    }
+  public int getPopulationSize() {
+    return populationSize;
+  }
 
-    public ABYSSBuilder setPopulationSize(int populationSize) {
-        this.populationSize = populationSize;
-        return  this;
-    }
+  public ABYSSBuilder setPopulationSize(int populationSize) {
+    this.populationSize = populationSize;
+    return  this;
+  }
 
-    public int getRefSet1Size() {
-        return refSet1Size;
-    }
+  public int getRefSet1Size() {
+    return refSet1Size;
+  }
 
-    public ABYSSBuilder setRefSet1Size(int refSet1Size) {
-        this.refSet1Size = refSet1Size;
-        return  this;
-    }
+  public ABYSSBuilder setRefSet1Size(int refSet1Size) {
+    this.refSet1Size = refSet1Size;
+    return  this;
+  }
 
-    public int getRefSet2Size() {
-        return refSet2Size;
-    }
+  public int getRefSet2Size() {
+    return refSet2Size;
+  }
 
-    public ABYSSBuilder setRefSet2Size(int refSet2Size) {
-        this.refSet2Size = refSet2Size;
-        return  this;
-    }
+  public ABYSSBuilder setRefSet2Size(int refSet2Size) {
+    this.refSet2Size = refSet2Size;
+    return  this;
+  }
 
-    public int getArchiveSize() {
-        return archiveSize;
-    }
+  public int getArchiveSize() {
+    return archiveSize;
+  }
 
-    public ABYSSBuilder setArchiveSize(int archiveSize) {
-        this.archiveSize = archiveSize;
-        return  this;
-    }
+  public ABYSSBuilder setArchiveSize(int archiveSize) {
+    this.archiveSize = archiveSize;
+    return  this;
+  }
 
-    public int getMaxEvaluations() {
-        return maxEvaluations;
-    }
+  public int getMaxEvaluations() {
+    return maxEvaluations;
+  }
 
-    public ABYSSBuilder setMaxEvaluations(int maxEvaluations) {
-        this.maxEvaluations = maxEvaluations;
-        return  this;
-    }
+  public ABYSSBuilder setMaxEvaluations(int maxEvaluations) {
+    this.maxEvaluations = maxEvaluations;
+    return  this;
+  }
 }// end ABYSSBuilder
