3649378d31023ca709c494e6bcf1df4c4fe95f4f
ajnebro <ajnebro@outlook.com>
2015-04-10 07:18:26 +0200

    Moving class MOCellNeighgourhood to mocell/util

--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/mocell/util/MOCellNeighborhood.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/mocell/util/MOCellNeighborhood.java
@@ -19,7 +19,7 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package org.uma.jmetal.algorithm.multiobjective.mocell;
+package org.uma.jmetal.algorithm.multiobjective.mocell.util;
 
 import java.util.ArrayList;
 import java.util.List;
