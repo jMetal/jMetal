4aab431af388cb25b4640b9142753466990832be
ajnebro <ajnebro@outlook.com>
2015-05-02 11:03:58 +0200

    Working on SMSEMOA

--- jmetal-core/src/main/java/org/uma/jmetal/qualityindicator/hypervolume/impl/PISAHypervolume.java
+++ jmetal-core/src/main/java/org/uma/jmetal/qualityindicator/hypervolume/impl/PISAHypervolume.java
@@ -3,10 +3,9 @@ package org.uma.jmetal.qualityindicator.hypervolume.impl;
 import org.uma.jmetal.qualityindicator.hypervolume.Hypervolume;
 import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.util.JMetalException;
-import org.uma.jmetal.util.criteria.Criteria;
 import org.uma.jmetal.util.front.Front;
 import org.uma.jmetal.util.front.imp.ArrayFront;
-import org.uma.jmetal.util.front.util.FrontUtils;
+import org.uma.jmetal.util.front.imp.FrontUtils;
 import org.uma.jmetal.util.naming.impl.SimpleDescribedEntity;
 
 import java.util.List;
@@ -25,16 +24,19 @@ import java.util.List;
  */
 
 public class PISAHypervolume extends SimpleDescribedEntity implements Hypervolume {
+
   public PISAHypervolume() {
     super("HV", "Hypervolume quality indicator. PISA based implementation") ;
   }
 
   @Override
-  public double[] computeHypervolumeContribution(List<Criteria> front) {
-    return new double[0];
+  public double[] computeHypervolumeContribution(Front front) {
+return null ;
+    //return hypervolume(front, trueParetoFront) ;
   }
 
-    return hypervolume(paretoFrontApproximation, trueParetoFront) ;
+  @Override public double execute(Front frontA, Front frontB) {
+    return 0;
   }
 
   @Override
