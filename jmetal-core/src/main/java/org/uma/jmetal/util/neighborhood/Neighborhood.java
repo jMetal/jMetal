18a59d019edc94ab53b0bef432593b4109429cd4
ajnebro <ajnebro@outlook.com>
2015-05-21 08:09:57 +0200

    Starting to work in neighborhoods

---
+++ jmetal-core/src/main/java/org/uma/jmetal/util/neighborhood/Neighborhood.java
@@ -0,0 +1,12 @@
+package org.uma.jmetal.util.neighborhood;
+
+import org.uma.jmetal.solution.Solution;
+
+import java.util.List;
+
+/**
+ * Created by ajnebro on 20/5/15.
+ */
+public interface Neighborhood<S extends Solution> {
+  public List<S> getNeighbors(List<S> solutionList, int solutionIndex) ;
+}
