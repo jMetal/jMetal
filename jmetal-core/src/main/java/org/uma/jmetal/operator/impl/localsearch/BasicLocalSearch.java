6014dc72d7fd23f043bbd8728b77bf7eda87f63f
ajnebro <ajnebro@outlook.com>
2015-06-15 08:02:25 +0200

    Adding class BasicLocalSearch

--- jmetal-core/src/main/java/org/uma/jmetal/operator/impl/localsearch/BasicLocalSearch.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/impl/localsearch/BasicLocalSearch.java
@@ -5,54 +5,51 @@ import org.uma.jmetal.operator.MutationOperator;
 import org.uma.jmetal.problem.ConstrainedProblem;
 import org.uma.jmetal.problem.Problem;
 import org.uma.jmetal.solution.Solution;
-import org.uma.jmetal.util.archive.Archive;
-import org.uma.jmetal.util.comparator.DominanceComparator;
 import org.uma.jmetal.util.comparator.impl.OverallConstraintViolationComparator;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
 import java.util.Comparator;
 
-
 /**
- * This class implements a local search operator based in the use of a
- * mutation operator. An archive is used to store the non-dominated solutions
- * found during the search.
+ * This class implements a basic local search operator based in the use of a
+ * mutation operator.
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
-public class ArchiveMutationLocalSearch<S extends Solution<?>> implements LocalSearchOperator<S>{
+public class BasicLocalSearch<S extends Solution<?>> implements LocalSearchOperator<S>{
   private Problem<S> problem;
-  private Archive<S> archive;
   private int improvementRounds ;
   private Comparator constraintComparator ;
-  private Comparator dominanceComparator ;
+  private Comparator comparator ;
 
   private MutationOperator mutationOperator;
   private int evaluations ;
 
+  private JMetalRandom randomGenerator ;
   /**
    * Constructor.
    * Creates a new local search object.
    * @param improvementRounds number of iterations
    * @param mutationOperator mutation operator
-   * @param archive archive to store non-dominated solution
+   * @param comparator comparator to determine which solution is the best
    * @param problem problem to resolve
 
    */
-  public ArchiveMutationLocalSearch(int improvementRounds, MutationOperator<S> mutationOperator,
-      Archive<S> archive, Problem<S> problem){
+  public BasicLocalSearch(int improvementRounds, MutationOperator<S> mutationOperator,
+      Comparator<S> comparator, Problem<S> problem){
     this.problem=problem;
     this.mutationOperator=mutationOperator;
     this.improvementRounds=improvementRounds;
-    this.archive=archive;
-    dominanceComparator  = new DominanceComparator();
+    this.comparator  = comparator ;
     constraintComparator = new OverallConstraintViolationComparator();
+
+    randomGenerator = JMetalRandom.getInstance() ;
   }
 
   /**
    * Executes the local search.
-   *
    * @param  solution The solution to improve
-   * @return The improved solution
+   * @return An improved solution
    */
   public S execute(S solution) {
     int i = 0;
@@ -67,11 +64,10 @@ public class ArchiveMutationLocalSearch<S extends Solution<?>> implements LocalS
 
         ((ConstrainedProblem) problem).evaluateConstraints(mutatedSolution);
         best = constraintComparator.compare(mutatedSolution, solution);
-        if (best == 0)
-        {
+        if (best == 0) {
           problem.evaluate(mutatedSolution);
           evaluations++;
-          best = dominanceComparator.compare(mutatedSolution, solution);
+          best = comparator.compare(mutatedSolution, solution);
         } else if (best == -1) {
           problem.evaluate(mutatedSolution);
           evaluations++;
@@ -79,7 +75,7 @@ public class ArchiveMutationLocalSearch<S extends Solution<?>> implements LocalS
       } else {
         problem.evaluate(mutatedSolution);
         evaluations++;
-        best = dominanceComparator.compare(mutatedSolution, solution);
+        best = comparator.compare(mutatedSolution, solution);
       }
       if (best == -1) {
         solution = mutatedSolution;
@@ -88,7 +84,9 @@ public class ArchiveMutationLocalSearch<S extends Solution<?>> implements LocalS
         ;
       }
       else {
-        archive.add(mutatedSolution);
+        if (randomGenerator.nextDouble() < 0.5) {
+          solution = mutatedSolution ;
+        }
       }
       i++ ;
     }
