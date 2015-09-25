08f2f6fb4e44b0bfbaa9567cfb370895c035ce5f
juanjo <juanjod@gmail.com>
2015-09-25 11:28:19 +0200

    Removed unused import from MOMBI2

--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/mombi/MOMBI2.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/mombi/MOMBI2.java
@@ -1,7 +1,6 @@
 package org.uma.jmetal.algorithm.multiobjective.mombi;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.uma.jmetal.algorithm.multiobjective.mombi.util.*;
