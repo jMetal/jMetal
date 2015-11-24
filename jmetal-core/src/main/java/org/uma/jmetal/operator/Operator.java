cc46ac8b2a8d124a849fdbe986784fde8220dc48
ajnebro <ajnebro@outlook.com>
2015-11-24 23:42:41 +0100

    Minor changes

--- jmetal-core/src/main/java/org/uma/jmetal/operator/Operator.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/Operator.java
@@ -13,11 +13,11 @@
 
 package org.uma.jmetal.operator;
 
-/**
- * Interface representing an operator
+/** * Interface representing an operator
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  * @version 0.1
+
  * @param <Source> Source Class of the object to be operated with
  * @param <Result> Result Class of the result obtained after applying the operator
  */
