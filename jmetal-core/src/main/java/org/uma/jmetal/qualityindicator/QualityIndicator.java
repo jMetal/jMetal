cd6458ae26e30a577e9bee637b951e14c43ee263
ajnebro <ajnebro@outlook.com>
2015-12-22 11:51:10 +0100

    Minor changes

--- jmetal-core/src/main/java/org/uma/jmetal/qualityindicator/QualityIndicator.java
+++ jmetal-core/src/main/java/org/uma/jmetal/qualityindicator/QualityIndicator.java
@@ -15,12 +15,14 @@ package org.uma.jmetal.qualityindicator;
 
 import org.uma.jmetal.util.naming.DescribedEntity;
 
+import java.io.Serializable;
+
 /**
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
 
  * @param <Evaluate> Entity to evaluate
  * @param <Result> Result of the evaluation
  */
-public interface QualityIndicator<Evaluate, Result> extends DescribedEntity {
+public interface QualityIndicator<Evaluate, Result> extends DescribedEntity, Serializable {
   public Result evaluate(Evaluate evaluate) ;
 }
