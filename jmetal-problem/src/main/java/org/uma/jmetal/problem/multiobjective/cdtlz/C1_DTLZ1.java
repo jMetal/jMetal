bc6cedfa786b036d772e7b9241fa4ae9786661c4
ajnebro <ajnebro@outlook.com>
2016-01-08 12:38:41 +0100

    Adding classes C1_DTLZ1 and C1_DTLZ3

---
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/cdtlz/C1_DTLZ1.java
@@ -0,0 +1,86 @@
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package org.uma.jmetal.problem.multiobjective.cdtlz;
+
+import org.uma.jmetal.problem.ConstrainedProblem;
+import org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.solutionattribute.impl.NumberOfViolatedConstraints;
+import org.uma.jmetal.util.solutionattribute.impl.OverallConstraintViolation;
+
+/**
+ * Problem C1-DTLZ1, defined in:
+ * Jain, H. and K. Deb.  "An Evolutionary Many-Objective Optimization Algorithm Using Reference-Point-Based
+ * Nondominated Sorting Approach, Part II: Handling Constraints and Extending to an Adaptive Approach."
+ * EEE Transactions on Evolutionary Computation, 18(4):602-622, 2014.
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class C1_DTLZ1 extends DTLZ1 implements ConstrainedProblem<DoubleSolution> {
+  public OverallConstraintViolation<DoubleSolution> overallConstraintViolationDegree ;
+  public NumberOfViolatedConstraints<DoubleSolution> numberOfViolatedConstraints ;
+
+  /**
+   * Default constructor
+   */
+  public C1_DTLZ1() {
+    this(7, 3) ;
+  }
+
+  /**
+   * Constructor
+   * @param numberOfVariables
+   * @param numberOfObjectives
+   */
+  public C1_DTLZ1(int numberOfVariables, int numberOfObjectives) {
+    super(numberOfVariables, numberOfObjectives) ;
+
+    setNumberOfConstraints(1);
+
+    overallConstraintViolationDegree = new OverallConstraintViolation<DoubleSolution>() ;
+    numberOfViolatedConstraints = new NumberOfViolatedConstraints<DoubleSolution>() ;
+  }
+
+  @Override
+  public void evaluateConstraints(DoubleSolution solution) {
+    double[] constraint = new double[this.getNumberOfConstraints()];
+
+    int numberOfVariables = getNumberOfVariables();
+
+    double[] x = new double[numberOfVariables] ;
+
+    for (int i = 0; i < numberOfVariables; i++) {
+      x[i] = solution.getVariableValue(i) ;
+    }
+
+    double sum = 0 ;
+    for (int i = 0; i < getNumberOfObjectives() - 2; i++) {
+      sum += solution.getObjective(i) / 0.5 ;
+    }
+
+    constraint[0] = 1.0 - solution.getObjective(getNumberOfObjectives()-1) - sum ;
+
+    double overallConstraintViolation = 0.0;
+    int violatedConstraints = 0;
+    for (int i = 0; i < getNumberOfConstraints(); i++) {
+      if (constraint[i]<0.0){
+        overallConstraintViolation+=constraint[i];
+        violatedConstraints++;
+      }
+    }
+
+    overallConstraintViolationDegree.setAttribute(solution, overallConstraintViolation);
+    numberOfViolatedConstraints.setAttribute(solution, violatedConstraints);
+  }
+}
