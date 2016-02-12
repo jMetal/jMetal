a68a65c25b8bf7a970df1514ba9a76ea4f1300dd
ajnebro <ajnebro@outlook.com>
2016-02-12 17:08:37 +0100

    Added some tests

---
+++ jmetal-core/src/test/java/org/uma/jmetal/util/distance/impl/CosineDistanceBetweenSolutionsInObjectiveSpaceTest.java
@@ -0,0 +1,89 @@
+package org.uma.jmetal.util.distance.impl;
+
+import org.junit.Test;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.distance.impl.CosineDistanceBetweenSolutionsInObjectiveSpace ;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by ajnebro on 12/2/16.
+ */
+@SuppressWarnings("unchecked")
+public class CosineDistanceBetweenSolutionsInObjectiveSpaceTest {
+  private static final double EPSILON = 0.00000000001 ;
+
+  @Test
+  public void shouldIdenticalPointsHaveADistanceOfOne() {
+    Solution idealPoint = mock(Solution.class) ;
+    when(idealPoint.getObjective(0)).thenReturn(0.0) ;
+    when(idealPoint.getObjective(1)).thenReturn(0.0) ;
+    when(idealPoint.getNumberOfObjectives()).thenReturn(3) ;
+
+    Solution point1 = mock(Solution.class) ;
+    when(point1.getObjective(0)).thenReturn(1.0) ;
+    when(point1.getObjective(1)).thenReturn(1.0) ;
+    when(point1.getNumberOfObjectives()).thenReturn(3) ;
+
+    Solution point2 = mock(Solution.class) ;
+    when(point2.getObjective(0)).thenReturn(1.0) ;
+    when(point2.getObjective(1)).thenReturn(1.0) ;
+    when(point2.getNumberOfObjectives()).thenReturn(3) ;
+
+    CosineDistanceBetweenSolutionsInObjectiveSpace distance =
+        new CosineDistanceBetweenSolutionsInObjectiveSpace(idealPoint) ;
+
+    double receivedValue = distance.getDistance(point1, point2) ;
+    assertEquals(1.0, receivedValue, EPSILON) ;
+  }
+
+  @Test
+  public void shouldIdenticalPointsInTheSameDirectionHaveADistanceOfOne() {
+    Solution idealPoint = mock(Solution.class) ;
+    when(idealPoint.getObjective(0)).thenReturn(0.0) ;
+    when(idealPoint.getObjective(1)).thenReturn(0.0) ;
+    when(idealPoint.getNumberOfObjectives()).thenReturn(3) ;
+
+    Solution point1 = mock(Solution.class) ;
+    when(point1.getObjective(0)).thenReturn(1.0) ;
+    when(point1.getObjective(1)).thenReturn(1.0) ;
+    when(point1.getNumberOfObjectives()).thenReturn(3) ;
+
+    Solution point2 = mock(Solution.class) ;
+    when(point2.getObjective(0)).thenReturn(2.0) ;
+    when(point2.getObjective(1)).thenReturn(2.0) ;
+    when(point2.getNumberOfObjectives()).thenReturn(3) ;
+
+    CosineDistanceBetweenSolutionsInObjectiveSpace distance =
+        new CosineDistanceBetweenSolutionsInObjectiveSpace(idealPoint) ;
+
+    double receivedValue = distance.getDistance(point1, point2) ;
+    assertEquals(1.0, receivedValue, EPSILON) ;
+  }
+
+  @Test
+  public void shouldTwoPerpendicularPointsHaveADistanceOfZero() {
+    Solution idealPoint = mock(Solution.class) ;
+    when(idealPoint.getObjective(0)).thenReturn(0.0) ;
+    when(idealPoint.getObjective(1)).thenReturn(0.0) ;
+    when(idealPoint.getNumberOfObjectives()).thenReturn(3) ;
+
+    Solution point1 = mock(Solution.class) ;
+    when(point1.getObjective(0)).thenReturn(0.0) ;
+    when(point1.getObjective(1)).thenReturn(1.0) ;
+    when(point1.getNumberOfObjectives()).thenReturn(3) ;
+
+    Solution point2 = mock(Solution.class) ;
+    when(point2.getObjective(0)).thenReturn(1.0) ;
+    when(point2.getObjective(1)).thenReturn(0.0) ;
+    when(point2.getNumberOfObjectives()).thenReturn(3) ;
+
+    CosineDistanceBetweenSolutionsInObjectiveSpace distance =
+        new CosineDistanceBetweenSolutionsInObjectiveSpace(idealPoint) ;
+
+    double receivedValue = distance.getDistance(point1, point2) ;
+    assertEquals(0.0, receivedValue, EPSILON) ;
+  }
+}
