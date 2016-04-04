4c99f14cbafc111b662af507f0376746abad7b68
ajnebro <ajnebro@outlook.com>
2016-04-04 11:35:16 +0200

    Adding reference fronts to jmetal-algorithm/src/test/resources

--- jmetal-algorithm/src/test/java/org/uma/jmetal/algorithm/multiobjective/nsgaii/NSGAIIIT.java
+++ jmetal-algorithm/src/test/java/org/uma/jmetal/algorithm/multiobjective/nsgaii/NSGAIIIT.java
@@ -6,9 +6,17 @@ import org.uma.jmetal.operator.CrossoverOperator;
 import org.uma.jmetal.operator.MutationOperator;
 import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
 import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
+import org.uma.jmetal.problem.multiobjective.ConstrEx;
 import org.uma.jmetal.problem.multiobjective.Kursawe;
+import org.uma.jmetal.qualityindicator.QualityIndicator;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.PISAHypervolume;
 import org.uma.jmetal.solution.DoubleSolution;
 import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.front.Front;
+import org.uma.jmetal.util.front.imp.ArrayFront;
+import org.uma.jmetal.util.front.util.FrontNormalizer;
+import org.uma.jmetal.util.front.util.FrontUtils;
+import org.uma.jmetal.util.point.util.PointSolution;
 
 import java.util.List;
 
@@ -43,5 +51,42 @@ public class NSGAIIIT {
     settings, should return 100 solutions
     */
     assertTrue(population.size() >= 98) ;
+  }
+
+  @Test
+  public void shouldTheAlgorithmReturnAGoodQualityFrontWhenSolvingAConstrainedProblem() throws Exception {
+    ConstrEx problem = new ConstrEx() ;
+    CrossoverOperator<DoubleSolution> crossover;
+    MutationOperator<DoubleSolution> mutation;
+
+    double crossoverProbability = 0.9 ;
+    double crossoverDistributionIndex = 20.0 ;
+    crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex) ;
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables() ;
+    double mutationDistributionIndex = 20.0 ;
+    mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex) ;
+
+    algorithm = new NSGAIIBuilder<DoubleSolution>(problem, crossover, mutation).build() ;
+
+    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+        .execute() ;
+
+    List<DoubleSolution> population = algorithm.getResult() ;
+
+    String referenceFrontFileName = "/referenceFronts/ConstrEx.pf" ;
+
+    Front referenceFront = new ArrayFront(referenceFrontFileName);
+    FrontNormalizer frontNormalizer = new FrontNormalizer(referenceFront) ;
+
+    Front normalizedReferenceFront = frontNormalizer.normalize(referenceFront) ;
+    Front normalizedFront = frontNormalizer.normalize(new ArrayFront(population)) ;
+    List<PointSolution> normalizedPopulation = FrontUtils
+        .convertFrontToSolutionList(normalizedFront) ;
+
+    double hv = new PISAHypervolume<PointSolution>(normalizedReferenceFront).evaluate(normalizedPopulation) ;
+
+    assertTrue(population.size() >= 98) ;
+    assertTrue(hv > 0.77) ;
   }
 }
