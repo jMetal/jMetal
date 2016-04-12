48752561168ab70d4e711f04aad0d02f5fee4c37
ajnebro <ajnebro@outlook.com>
2016-04-12 16:38:47 +0200

    Working on DefaultIntegerPermutationSolutionTest

--- jmetal-core/src/test/java/org/uma/jmetal/solution/impl/DefaultIntegerPermutationSolutionTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/solution/impl/DefaultIntegerPermutationSolutionTest.java
@@ -13,11 +13,52 @@
 
 package org.uma.jmetal.solution.impl;
 
+import org.junit.Test;
+import org.uma.jmetal.problem.impl.AbstractBinaryProblem;
+import org.uma.jmetal.problem.impl.AbstractIntegerPermutationProblem;
+import org.uma.jmetal.solution.BinarySolution;
+import org.uma.jmetal.solution.PermutationSolution;
+
 import static org.junit.Assert.*;
 
 /**
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
 public class DefaultIntegerPermutationSolutionTest {
-  
+
+  @Test
+  public void shouldConstructorCreateAValidSolution() {
+
+  }
+
+  /**
+   * Mock class representing a integer permutation problem
+   */
+  private class MockIntegerPermutationProblem extends AbstractIntegerPermutationProblem {
+    /**
+     * Constructor
+     */
+    public MockIntegerPermutationProblem() {
+      this(3);
+    }
+
+    /**
+     * Constructor
+     */
+    public MockIntegerPermutationProblem(Integer numberOfVariables) {
+      setNumberOfVariables(numberOfVariables);
+      setNumberOfObjectives(2);
+    }
+
+    @Override
+    public void evaluate(PermutationSolution<Integer> solution) {
+
+    }
+
+
+    @Override
+    public int getPermutationLength() {
+      return 0;
+    }
+  }
 }
