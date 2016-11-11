3c53d8a6dc50d119a27e66b7e651fe9cd86f7fb7
Antonio J. Nebro <antonio@lcc.uma.es>
2016-11-12 00:52:54 +0100

    Improve class MOCell

--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/mocell/MOCell.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/mocell/MOCell.java
@@ -45,14 +45,14 @@ public class MOCell<S extends Solution<?>> extends AbstractGeneticAlgorithm<S, L
   protected int maxEvaluations;
   protected final SolutionListEvaluator<S> evaluator;
 
-  private Neighborhood<S> neighborhood;
-  private int currentIndividual;
-  private List<S> currentNeighbors;
+  protected Neighborhood<S> neighborhood;
+  protected int currentIndividual;
+  protected List<S> currentNeighbors;
 
-  private BoundedArchive<S> archive;
+  protected BoundedArchive<S> archive;
 
-  private Comparator<S> dominanceComparator;
-  private LocationAttribute<S> location;
+  protected Comparator<S> dominanceComparator;
+  protected LocationAttribute<S> location;
 
   /**
    * Constructor
@@ -203,4 +203,6 @@ public class MOCell<S extends Solution<?>> extends AbstractGeneticAlgorithm<S, L
   @Override public String getDescription() {
     return "Multi-Objective Cellular evolutionry algorithm" ;
   }
+
+
 }
