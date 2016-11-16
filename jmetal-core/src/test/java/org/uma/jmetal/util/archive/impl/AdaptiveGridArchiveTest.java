22e76ccef6939a9dc32d3acd60c2734ced6620b3
Antonio J. Nebro <antonio@lcc.uma.es>
2016-11-16 12:26:24 +0100

    Add classes AdaptiveGridArchiveTest and NonDominatedSolutionListArchiveTest

---
+++ jmetal-core/src/test/java/org/uma/jmetal/util/archive/impl/AdaptiveGridArchiveTest.java
@@ -0,0 +1,40 @@
+package org.uma.jmetal.util.archive.impl;
+
+import org.junit.Test;
+import org.uma.jmetal.solution.IntegerSolution;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ajnebro on 16/11/16.
+ */
+public class AdaptiveGridArchiveTest {
+  @Test
+  public void shouldConstructorCreateAnArchiveWithTheRightCapacity() {
+    AdaptiveGridArchive<IntegerSolution> archive ;
+
+    int capacity = 100 ;
+    archive = new AdaptiveGridArchive<>(100, 2, 2) ;
+
+    assertEquals(capacity, archive.getMaxSize()) ;
+  }
+
+  @Test
+  public void shouldConstructorCreateAnEmptyArchive() {
+    AdaptiveGridArchive<IntegerSolution> archive ;
+
+    archive = new AdaptiveGridArchive<>(100, 2, 2) ;
+
+    assertEquals(0, archive.size()) ;
+  }
+
+  @Test
+  public void shouldProneDoNothingIfTheArchiveIsEmpty() {
+    AdaptiveGridArchive<IntegerSolution> archive ;
+
+    archive = new AdaptiveGridArchive<>(4, 2, 2) ;
+    archive.prune();
+
+    assertEquals(0, archive.size()) ;
+  }
+}
