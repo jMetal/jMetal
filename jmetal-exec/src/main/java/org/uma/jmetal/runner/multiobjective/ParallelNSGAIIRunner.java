6f8ff578acb3f0b6caa7ed7a13dbcce76dc321b6
Antonio J. Nebro <antonio@lcc.uma.es>
2017-09-10 13:06:47 +0200

    Minor changes

--- jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/ParallelNSGAIIRunner.java
+++ jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/ParallelNSGAIIRunner.java
@@ -66,7 +66,7 @@ public class ParallelNSGAIIRunner extends AbstractAlgorithmRunner {
 
     selection = new BinaryTournamentSelection<DoubleSolution>();
 
-    SolutionListEvaluator<DoubleSolution> evaluator = new MultithreadedSolutionListEvaluator<DoubleSolution>(0, problem) ;
+    SolutionListEvaluator<DoubleSolution> evaluator = new MultithreadedSolutionListEvaluator<DoubleSolution>(8, problem) ;
 
     NSGAIIBuilder<DoubleSolution> builder = new NSGAIIBuilder<DoubleSolution>(problem, crossover, mutation)
         .setSelectionOperator(selection)
