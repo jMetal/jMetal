c06424f9d25874bc51c8f0465cee4b70164b6717
ajnebro <antonio@lcc.uma.es>
2018-11-27 18:54:07 +0100

    Minor changes

--- jmetal-core/src/main/java/org/uma/jmetal/util/aggregativefunction/AggregativeFunction.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/aggregativefunction/AggregativeFunction.java
@@ -1,6 +1,5 @@
 package org.uma.jmetal.util.aggregativefunction;
 
-import com.sun.tools.jdi.SocketListeningConnector;
 import org.uma.jmetal.solution.Solution;
 
 public interface AggregativeFunction<S extends Solution<?>> {
