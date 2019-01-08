d9d30823c00942b3dae5af908d66adf03f2bed9c
Juanjo <juanjod@gmail.com>
2019-01-08 09:52:43 +0100

    Added atrribute for storing a textual representation of solution

---
+++ jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/SolutionTextRepresentation.java
@@ -0,0 +1,6 @@
+package org.uma.jmetal.util.solutionattribute.impl;
+
+import org.uma.jmetal.solution.Solution;
+
+public class SolutionTextRepresentation extends GenericSolutionAttribute<Solution<?>,String>{
+}
