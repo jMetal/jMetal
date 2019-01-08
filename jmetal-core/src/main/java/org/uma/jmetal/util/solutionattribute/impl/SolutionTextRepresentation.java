b6320865ff70695836c389cf126d90541b3ea344
Juanjo <juanjod@gmail.com>
2019-01-08 14:16:58 +0100

    Applied singleton pattern to the SolutionTextRepresentation attribute

--- jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/SolutionTextRepresentation.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/solutionattribute/impl/SolutionTextRepresentation.java
@@ -3,4 +3,13 @@ package org.uma.jmetal.util.solutionattribute.impl;
 import org.uma.jmetal.solution.Solution;
 
 public class SolutionTextRepresentation extends GenericSolutionAttribute<Solution<?>,String>{
+
+  private static SolutionTextRepresentation singleInstance = null;
+  private SolutionTextRepresentation() {}
+
+  public static SolutionTextRepresentation getAttribute() {
+    if (singleInstance == null)
+      singleInstance = new SolutionTextRepresentation();
+    return singleInstance;
+  }
 }
