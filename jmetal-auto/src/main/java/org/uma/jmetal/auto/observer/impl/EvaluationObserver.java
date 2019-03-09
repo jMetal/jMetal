22ea7346975096577c1d4362e12c7d78726780a1
ajnebro <antonio@lcc.uma.es>
2019-03-10 00:40:30 +0100

    Add subproject jmetal-auto

---
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/observer/impl/EvaluationObserver.java
@@ -0,0 +1,59 @@
+package org.uma.jmetal.auto.observer.impl;
+
+import org.uma.jmetal.auto.observable.Observable;
+import org.uma.jmetal.auto.observer.Observer;
+import org.uma.jmetal.util.JMetalLogger;
+
+import java.util.Map;
+
+/**
+ * This observer prints the current evaluation number of an algorithm. It requires a pair
+ * (EVALUATIONS, int) in the map used in the update() method.
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class EvaluationObserver implements Observer<Map<String, Object>> {
+
+  private Integer frequency ;
+
+  /**
+   * Constructor
+   * value.
+   */
+
+  public EvaluationObserver(Integer frequency) {
+    this.frequency = frequency ;
+  }
+
+  public EvaluationObserver() {
+    this(1) ;
+  }
+
+  /**
+   * This method gets the evaluation number from the dada map and prints it in the screen.
+   * @param data Map of pairs (key, value)
+   */
+  @Override
+  public void update(Observable<Map<String, Object>> observable, Map<String, Object> data) {
+    Integer evaluations = (Integer)data.get("EVALUATIONS") ;
+
+    if (evaluations!=null) {
+      if (evaluations % frequency == 0) {
+        System.out.println("Evaluations: " + evaluations);
+      }
+    } else {
+      JMetalLogger.logger.warning(getClass().getName()+
+          ": The algorithm has not registered yet any info related to the EVALUATIONS key");
+    }
+  }
+
+  @Override
+  public String getName() {
+    return "Evaluation observer";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Observer of the number of evaluations of an algorithm";
+  }
+}
