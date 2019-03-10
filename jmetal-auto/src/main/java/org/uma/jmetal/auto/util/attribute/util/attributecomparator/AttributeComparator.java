b9d2f5ef44ae33c3addb3ed76bb1b71d78e65d6b
ajnebro <antonio@lcc.uma.es>
2019-03-10 11:05:12 +0100

    Add package auto.util.attribute

---
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/util/attribute/util/attributecomparator/AttributeComparator.java
@@ -0,0 +1,34 @@
+package org.uma.jmetal.auto.util.attribute.util.attributecomparator;
+
+import org.uma.jmetal.auto.util.checking.Checker;
+import org.uma.jmetal.solution.Solution;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * Compares two solutions according to an attribute value, which must be a double number. The higher
+ * the value the better.
+ *
+ * @author Antonio J. Nebro
+ */
+@SuppressWarnings("serial")
+public abstract class AttributeComparator<S extends Solution<?>> implements Comparator<S>, Serializable {
+  public enum Ordering {
+    DESCENDING,
+    ASCENDING
+  };
+
+  protected final String attributeName;
+  protected Checker checker = new Checker();
+  protected Ordering ordering;
+
+  public AttributeComparator(String name, Ordering ordering) {
+    this.attributeName = name;
+    this.ordering = ordering;
+  }
+
+  public AttributeComparator(String name) {
+    this(name, Ordering.DESCENDING);
+  }
+}
