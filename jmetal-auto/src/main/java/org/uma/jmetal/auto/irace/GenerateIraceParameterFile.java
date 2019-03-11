e77fede581d4e5d4a3f7673173f3b3cbfca1713e
ajnebro <ajnebro@outlook.com>
2019-03-11 13:01:05 +0100

    Improve output of class GenerateIraceParameterFile

--- jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/GenerateIraceParameterFile.java
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/GenerateIraceParameterFile.java
@@ -10,24 +10,48 @@ import java.util.List;
 
 public class GenerateIraceParameterFile {
   public static void main(String[] args) {
-    List<Parameter> parameterList = new ArrayList<>() ;
-    parameterList.add(new CrossoverParameter()) ;
-    parameterList.add(new MutationParameter()) ;
-    parameterList.add(new SelectionParameter()) ;
-    parameterList.add(new Parameter(
-        "offspringPopulationSize",
-        "--offspringPopulationSize",
-        ParameterType.i,
-        "(1, 400)",
-        "",
-        Collections.emptyList())) ;
+    List<Parameter> parameterList = new ArrayList<>();
+    parameterList.add(new CrossoverParameter());
+    parameterList.add(new MutationParameter());
+    parameterList.add(new SelectionParameter());
+    parameterList.add(
+        new Parameter(
+            "offspringPopulationSize",
+            "--offspringPopulationSize",
+            ParameterType.i,
+            "(1, 400)",
+            "",
+            Collections.emptyList()));
 
+    String formatString = "%40s %40s %7s %30s %20s\n" ;
     for (Parameter parameter : parameterList) {
-      System.out.println(parameter) ;
+      System.out.format(
+          formatString,
+          parameter.getName(),
+          parameter.getSwitch(),
+          parameter.getType(),
+          parameter.getValidValues(),
+          parameter.getConditionalParameters());
 
       for (Parameter relatedParameter : parameter.getAssociatedParameters()) {
-        System.out.println(relatedParameter) ;
+        System.out.format(
+            formatString,
+            relatedParameter.getName(),
+            relatedParameter.getSwitch(),
+            relatedParameter.getType(),
+            relatedParameter.getValidValues(),
+            relatedParameter.getConditionalParameters());
       }
     }
   }
 }
+/*
+System.out.printf("%10s %30s %20s %5s %5s", "STUDENT ID", "EMAIL ID", "NAME", "AGE", "GRADE");
+    System.out.println();
+    System.out.println("-----------------------------------------------------------------------------");
+    for(Student student: students){
+        System.out.format("%10s %30s %20s %5d %5c",
+                student.getId(), student.getEmailId(), student.getName(), student.getAge(), student.getGrade());
+        System.out.println();
+    }
+ */
