6b5704f1f93ba5272d3fc1ab6cbf5ce0d3e5a395
ajnebro <antonio@lcc.uma.es>
2019-06-25 17:00:16 +0200

    Add unit tests for AutoNSGAII

--- jmetal-auto/src/main/java/org/uma/jmetal/auto/algorithm/nsgaii/SteadyStateNSGAIIWithParameters.java
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/algorithm/nsgaii/SteadyStateNSGAIIWithParameters.java
@@ -1,37 +1,11 @@
 package org.uma.jmetal.auto.algorithm.nsgaii;
 
 import org.uma.jmetal.auto.algorithm.EvolutionaryAlgorithm;
-import org.uma.jmetal.auto.component.evaluation.Evaluation;
-import org.uma.jmetal.auto.component.evaluation.impl.SequentialEvaluation;
-import org.uma.jmetal.auto.component.initialsolutionscreation.InitialSolutionsCreation;
-import org.uma.jmetal.auto.component.replacement.Replacement;
-import org.uma.jmetal.auto.component.replacement.impl.RankingAndDensityEstimatorReplacement;
-import org.uma.jmetal.auto.component.selection.MatingPoolSelection;
-import org.uma.jmetal.auto.component.termination.Termination;
-import org.uma.jmetal.auto.component.termination.impl.TerminationByEvaluations;
-import org.uma.jmetal.auto.component.variation.Variation;
-import org.uma.jmetal.auto.parameter.IntegerParameter;
-import org.uma.jmetal.auto.parameter.Parameter;
-import org.uma.jmetal.auto.parameter.RealParameter;
-import org.uma.jmetal.auto.parameter.catalogue.*;
-import org.uma.jmetal.auto.util.densityestimator.DensityEstimator;
-import org.uma.jmetal.auto.util.densityestimator.impl.CrowdingDistanceDensityEstimator;
-import org.uma.jmetal.auto.util.observer.impl.ExternalArchiveObserver;
-import org.uma.jmetal.auto.util.ranking.Ranking;
-import org.uma.jmetal.auto.util.ranking.impl.DominanceRanking;
-import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
-import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
-import org.uma.jmetal.util.comparator.DominanceComparator;
-import org.uma.jmetal.util.comparator.MultiComparator;
 import org.uma.jmetal.util.fileoutput.SolutionListOutput;
 import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class NSGAIIWithParameters {
+public class SteadyStateNSGAIIWithParameters {
 
   public static void main(String[] args) {
     String[] parameters =
@@ -40,7 +14,7 @@ public class NSGAIIWithParameters {
                 + "--maximumNumberOfEvaluations 25000 "
                 + "--algorithmResult population "
                 + "--populationSize 100 "
-                + "--offspringPopulationSize 100 "
+                + "--offspringPopulationSize 1 "
                 + "--createInitialSolutions random "
                 + "--variation crossoverAndMutationVariation "
                 + "--selection tournament "
--- jmetal-auto/src/test/org/uma/jmetal/auto/algorithm/nsgaii/AutoNSGAIITest.java
+++ jmetal-auto/src/test/org/uma/jmetal/auto/algorithm/nsgaii/AutoNSGAIITest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 
 public class AutoNSGAIITest {
   @Test
-  public void shouldNSGAIIWithStandardSettingsSolveTheZDT1() {
+  public void shouldNSGAIIWithStandardSettingsSolveTheZDT1Problem() {
     String[] parameters =
         ("--problemName org.uma.jmetal.problem.multiobjective.zdt.ZDT1 "
                 + "--referenceFrontFileName ZDT1.pf "
@@ -30,6 +30,39 @@ public class AutoNSGAIITest {
                 + "--mutationProbability 0.01 "
                 + "--mutationRepairStrategy bounds "
                 + "--polynomialMutationDistributionIndex 20.0 ")
+            .split("\\s+");
+    AutoNSGAII autoNSGAII = new AutoNSGAII();
+    autoNSGAII.parseParameters(parameters);
+
+    EvolutionaryAlgorithm<DoubleSolution> nsgaII = autoNSGAII.create();
+    nsgaII.run();
+
+    List<DoubleSolution> result = nsgaII.getResult() ;
+    assertEquals(100, result.size());
+  }
+
+  @Test
+  public void shouldNSGAIIWithAnExternalArchiveSolveTheZDT4Problem() {
+    String[] parameters =
+        ("--problemName org.uma.jmetal.problem.multiobjective.zdt.ZDT4 "
+            + "--referenceFrontFileName ZDT4.pf "
+            + "--maximumNumberOfEvaluations 25000 "
+            + "--algorithmResult externalArchive "
+            + "--populationSizeWithArchive 100 "
+            + "--populationSize 100 "
+            + "--offspringPopulationSize 100 "
+            + "--createInitialSolutions random "
+            + "--variation crossoverAndMutationVariation "
+            + "--selection tournament "
+            + "--selectionTournamentSize 2 "
+            + "--crossover SBX "
+            + "--crossoverProbability 0.9 "
+            + "--crossoverRepairStrategy bounds "
+            + "--sbxDistributionIndex 20.0 "
+            + "--mutation polynomial "
+            + "--mutationProbability 0.01 "
+            + "--mutationRepairStrategy bounds "
+            + "--polynomialMutationDistributionIndex 20.0 ")
             .split("\\s+");
     AutoNSGAII autoNSGAII = new AutoNSGAII();
     autoNSGAII.parseParameters(parameters);
