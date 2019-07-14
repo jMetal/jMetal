a7aae348896c8bc303db9120c46a81ee0175d7c8
ajnebro <antonio@lcc.uma.es>
2019-07-14 16:15:58 +0200

    Refactor class DifferentialEvolutionSelection

--- jmetal-core/src/main/java/org/uma/jmetal/operator/selection/impl/DifferentialEvolutionSelection.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/selection/impl/DifferentialEvolutionSelection.java
@@ -1,8 +1,10 @@
 package org.uma.jmetal.operator.selection.impl;
 
+import com.sun.tools.doclint.Checker;
 import org.uma.jmetal.operator.selection.SelectionOperator;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
 import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.checking.Check;
 import org.uma.jmetal.util.pseudorandom.BoundedRandomGenerator;
 import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
@@ -10,62 +12,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class implementing the selection operator used in DE: three different solutions
- * are returned from a population. The three solutions must be also different from the one
- * indicated by an index (its position in the list). As a consequence, the operator requires a
- * solution list with at least for elements.
+ * Class implementing the selection operator used in DE: three different solutions are returned from
+ * a population. The three solutions must be also different from the one indicated by an index (its
+ * position in the list). As a consequence, the operator requires a solution list with at least for
+ * elements.
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  * @author Juan J. Durillo
  */
 @SuppressWarnings("serial")
-public class DifferentialEvolutionSelection implements SelectionOperator<List<DoubleSolution>,List<DoubleSolution>> {
-  private int solutionListIndex = Integer.MIN_VALUE ;
-  private BoundedRandomGenerator<Integer> randomGenerator ;
+public class DifferentialEvolutionSelection
+    implements SelectionOperator<List<DoubleSolution>, List<DoubleSolution>> {
+  private int solutionListIndex = Integer.MIN_VALUE;
+  private BoundedRandomGenerator<Integer> randomGenerator;
+  private int numberOfSolutionsToSelect;
 
   /** Constructor */
   public DifferentialEvolutionSelection() {
-	  this((a, b) -> JMetalRandom.getInstance().nextInt(a, b));
+    this(3);
   }
 
   /** Constructor */
-  public DifferentialEvolutionSelection(BoundedRandomGenerator<Integer> randomGenerator) {
-    this.randomGenerator = randomGenerator ;
+  public DifferentialEvolutionSelection(int numberOfSolutionsToSelect) {
+    this((a, b) -> JMetalRandom.getInstance().nextInt(a, b), numberOfSolutionsToSelect);
+  }
+
+  /** Constructor */
+  public DifferentialEvolutionSelection(
+      BoundedRandomGenerator<Integer> randomGenerator, int numberOfSolutionsToSelect) {
+    this.randomGenerator = randomGenerator;
+    this.numberOfSolutionsToSelect = numberOfSolutionsToSelect;
   }
 
   public void setIndex(int index) {
-    this.solutionListIndex = index ;
+    this.solutionListIndex = index;
   }
 
-  /** Execute() method  */
+  /** Execute() method */
   @Override
-  public List<DoubleSolution> execute(List<DoubleSolution> solutionSet) {
-    if (null == solutionSet) {
-      throw new JMetalException("Parameter is null") ;
-    } else if ((solutionListIndex < 0) || (solutionListIndex > solutionSet.size())) {
-      throw new JMetalException(
-              "Index value invalid: " + solutionListIndex );
-    } else if (solutionSet.size() < 4) {
-      throw new JMetalException(
-          "The population has less than four solutions: " + solutionSet.size());
+  public List<DoubleSolution> execute(List<DoubleSolution> solutionList) {
+    Check.isNotNull(solutionList);
+    Check.isTrue(
+        (solutionListIndex >= 0) && (solutionListIndex <= solutionList.size()),
+        "Index value invalid: " + solutionListIndex);
+    Check.isTrue(
+        solutionList.size() >= numberOfSolutionsToSelect,
+        "The population has less than " + numberOfSolutionsToSelect + " solution: " + solutionList.size());
+
+    List<Integer> indexList = new ArrayList<>(numberOfSolutionsToSelect);
+
+    do {
+      int index = randomGenerator.getRandomValue(0, solutionList.size() - 1);
+      if (index != solutionListIndex && !indexList.contains(index)) {
+        indexList.add(index);
+      }
+    } while (indexList.size() < numberOfSolutionsToSelect);
+
+    List<DoubleSolution> parents = new ArrayList<>();
+
+    for (Integer index : indexList) {
+      parents.add(solutionList.get(index));
     }
-
-    List<DoubleSolution> parents = new ArrayList<>(3);
-    int r1, r2, r3;
-
-    do {
-      r1 = randomGenerator.getRandomValue(0, solutionSet.size() - 1);
-    } while (r1 == solutionListIndex);
-    do {
-      r2 = randomGenerator.getRandomValue(0, solutionSet.size() - 1);
-    } while (r2 == solutionListIndex || r2 == r1);
-    do {
-      r3 = randomGenerator.getRandomValue(0, solutionSet.size() - 1);
-    } while (r3 == solutionListIndex || r3 == r1 || r3 == r2);
-
-    parents.add(solutionSet.get(r1));
-    parents.add(solutionSet.get(r2));
-    parents.add(solutionSet.get(r3));
 
     return parents;
   }
--- jmetal-core/src/test/java/org/uma/jmetal/operator/impl/selection/DifferentialEvolutionSelectionTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/operator/impl/selection/DifferentialEvolutionSelectionTest.java
@@ -20,24 +20,21 @@ import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
-/**
- * Created by ajnebro on 3/5/15.
- */
+/** Created by ajnebro on 3/5/15. */
 public class DifferentialEvolutionSelectionTest {
-  private DifferentialEvolutionSelection selection ;
-  private List<DoubleSolution> population ;
+  private DifferentialEvolutionSelection selection;
+  private List<DoubleSolution> population;
 
-  @Rule
-  public ExpectedException exception = ExpectedException.none();
+  @Rule public ExpectedException exception = ExpectedException.none();
 
   @Test
   public void shouldExecuteRaiseAnExceptionIfTheListOfSolutionsIsNull() {
     exception.expect(JMetalException.class);
     exception.expectMessage(containsString("Parameter is null"));
 
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
 
-    selection.execute(null) ;
+    selection.execute(null);
   }
 
   @Test
@@ -45,12 +42,12 @@ public class DifferentialEvolutionSelectionTest {
     exception.expect(JMetalException.class);
     exception.expectMessage(containsString("The population has less than four solutions: " + 1));
 
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
     selection.setIndex(0);
 
-    population = Arrays.asList(mock(DoubleSolution.class)) ;
+    population = Arrays.asList(mock(DoubleSolution.class));
 
-    selection.execute(population) ;
+    selection.execute(population);
   }
 
   @Test
@@ -58,12 +55,12 @@ public class DifferentialEvolutionSelectionTest {
     exception.expect(JMetalException.class);
     exception.expectMessage(containsString("The population has less than four solutions: " + 0));
 
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
     selection.setIndex(0);
 
     population = Collections.emptyList();
 
-    selection.execute(population) ;
+    selection.execute(population);
   }
 
   @Test
@@ -71,13 +68,14 @@ public class DifferentialEvolutionSelectionTest {
     exception.expect(JMetalException.class);
     exception.expectMessage(containsString("Index value invalid: " + -1));
 
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
     selection.setIndex(-1);
 
-    population = Arrays.asList(mock(DoubleSolution.class), mock(DoubleSolution.class),
-        mock(DoubleSolution.class)) ;
+    population =
+        Arrays.asList(
+            mock(DoubleSolution.class), mock(DoubleSolution.class), mock(DoubleSolution.class));
 
-    selection.execute(population) ;
+    selection.execute(population);
   }
 
   @Test
@@ -85,11 +83,13 @@ public class DifferentialEvolutionSelectionTest {
     exception.expect(JMetalException.class);
     exception.expectMessage(containsString("Index value invalid: " + Integer.MIN_VALUE));
 
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
 
-    population = Arrays.asList(mock(DoubleSolution.class), mock(DoubleSolution.class), mock(DoubleSolution.class)) ;
+    population =
+        Arrays.asList(
+            mock(DoubleSolution.class), mock(DoubleSolution.class), mock(DoubleSolution.class));
 
-    selection.execute(population) ;
+    selection.execute(population);
   }
 
   @Test
@@ -97,69 +97,106 @@ public class DifferentialEvolutionSelectionTest {
     exception.expect(JMetalException.class);
     exception.expectMessage(containsString("Index value invalid: " + 5));
 
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
     selection.setIndex(5);
 
-    population = Arrays.asList(mock(DoubleSolution.class), mock(DoubleSolution.class), mock(DoubleSolution.class)) ;
+    population =
+        Arrays.asList(
+            mock(DoubleSolution.class), mock(DoubleSolution.class), mock(DoubleSolution.class));
 
-    selection.execute(population) ;
+    selection.execute(population);
   }
 
   @Test
   public void shouldExecuteReturnThreeDifferentSolutionsIfTheListHasFourElements() {
-    selection = new DifferentialEvolutionSelection() ;
+    selection = new DifferentialEvolutionSelection();
     selection.setIndex(1);
 
-    population = Arrays.asList(mock(DoubleSolution.class), mock(DoubleSolution.class),
-        mock(DoubleSolution.class), mock(DoubleSolution.class)) ;
+    population =
+        Arrays.asList(
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class));
 
-    List<DoubleSolution> parents = selection.execute(population) ;
-    assertEquals(3, parents.size()) ;
+    List<DoubleSolution> parents = selection.execute(population);
+    assertEquals(3, parents.size());
 
     // The index solution must not be in the result
     assertNotSame(population.get(1), parents.get(0));
     assertNotSame(population.get(1), parents.get(1));
     assertNotSame(population.get(1), parents.get(2));
 
-    assertThat(parents, hasItem(population.get(0))) ;
-    assertThat(parents, hasItem(population.get(2))) ;
-    assertThat(parents, hasItem(population.get(3))) ;
-    assertThat(parents, not(hasItem(population.get(1)))) ;
+    assertThat(parents, hasItem(population.get(0)));
+    assertThat(parents, hasItem(population.get(2)));
+    assertThat(parents, hasItem(population.get(3)));
+    assertThat(parents, not(hasItem(population.get(1))));
   }
-  
-	@Test
-	public void shouldJMetalRandomGeneratorNotBeUsedWhenCustomRandomGeneratorProvided() {
-		// Configuration
-		List<DoubleSolution> solutions = Arrays.asList(mock(DoubleSolution.class), mock(DoubleSolution.class),
-        mock(DoubleSolution.class), mock(DoubleSolution.class));
 
-		// Check configuration leads to use default generator by default
-		final int[] defaultUses = { 0 };
-		JMetalRandom defaultGenerator = JMetalRandom.getInstance();
-		AuditableRandomGenerator auditor = new AuditableRandomGenerator(defaultGenerator.getRandomGenerator());
-		defaultGenerator.setRandomGenerator(auditor);
-		auditor.addListener((a) -> defaultUses[0]++);
+  @Test
+  public void shouldExecuteReturnTwoDifferentSolutionsIfTheListHasThreeElements() {
+    selection = new DifferentialEvolutionSelection(2);
+    selection.setIndex(1);
 
-		DifferentialEvolutionSelection selection = new DifferentialEvolutionSelection();
-		selection.setIndex(1);
-		selection.execute(solutions);
-		assertTrue("No use of the default generator", defaultUses[0] > 0);
+    population =
+        Arrays.asList(
+            mock(DoubleSolution.class), mock(DoubleSolution.class), mock(DoubleSolution.class));
 
-		// Test same configuration uses custom generator instead
-		solutions = Arrays.asList(mock(DoubleSolution.class), mock(DoubleSolution.class),
-        mock(DoubleSolution.class), mock(DoubleSolution.class));
-		defaultUses[0] = 0;
-		
-		final int[] customUses = { 0 };
-		selection = new DifferentialEvolutionSelection((a, b) -> {
-			customUses[0]++;
-			return new Random().nextInt(b+1-a)+a;
-		});
-		selection.setIndex(1);
-		selection.execute(solutions);
-		assertTrue("Default random generator used", defaultUses[0] == 0);
-		assertTrue("No use of the custom generator", customUses[0] > 0);
-	}
+    List<DoubleSolution> parents = selection.execute(population);
+    assertEquals(2, parents.size());
+
+    // The index solution must not be in the result
+    assertNotSame(population.get(1), parents.get(0));
+    assertNotSame(population.get(1), parents.get(1));
+
+    assertThat(parents, hasItem(population.get(0)));
+    assertThat(parents, hasItem(population.get(2)));
+    assertThat(parents, not(hasItem(population.get(1))));
+  }
+
+  @Test
+  public void shouldJMetalRandomGeneratorNotBeUsedWhenCustomRandomGeneratorProvided() {
+    // Configuration
+    List<DoubleSolution> solutions =
+        Arrays.asList(
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class));
+
+    // Check configuration leads to use default generator by default
+    final int[] defaultUses = {0};
+    JMetalRandom defaultGenerator = JMetalRandom.getInstance();
+    AuditableRandomGenerator auditor =
+        new AuditableRandomGenerator(defaultGenerator.getRandomGenerator());
+    defaultGenerator.setRandomGenerator(auditor);
+    auditor.addListener((a) -> defaultUses[0]++);
+
+    DifferentialEvolutionSelection selection = new DifferentialEvolutionSelection();
+    selection.setIndex(1);
+    selection.execute(solutions);
+    assertTrue("No use of the default generator", defaultUses[0] > 0);
+
+    // Test same configuration uses custom generator instead
+    solutions =
+        Arrays.asList(
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class),
+            mock(DoubleSolution.class));
+    defaultUses[0] = 0;
+
+    final int[] customUses = {0};
+    selection =
+        new DifferentialEvolutionSelection(
+            (a, b) -> {
+              customUses[0]++;
+              return new Random().nextInt(b + 1 - a) + a;
+            },
+            3);
+    selection.setIndex(1);
+    selection.execute(solutions);
+    assertTrue("Default random generator used", defaultUses[0] == 0);
+    assertTrue("No use of the custom generator", customUses[0] > 0);
+  }
 }
-
-
