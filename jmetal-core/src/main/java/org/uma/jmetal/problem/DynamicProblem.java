8dc1bded322d0e6ef80db2212a7521424b7733e3
ajnebro <antonio@lcc.uma.es>
2019-07-15 13:17:25 +0200

    Minor changes

--- jmetal-core/src/main/java/org/uma/jmetal/problem/DynamicProblem.java
+++ jmetal-core/src/main/java/org/uma/jmetal/problem/DynamicProblem.java
@@ -1,10 +1,16 @@
 package org.uma.jmetal.problem;
 
 import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.observable.Observable;
 import org.uma.jmetal.util.observer.Observer;
 
 /**
- * Created by ajnebro on 18/4/16.
+ * Interface representing dynamic problems. Problems implementing this interface act as {@link Observer} entities
+ * in the sense that the can register themselves in {@link Observable} objects that may modify some of the problem
+ * properties. A dynamic problem has an internal state about whether it has been updated or not, and there are methods
+ * to query, set, and reset that state.
+ *
+ * @author Antonio J. Nebro <ajnebro@uma.es>
  */
 public interface DynamicProblem<S extends Solution<?>, D>
     extends Problem<S>, Observer<D> {
