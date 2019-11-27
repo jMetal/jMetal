56dee3e4287c95c247e5d6c5b8e9fc661c75265f
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-11-28 02:03:13 +0300

    Upgrade PointSolution to have both objectives and constraints.

--- jmetal-core/src/main/java/org/uma/jmetal/util/point/PointSolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/point/PointSolution.java
@@ -5,79 +5,75 @@ import org.uma.jmetal.solution.Solution;
 import java.util.*;
 
 /**
- * Solution used to wrap a {@link Point} object. Only objectives are used.
+ * This is a simple solution which has only objectives, constraints and properties, but no variables.
  *
  * @author Antonio J. Nebro
  */
 @SuppressWarnings("serial")
 public class PointSolution implements Solution<Double> {
-  private int numberOfObjectives ;
   private double[] objectives;
+  private double[] constraints;
   protected Map<Object, Object> attributes ;
 
   /**
-   * Constructor
+   * Creates a solution with the given numbers of objectives and constraints,
+   * all initialized to zeros.
    *
-   * @param numberOfObjectives
+   * @param numberOfObjectives the number of objectives.
+   * @param numberOfConstraints the number of constraints.
+   */
+  public PointSolution(int numberOfObjectives, int numberOfConstraints) {
+    this.objectives = new double[numberOfObjectives];
+    this.constraints = new double[numberOfConstraints];
+    this.attributes = new HashMap<>() ;
+  }
+
+  /**
+   * Creates a solution with the given numbers of objectives,
+   * all initialized to zeros, and no constraints.
+   *
+   * @param numberOfObjectives the number of objectives.
    */
   public PointSolution(int numberOfObjectives) {
-    this.numberOfObjectives = numberOfObjectives ;
-    objectives = new double[numberOfObjectives] ;
-    attributes = new HashMap<>() ;
+    this(numberOfObjectives, 0);
   }
 
   /**
-   * Constructor
+   * Creates a solution, whose objectives are equal to the given point,
+   * and no constraints.
    *
-   * @param point
+   * @param point the point to copy.
    */
   public PointSolution(Point point) {
-    this.numberOfObjectives = point.getDimension() ;
-    objectives = new double[numberOfObjectives] ;
-
-    for (int i = 0; i < numberOfObjectives; i++) {
-      this.objectives[i] = point.getValue(i) ;
-    }
+    this.objectives = point.getValues().clone();
+    this.constraints = new double[0];
+    this.attributes = new HashMap<>();
   }
 
   /**
-   * Constructor
+   * Creates a copy of the given solution.
    *
-   * @param solution
+   * @param solution the solution to copy.
    */
   public PointSolution(Solution<?> solution) {
-    this.numberOfObjectives = solution.getNumberOfObjectives() ;
-    objectives = new double[numberOfObjectives] ;
-
-    for (int i = 0; i < numberOfObjectives; i++) {
-      this.objectives[i] = solution.getObjective(i) ;
-    }
+    this.objectives = solution.getObjectives().clone();
+    this.constraints = solution.getConstraints().clone();
+    this.attributes = new HashMap<>(solution.getAttributes());
   }
 
-  /**
-   * Copy constructor
-   *
-   * @param point
-   */
-  public PointSolution(PointSolution point) {
-    this(point.getNumberOfObjectives()) ;
-
-    for (int i = 0; i < numberOfObjectives; i++) {
-      this.objectives[i] = point.getObjective(i) ;
-    }
+  @Override
+  public void setObjective(int index, double value) {
+    objectives[index] = value;
   }
 
-  @Override public void setObjective(int index, double value) {
-    objectives[index]=  value ;
-  }
-
-  @Override public double getObjective(int index) {
+  @Override
+  public double getObjective(int index) {
     return objectives[index];
   }
 
   @Override
   public double[] getObjectives() {
-    return objectives ;
+    return objectives;
   }
 
   @Override
@@ -95,17 +91,17 @@ public class PointSolution implements Solution<Double> {
 
   @Override
   public double[] getConstraints() {
-    return new double[0];
+    return constraints;
   }
 
   @Override
   public double getConstraint(int index) {
-    return 0;
+    return constraints[index];
   }
 
   @Override
   public void setConstraint(int index, double value) {
-
+    constraints[index] = value;
   }
 
   @Override public int getNumberOfVariables() {
@@ -113,32 +109,36 @@ public class PointSolution implements Solution<Double> {
   }
 
   @Override public int getNumberOfObjectives() {
-    return numberOfObjectives;
+    return objectives.length;
   }
 
   @Override
   public int getNumberOfConstraints() {
-    return 0;
+    return constraints.length;
   }
 
-  @Override public PointSolution copy() {
+  @Override
+  public PointSolution copy() {
     return new PointSolution(this);
   }
 
-  @Override public void setAttribute(Object id, Object value) {
-    attributes.put(id, value) ;
+  @Override
+  public void setAttribute(Object id, Object value) {
+    attributes.put(id, value);
   }
 
-  @Override public Object getAttribute(Object id) {
+  @Override
+  public Object getAttribute(Object id) {
     return attributes.get(id);
   }
 
   @Override
   public boolean hasAttribute(Object id) {
-    return false;
+    return attributes.containsKey(id);
   }
 
-  @Override public boolean equals(Object o) {
+  @Override
+  public boolean equals(Object o) {
     if (this == o)
       return true;
     if (o == null || getClass() != o.getClass())
@@ -146,25 +146,23 @@ public class PointSolution implements Solution<Double> {
 
     PointSolution that = (PointSolution) o;
 
-    if (numberOfObjectives != that.numberOfObjectives)
-      return false;
-    if (!Arrays.equals(objectives, that.objectives))
-      return false;
-
-    return true;
+    return Arrays.equals(objectives, that.objectives)
+            && Arrays.equals(constraints, that.constraints)
+            && attributes.equals(that.attributes);
   }
 
-  @Override public int hashCode() {
-    return Arrays.hashCode(objectives);
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(objectives) ^ Arrays.hashCode(constraints) ^ attributes.hashCode();
   }
   
   @Override
-	public String toString() {
-		return Arrays.toString(objectives);
-	}
+  public String toString() {
+	return Arrays.toString(objectives);
+  }
 
-	@Override
-	public Map<Object, Object> getAttributes() {
-		return attributes;
-	}
+  @Override
+  public Map<Object, Object> getAttributes() {
+	return attributes;
+  }
 }
--- jmetal-core/src/test/java/org/uma/jmetal/util/point/impl/PointSolutionTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/util/point/impl/PointSolutionTest.java
@@ -1,86 +1,77 @@
 package org.uma.jmetal.util.point.impl;
 
 import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.uma.jmetal.util.point.PointSolution;
-
-import java.util.Arrays;
-
 import static org.junit.Assert.*;
 
+import org.uma.jmetal.util.point.PointSolution;
 /**
  * @author Antonio J. Nebro
  * @version 1.0
  */
 public class PointSolutionTest {
-  private static double EPSILON = 0.0000001 ;
-
-  @Test public void shouldDefaultConstructorCreateTheObjectCorrectly() {
-    int numberOfObjectives = 10 ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-
-    assertEquals(numberOfObjectives, ReflectionTestUtils.getField(solution, "numberOfObjectives"));
-    assertNotNull(ReflectionTestUtils.getField(solution, "objectives")) ;
+  @Test
+  public void shouldDefaultConstructorCreateTheObjectCorrectly() {
+    int numberOfObjectives = 10;
+    PointSolution solution = new PointSolution(numberOfObjectives);
+    assertEquals(numberOfObjectives, solution.getNumberOfObjectives());
+    assertEquals(0, solution.getNumberOfConstraints());
+    for (int i = 0; i < numberOfObjectives; ++i) {
+      assertEquals(0.0, solution.getObjective(i), 0);
+    }
+    assertEquals(numberOfObjectives, solution.getObjectives().length);
+    assertEquals(0, solution.getConstraints().length);
+    assertArrayEquals(new double[numberOfObjectives], solution.getObjectives(), 0);
+    assertArrayEquals(new double[0], solution.getConstraints(), 0);
   }
 
-  @Test public void shouldCopyConstructorCreateAnIdenticalObject() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
+  @Test
+  public void shouldCopyConstructorCreateAnIdenticalObject() {
+    PointSolution solution = new PointSolution(3, 2);
+    solution.setObjective(0, 1);
+    solution.setObjective(1, 2);
+    solution.setObjective(2, 3);
+    solution.setConstraint(0, 4);
+    solution.setConstraint(1, 5);
 
     PointSolution newSolution = new PointSolution(solution) ;
+    assertEquals(solution.getNumberOfObjectives(), newSolution.getNumberOfObjectives());
+    assertEquals(solution.getNumberOfConstraints(), newSolution.getNumberOfConstraints());
 
-    assertEquals(numberOfObjectives, ReflectionTestUtils.getField(newSolution, "numberOfObjectives"));
-
-    double [] resultValues = (double [])ReflectionTestUtils.getField(solution, "objectives") ;
-    assertArrayEquals(values, resultValues, EPSILON) ;
+    assertArrayEquals(solution.getObjectives(), newSolution.getObjectives(), 0);
+    assertArrayEquals(solution.getConstraints(), newSolution.getConstraints(), 0);
+    assertNotSame(solution.getObjectives(), newSolution.getObjectives());
+    assertNotSame(solution.getConstraints(), newSolution.getConstraints());
   }
 
-  @Test public void shouldGetObjectiveReturnTheCorrectValue() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
-
-    assertEquals(values[0], solution.getObjective(0), EPSILON) ;
-    assertEquals(values[1], solution.getObjective(1), EPSILON) ;
-    assertEquals(values[2], solution.getObjective(2), EPSILON) ;
+  @Test
+  public void shouldGetObjectiveReturnTheCorrectValue() {
+    PointSolution solution = new PointSolution(3, 2);
+    solution.setObjective(0, 1);
+    solution.setObjective(1, 2);
+    solution.setObjective(2, 3);
+    solution.setConstraint(0, 4);
+    solution.setConstraint(1, 5);
+    assertEquals(1.0, solution.getObjective(0), 0);
+    assertEquals(2.0, solution.getObjective(1), 0);
+    assertEquals(3.0, solution.getObjective(2), 0);
+    assertEquals(4.0, solution.getConstraint(0), 0);
+    assertEquals(5.0, solution.getConstraint(1), 0);
   }
 
-  @Test public void shouldSetObjectiveAssignTheTheCorrectValue() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
+  @Test
+  public void shouldCopyReturnACopyOfTheSolution() {
+    PointSolution solution = new PointSolution(3);
+    solution.setObjective(0, 1);
+    solution.setObjective(1, 2);
+    solution.setObjective(2, 3);
 
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    solution.setObjective(0, values[0]);
-    solution.setObjective(1, values[1]);
-    solution.setObjective(2, values[2]);
-
-    double [] resultValues = (double [])ReflectionTestUtils.getField(solution, "objectives") ;
-    assertArrayEquals(values, resultValues, EPSILON) ;
+    PointSolution newSolution = solution.copy();
+    assertEquals(solution, newSolution);
+    assertNotSame(solution, newSolution);
   }
 
-  @Test public void shouldGetNumberOfObjectivesReturnTheCorrectValue() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
-
-    assertEquals(numberOfObjectives, solution.getNumberOfObjectives()) ;
-  }
-
-  @Test public void shouldCopyReturnACopyOfTheSolution() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
-
-    PointSolution newSolution = (PointSolution)solution.copy() ;
-    assertEquals(solution, newSolution) ;
-  }
-
-  @Test public void idleTestToCoverTheUnusedMethods() {
+  @Test
+  public void testVariablesAreGracefullyUnsupported() {
     PointSolution solution = new PointSolution(3) ;
 
     solution.setVariable(0, 0.0);
@@ -94,89 +85,74 @@ public class PointSolutionTest {
 
   @Test
   public void shouldEqualsReturnTrueIfTheSolutionsAreIdentical() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
+    PointSolution solutionA = new PointSolution(3);
+    solutionA.setObjective(0, 1);
+    solutionA.setObjective(1, 2);
+    solutionA.setObjective(2, 3);
 
-    PointSolution newSolution = (PointSolution)solution.copy() ;
-    assertTrue(solution.equals(newSolution));
+    PointSolution solutionB = new PointSolution(3);
+    solutionB.setObjective(0, 1);
+    solutionB.setObjective(1, 2);
+    solutionB.setObjective(2, 3);
+
+    assertEquals(solutionA, solutionB);
   }
 
   @Test
   public void shouldEqualsReturnFalseIfTheTwoSolutionsHaveDifferentNumberOfObjectives() {
-    PointSolution solution1 = new PointSolution(10) ;
-    PointSolution solution2 = new PointSolution(4) ;
-
-    assertFalse(solution1.equals(solution2));
+    PointSolution solution1 = new PointSolution(10);
+    PointSolution solution2 = new PointSolution(4);
+    assertNotEquals(solution1, solution2);
   }
 
   @Test
   public void shouldEqualsReturnTrueIfTheTwoPointsAreTheSame() {
-    int numberOfObjectives = 5 ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-
-    assertTrue(solution.equals(solution));
+    PointSolution solution = new PointSolution(5);
+    assertEquals(solution, solution);
   }
 
   @Test
   public void shouldEqualsReturnFalseIfThePointsAreNotIdentical() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
+    PointSolution solution = new PointSolution(3);
+    solution.setObjective(0, 1);
+    solution.setObjective(1, 2);
+    solution.setObjective(2, 3);
 
-    PointSolution newSolution = (PointSolution)solution.copy() ;
+    PointSolution newSolution = solution.copy();
     newSolution.setObjective(0, 23424);
 
-    assertFalse(solution.equals(newSolution));
+    assertNotEquals(solution, newSolution);
   }
 
+  @SuppressWarnings({"SimplifiableJUnitAssertion", "ConstantConditions"})
   @Test
   public void shouldEqualsReturnFalseIfTheSolutionIsNull() {
-    int numberOfObjectives = 3 ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-
+    PointSolution solution = new PointSolution(3) ;
     assertFalse(solution.equals(null));
   }
 
-  @SuppressWarnings("unlikely-arg-type")
+  @SuppressWarnings({"unlikely-arg-type", "SimplifiableJUnitAssertion", "EqualsBetweenInconvertibleTypes"})
   @Test
   public void shouldEqualsReturnFalseIfTheClassIsNotAPoint() {
-    int numberOfObjectives = 3 ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-
-    assertFalse(solution.equals(new String("")));
+    PointSolution solution = new PointSolution(3);
+    assertFalse(solution.equals(""));
   }
 
   @Test
-  public void shouldHashCodeReturnTheCorrectValue() {
-    int numberOfObjectives = 3 ;
-    double [] values = {1.0, 2.0, 3.0} ;
-    PointSolution solution = new PointSolution(numberOfObjectives) ;
-    ReflectionTestUtils.setField(solution, "objectives", values);
-
-    assertEquals(Arrays.hashCode(values), solution.hashCode());
+  public void shouldGetAttributesReturnAnNoAttributesWhenInitiateAnPointSolution() {
+	PointSolution solution = new PointSolution(3);
+	assertTrue(solution.getAttributes().isEmpty());
   }
-  
-	@Test
-	public void shouldGetAttributesReturnAnNoAttributesWhenInitiateAnPointSolution() {
 
-		PointSolution solution = new PointSolution(3);
+  @Test
+  public void shouldReturnTheCorrectAttributesWhenGetAllAttributes() {
+	PointSolution solution = new PointSolution(3);
 
-		assertTrue(solution.getAttributes().isEmpty());
-	}
+	solution.setAttribute("fake-attribute-1", 1);
+	solution.setAttribute("fake-attribute-2", 2);
 
-	@Test
-	public void shouldReturnTheCorrectAttributesWhenGetAllAttributes() {
-
-		PointSolution solution = new PointSolution(3);
-
-		solution.setAttribute("fake-atribute-1", 1);
-		solution.setAttribute("fake-atribute-2", 2);
-
-		assertFalse(solution.getAttributes().isEmpty());
-		assertEquals((int) solution.getAttributes().get("fake-atribute-1"), 1);
-		assertEquals((int) solution.getAttributes().get("fake-atribute-2"), 2);
-	}
+	assertFalse(solution.getAttributes().isEmpty());
+	assertEquals((int) solution.getAttributes().get("fake-attribute-1"), 1);
+	assertEquals((int) solution.getAttributes().get("fake-attribute-2"), 2);
+  }
 }
