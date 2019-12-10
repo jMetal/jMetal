3b079bd2a03e9cd8ed3c70ed4889e549ab31cc2d
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-12-10 23:58:31 +0300

    Revert the type parameter narrowing.
    Instead, the Attribute loses its type constraint.
    The only thing is that its getSolutionComparator method
    still references solutions.

--- jmetal-core/src/main/java/org/uma/jmetal/component/ranking/Ranking.java
+++ jmetal-core/src/main/java/org/uma/jmetal/component/ranking/Ranking.java
@@ -1,6 +1,5 @@
 package org.uma.jmetal.component.ranking;
 
-import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.solution.util.attribute.Attribute;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import java.util.List;
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
-public interface Ranking<S extends Solution<?>> extends Attribute<S> {
+public interface Ranking<S> extends Attribute<S> {
   Ranking<S> computeRanking(List<S> solutionList) ;
   List<S> getSubFront(int rank) ;
   int getNumberOfSubFronts() ;
--- jmetal-core/src/main/java/org/uma/jmetal/solution/util/attribute/Attribute.java
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/util/attribute/Attribute.java
@@ -1,10 +1,8 @@
 package org.uma.jmetal.solution.util.attribute;
 
-import org.uma.jmetal.solution.Solution;
-
 import java.util.Comparator;
 
-public interface Attribute<S extends Solution<?>> {
+public interface Attribute<S> {
   String getAttributeId() ;
   Comparator<S> getSolutionComparator() ;
 }
