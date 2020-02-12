710f348392a42f19f2a879a47466d9d677b18683
ajnebro <ajnebro@outlook.com>
2020-02-12 12:32:11 +0100

    Starting to work on ensembles

---
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/ensemble/ArchiveBasedAlgorithmsEnsemble.java
@@ -0,0 +1,28 @@
+package org.uma.jmetal.algorithm.multiobjective.ensemble;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+
+import java.util.List;
+
+public class ArchiveBasedAlgorithmsEnsemble implements Algorithm<List<DoubleSolution>> {
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public List<DoubleSolution> getResult() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "Ensemble";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Ensemble of multiobjective algorithms using an external archive";
+    }
+}
