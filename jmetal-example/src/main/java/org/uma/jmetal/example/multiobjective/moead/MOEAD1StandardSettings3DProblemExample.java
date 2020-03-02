1b9174a4080837f44bcb5c018441c651bf004e9d
ajnebro <ajnebro@outlook.com>
2020-03-02 17:03:34 +0100

    Working on the refactoring of MOEA/D

---
+++ jmetal-example/src/main/java/org/uma/jmetal/example/multiobjective/moead/MOEAD1StandardSettings3DProblemExample.java
@@ -0,0 +1,101 @@
+package org.uma.jmetal.example.multiobjective.moead;
+
+import org.uma.jmetal.algorithm.multiobjective.moead.MOEAD1;
+import org.uma.jmetal.component.termination.impl.TerminationByEvaluations;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
+import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.util.aggregativefunction.AggregativeFunction;
+import org.uma.jmetal.util.aggregativefunction.impl.PenaltyBoundaryIntersection;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.neighborhood.impl.WeightVectorNeighborhood;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+import org.uma.jmetal.util.sequencegenerator.SequenceGenerator;
+import org.uma.jmetal.util.sequencegenerator.impl.IntegerPermutationGenerator;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Class for configuring and running the MOEA/D-DE algorithm
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class MOEAD1StandardSettings3DProblemExample extends AbstractAlgorithmRunner {
+  /**
+   * @param args Command line arguments.
+   * @throws SecurityException Invoking command: java
+   *     org.uma.jmetal.runner.multiobjective.moead.MOEADRunner problemName [referenceFront]
+   */
+  public static void main(String[] args) throws FileNotFoundException {
+    DoubleProblem problem;
+    MOEAD1<DoubleSolution> algorithm;
+    MutationOperator<DoubleSolution> mutation;
+    CrossoverOperator<DoubleSolution> crossover;
+
+    String problemName = "org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1";
+    String referenceParetoFront = "resources/referenceFronts/DTLZ1.3D.pf";
+
+    problem = (DoubleProblem) ProblemUtils.<DoubleSolution>loadProblem(problemName);
+
+    int populationSize = 100;
+
+    SequenceGenerator<Integer> subProblemIdGenerator = new IntegerPermutationGenerator(populationSize);
+
+    crossover = new SBXCrossover(1.0, 20.0);
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+
+
+    double neighborhoodSelectionProbability = 0.9;
+    int neighborhoodSize = 20;
+    WeightVectorNeighborhood<DoubleSolution> neighborhood =
+        new WeightVectorNeighborhood<>(populationSize, neighborhoodSize);
+
+    int maximumNumberOfReplacedSolutions = 2;
+    AggregativeFunction aggregativeFunction = new PenaltyBoundaryIntersection();
+
+    algorithm =
+        new MOEAD1<>(
+            problem,
+            populationSize,
+            mutation,
+            crossover,
+            aggregativeFunction,
+            neighborhoodSelectionProbability,
+            maximumNumberOfReplacedSolutions,
+            neighborhoodSize,
+            "resources/weightVectorsFiles/moead",
+            new TerminationByEvaluations(50000));
+
+    algorithm.run();
+
+    List<DoubleSolution> population = algorithm.getResult();
+    JMetalLogger.logger.info("Total execution time : " + algorithm.getTotalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + algorithm.getEvaluations());
+
+    new SolutionListOutput(population)
+        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+        .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+
+    if (!referenceParetoFront.equals("")) {
+      printQualityIndicators(population, referenceParetoFront);
+    }
+
+    System.exit(0);
+  }
+}
