e44f0ced7179f7175bbb24bca0ed06b6aedf2290
ajnebro <ajnebro@users.noreply.github.com>
2020-03-30 15:32:57 +0200

    Minor changes

--- jmetal-core/src/main/java/org/uma/jmetal/qualityindicator/impl/NormalizedHypervolume.java
+++ jmetal-core/src/main/java/org/uma/jmetal/qualityindicator/impl/NormalizedHypervolume.java
@@ -15,6 +15,8 @@ import java.util.List;
  * Class providing an implementation of the normalized hypervolume, which is calculated as follows:
  * relative hypervolume = 1 - (HV of the front / HV of the reference front)
  *
+ * Before computing this indicator it must be checked that the HV of the reference front is not zero.
+ *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
 public class NormalizedHypervolume<S extends Solution<?>> extends GenericIndicator<S> {
