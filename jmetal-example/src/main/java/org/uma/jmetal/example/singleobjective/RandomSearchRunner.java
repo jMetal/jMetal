8700434c8a1e79e221e8cf96c979919b3a5b0ba2
ajnebro <ajnebro@users.noreply.github.com>
2020-09-03 10:25:17 +0200

    Add new example

---
+++ jmetal-example/src/main/java/org/uma/jmetal/example/singleobjective/RandomSearchRunner.java
@@ -0,0 +1,52 @@
+package org.uma.jmetal.example.singleobjective;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.multiobjective.randomsearch.RandomSearchBuilder;
+import org.uma.jmetal.example.AlgorithmRunner;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.singleobjective.OneMax;
+import org.uma.jmetal.solution.binarysolution.BinarySolution;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Class for configuring and running the random search algorithm
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+
+public class RandomSearchRunner extends AbstractAlgorithmRunner {
+  /**
+   * @param args Command line arguments.
+   * @throws SecurityException
+   * Invoking command:
+  java org.uma.jmetal.runner.multiobjective.RandomSearchRunner problemName [referenceFront]
+   */
+  public static void main(String[] args) throws JMetalException, FileNotFoundException {
+    Problem<BinarySolution> problem;
+    Algorithm<List<BinarySolution>> algorithm;
+
+    problem = new OneMax(1024) ;
+
+    algorithm = new RandomSearchBuilder<>(problem)
+            .setMaxEvaluations(25000)
+            .build() ;
+
+    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+            .execute() ;
+
+    List<BinarySolution> population = algorithm.getResult() ;
+    long computingTime = algorithmRunner.getComputingTime() ;
+
+    JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+
+    JMetalLogger.logger.info("Fitness: " + population.get(0).getObjective(0)) ;
+    JMetalLogger.logger.info("Solution: " + population.get(0).getVariable(0)) ;
+  }
+}
