941751b967c53cf6e19ccf4c7e342a46b1896821
ajnebro <ajnebro@users.noreply.github.com>
2020-09-15 09:37:21 +0200

    Refactor class

--- jmetal-parallel/src/main/java/org/uma/jmetal/parallel/example/SynchronousNSGAIIWithSparkExample.java
+++ jmetal-parallel/src/main/java/org/uma/jmetal/parallel/example/SynchronousNSGAIIWithSparkExample.java
@@ -44,26 +44,28 @@ public class SynchronousNSGAIIWithSparkExample extends AbstractAlgorithmRunner {
    *     org.uma.jmetal.runner.multiobjective.nsgaii.ParallelNSGAIIRunner problemName [referenceFront]
    */
   public static void main(String[] args) throws JMetalException, FileNotFoundException {
-    DoubleProblem problem;
+    Problem<DoubleSolution> problem;
     Algorithm<List<DoubleSolution>> algorithm;
     CrossoverOperator<DoubleSolution> crossover;
     MutationOperator<DoubleSolution> mutation;
     SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
 
-    String referenceParetoFront = "";
+    problem = new ZDT2() {
+      @Override
+      public DoubleSolution evaluate(DoubleSolution solution) {
+        super.evaluate(solution);
+        computingDelay();
 
-    String problemName;
-    if (args.length == 1) {
-      problemName = args[0];
-    } else if (args.length == 2) {
-      problemName = args[0];
-      referenceParetoFront = args[1];
-    } else {
-      problemName = "org.uma.jmetal.problem.multiobjective.zdt.ZDT1";
-      referenceParetoFront = "resources/referenceFrontsCSV/ZDT1.csv";
-    }
+        return solution;
+      }
 
-    problem = (DoubleProblem) ProblemUtils.<DoubleSolution>loadProblem(problemName);
+      private void computingDelay() {
+        for (long i = 0; i < 1000; i++)
+          for (long j = 0; j < 1000; j++) {
+            double a = sin(i) * Math.cos(j);
+          }
+      }
+    };
 
     double crossoverProbability = 0.9;
     double crossoverDistributionIndex = 20.0;
@@ -73,101 +75,31 @@ public class SynchronousNSGAIIWithSparkExample extends AbstractAlgorithmRunner {
     double mutationDistributionIndex = 20.0;
     mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
 
-    selection = new BinaryTournamentSelection<DoubleSolution>();
+    selection = new BinaryTournamentSelection<>(new RankingAndCrowdingDistanceComparator<>());
 
-    SolutionListEvaluator<DoubleSolution> evaluator =
-        new MultiThreadedSolutionListEvaluator<DoubleSolution>(8);
-
+    int maxEvaluations = 25000;
     int populationSize = 100;
-    NSGAIIBuilder<DoubleSolution> builder =
-        new NSGAIIBuilder<>(problem, crossover, mutation, populationSize)
-            .setSelectionOperator(selection)
-            .setMaxEvaluations(25000)
-            .setSolutionListEvaluator(evaluator);
 
-    algorithm = builder.build();
+    Logger.getLogger("org").setLevel(Level.OFF) ;
+
+    SparkConf sparkConf = new SparkConf()
+            .setMaster("local[8]")
+            .setAppName("NSGA-II with Spark");
+
+    JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+    SolutionListEvaluator<DoubleSolution> evaluator = new SparkSolutionListEvaluator<>(sparkContext) ;
+
+    algorithm = new NSGAIIBuilder<>(problem, crossover, mutation, populationSize)
+            .setSelectionOperator(selection)
+            .setMaxEvaluations(maxEvaluations)
+            .setSolutionListEvaluator(new SparkSolutionListEvaluator<>(sparkContext))
+            .build();
 
     algorithm.run();
-
-    builder.getSolutionListEvaluator().shutdown();
-
     List<DoubleSolution> population = algorithm.getResult();
 
     evaluator.shutdown();
 
     printFinalSolutionSet(population);
-    if (!referenceParetoFront.equals("")) {
-      printQualityIndicators(population, referenceParetoFront);
-    }
-  }
-
-  /**
-   * Class to configure and run the NSGA-II algorithm (variant with measures) with the G-Dominance Comparator.
-   */
-  public static class GNSGAIIMeasuresWithChartsRunner extends AbstractAlgorithmRunner {
-    /**
-     * @param args Command line arguments.
-     * @throws SecurityException Invoking command: java org.uma.jmetal.runner.multiobjective.nsgaii.NSGAIIMeasuresRunner
-     *                           problemName [referenceFront]
-     */
-    public static void main(String[] args) throws JMetalException, InterruptedException, IOException {
-      Problem<DoubleSolution> problem;
-      Algorithm<List<DoubleSolution>> algorithm;
-      CrossoverOperator<DoubleSolution> crossover;
-      MutationOperator<DoubleSolution> mutation;
-      SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
-
-      problem = new ZDT2() {
-        @Override
-        public DoubleSolution evaluate(DoubleSolution solution) {
-          super.evaluate(solution);
-          computingDelay();
-
-          return solution;
-        }
-
-        private void computingDelay() {
-          for (long i = 0; i < 1000; i++)
-            for (long j = 0; j < 1000; j++) {
-              double a = sin(i) * Math.cos(j);
-            }
-        }
-      };
-
-      double crossoverProbability = 0.9;
-      double crossoverDistributionIndex = 20.0;
-      crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
-
-      double mutationProbability = 1.0 / problem.getNumberOfVariables();
-      double mutationDistributionIndex = 20.0;
-      mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
-
-      selection = new BinaryTournamentSelection<>(new RankingAndCrowdingDistanceComparator<>());
-
-      int maxEvaluations = 25000;
-      int populationSize = 100;
-
-      Logger.getLogger("org").setLevel(Level.OFF) ;
-
-      SparkConf sparkConf = new SparkConf()
-              .setMaster("local[8]")
-              .setAppName("NSGA-II with Spark");
-
-      JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-
-      algorithm = new NSGAIIBuilder<>(problem, crossover, mutation, populationSize)
-              .setSelectionOperator(selection)
-              .setMaxEvaluations(maxEvaluations)
-              .setSolutionListEvaluator(new SparkSolutionListEvaluator<>(sparkContext))
-              .build();
-
-      algorithm.run();
-
-      printFinalSolutionSet(algorithm.getResult());
-
-      JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
-      JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
-      JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
-    }
   }
 }
