55b06c7fe1400af56e681450ad6d43ab735498d0
ajnebro <ajnebro@users.noreply.github.com>
2021-01-23 11:31:48 +0100

    Refactor class BestSolutionSelection

--- jmetal-core/src/main/java/org/uma/jmetal/operator/selection/impl/BestSolutionSelection.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/selection/impl/BestSolutionSelection.java
@@ -12,11 +12,11 @@ import java.util.List;
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
-@SuppressWarnings("serial")
 public class BestSolutionSelection<S> implements SelectionOperator<List<S>, S> {
-private Comparator<S> comparator ;
+private final Comparator<S> comparator ;
 
   public BestSolutionSelection(Comparator<S> comparator) {
+    Check.isNotNull(comparator);
     this.comparator = comparator ;
   }
 
@@ -25,13 +25,6 @@ private Comparator<S> comparator ;
     Check.isNotNull(solutionList);
     Check.collectionIsNotEmpty(solutionList);
 
-    int bestSolution = 0 ;
-    for (int i = 1; i < solutionList.size(); i++) {
-      if (comparator.compare(solutionList.get(i), solutionList.get(bestSolution)) < 0) {
-        bestSolution = i;
-      }
-    }
-
-    return solutionList.get(bestSolution) ;
+    return solutionList.stream().reduce(solutionList.get(0), (x, y) -> (comparator.compare(x, y) < 0) ? x : y);
   }
 }
