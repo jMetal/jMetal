54ceeeed3fc821431d273704f70594edc1b8818a
ajnebro <ajnebro@users.noreply.github.com>
2021-09-20 15:36:03 +0200

    Minor changes

--- jmetal-core/src/main/java/org/uma/jmetal/util/binarySet/BinarySet.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/binarySet/BinarySet.java
@@ -4,11 +4,12 @@ import java.util.BitSet;
 
 /**
  * Class representing a bit set including a method to get the total number of bits
+ *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
 @SuppressWarnings("serial")
 public class BinarySet extends BitSet {
-  private int numberOfBits ;
+  private int numberOfBits;
 
   /**
    * Constructor
@@ -16,12 +17,13 @@ public class BinarySet extends BitSet {
    * @param numberOfBits
    */
   public BinarySet(int numberOfBits) {
-    super(numberOfBits) ;
-    this.numberOfBits = numberOfBits ;
+    super(numberOfBits);
+    this.numberOfBits = numberOfBits;
   }
 
   /**
    * Returns the total number of bits
+   *
    * @return the number of bits of the binary set
    */
   public int getBinarySetLength() {
@@ -30,15 +32,14 @@ public class BinarySet extends BitSet {
 
   @Override
   public String toString() {
-    String result = "" ;
-    for (int i = 0; i < numberOfBits ; i++) {
+    String result = "";
+    for (int i = 0; i < numberOfBits; i++) {
       if (get(i)) {
-        result += "1" ;
-      }
-      else {
-        result+= "0" ;
+        result += "1";
+      } else {
+        result += "0";
       }
     }
-    return result ;
+    return result;
   }
 }
