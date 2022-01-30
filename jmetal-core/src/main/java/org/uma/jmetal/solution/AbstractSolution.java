7d483d2ea1c2fb3161b215fc44ec187a48ab2c12
ajnebro <ajnebro@users.noreply.github.com>
2022-01-30 18:57:26 +0100

    Refactor class AbstractSolution

--- jmetal-core/src/main/java/org/uma/jmetal/solution/AbstractSolution.java
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/AbstractSolution.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * Abstract class representing a generic solution
@@ -51,21 +52,15 @@ public abstract class AbstractSolution<T> implements Solution<T> {
     attributes = new HashMap<>();
 
     variables = new ArrayList<>(numberOfVariables);
-    for (int i = 0; i < numberOfVariables; i++) {
-      variables.add(i, null);
-    }
+    IntStream.range(0, numberOfVariables).forEach(i -> variables.add(i, null));
 
     objectives = new double[numberOfObjectives];
-    for (int i = 0; i < numberOfObjectives; i++) {
-      objectives[i] = 0.0;
-    }
+    IntStream.range(0, numberOfObjectives).forEach(i -> objectives[i] = 0.0);
 
     constraints = new double[numberOfConstraints];
-    for (int i = 0; i < numberOfConstraints; i++) {
-      constraints[i] = 0.0;
-    }
+    IntStream.range(0, numberOfConstraints).forEach(i -> constraints[i] = 0.0);
 
-    attributes = new HashMap<Object, Object>();
+    attributes = new HashMap<>();
   }
 
   @Override
@@ -91,7 +86,6 @@ public abstract class AbstractSolution<T> implements Solution<T> {
   @Override
   public boolean equals(Object o) {
     Check.notNull(o) ;
-
     Solution<T> solution = (Solution<T>) o;
 
     return this.variables().equals(solution.variables());
