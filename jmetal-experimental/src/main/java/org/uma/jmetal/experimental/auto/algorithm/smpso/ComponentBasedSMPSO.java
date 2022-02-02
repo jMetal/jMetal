1f38aa65b6670ac71eb06147502ffb483eb3dde1
ajnebro <ajnebro@users.noreply.github.com>
2022-02-02 15:41:17 +0100

    Add class ComponentBasedSMPSO

--- jmetal-experimental/src/main/java/org/uma/jmetal/experimental/auto/algorithm/smpso/ComponentBasedSMPSO.java
+++ jmetal-experimental/src/main/java/org/uma/jmetal/experimental/auto/algorithm/smpso/ComponentBasedSMPSO.java
@@ -3,7 +3,7 @@ package org.uma.jmetal.experimental.auto.algorithm.smpso;
 import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
 import org.uma.jmetal.problem.multiobjective.zdt.ZDT1;
 
-public class SMPSO {
+public class ComponentBasedSMPSO {
   public static void main(String[] args) {
     DoubleProblem problem = new ZDT1();
 
