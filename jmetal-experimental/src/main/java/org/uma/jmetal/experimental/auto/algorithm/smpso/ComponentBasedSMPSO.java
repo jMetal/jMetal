511dc4b423bc4dc2607818f8ef1e36848e7c03d6
ajnebro <ajnebro@users.noreply.github.com>
2022-03-02 08:49:44 +0100

    Update class ComponentBasedSMPSO

--- jmetal-experimental/src/main/java/org/uma/jmetal/experimental/auto/algorithm/smpso/ComponentBasedSMPSO.java
+++ jmetal-experimental/src/main/java/org/uma/jmetal/experimental/auto/algorithm/smpso/ComponentBasedSMPSO.java
@@ -17,7 +17,6 @@ import org.uma.jmetal.experimental.componentbasedalgorithm.catalogue.pso.velocit
 import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
 import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
-import org.uma.jmetal.problem.multiobjective.zdt.ZDT2;
 import org.uma.jmetal.problem.multiobjective.zdt.ZDT4;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
 import org.uma.jmetal.util.archive.BoundedArchive;
