706a8ec55920b7506c80c6856bca006ae5d69264
ajnebro <ajnebro@users.noreply.github.com>
2022-03-25 13:14:26 +0100

    Working on refactoring the constraint handling class

--- jmetal-core/src/main/java/org/uma/jmetal/util/ConstraintHandling.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/ConstraintHandling.java
@@ -1,63 +1,70 @@
 package org.uma.jmetal.util;
 
-import java.util.List;
+import java.util.stream.IntStream;
 import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.util.errorchecking.Check;
 
+import java.util.List;
+
 public class ConstraintHandling {
 
+  public enum PRECOMPUTED {
+    OVERALL_CONSTRAINT_VIOLATION,
+    NUMBER_OF_VIOLATED_CONSTRAINTS
+  }
+
   public static <S extends Solution<?>> boolean isFeasible(S solution) {
-    return numberOfViolatedConstraints(solution) == 0 ;
+    return numberOfViolatedConstraints(solution) == 0;
   }
 
   /**
    * Returns the number of constraints a solution violates.
+   *
    * @param solution
    * @param <S>
    * @return
    */
   public static <S extends Solution<?>> int numberOfViolatedConstraints(S solution) {
-    int result = 0 ;
-    for (int i = 0; i < solution.constraints().length; i++) {
-      if (solution.constraints()[i] < 0) {
-        result ++ ;
-      }
+    int result =
+        (int) solution.attributes().getOrDefault(PRECOMPUTED.NUMBER_OF_VIOLATED_CONSTRAINTS, 0);
+    if (result == 0) {
+      result = (int) IntStream.range(0, solution.constraints().length)
+          .filter(i -> solution.constraints()[i] < 0)
+          .count();
     }
-
-    return result ;
+    return result;
   }
 
   /**
    * Returns the overall constraint violation degree of a solution.
+   *
    * @param solution
    * @param <S>
    * @return
    */
   public static <S extends Solution<?>> double overallConstraintViolationDegree(S solution) {
-    double overallConstraintViolation = 0.0;
-    for (int i = 0; i < solution.constraints().length; i++) {
-      if (solution.constraints()[i] < 0.0) {
-        overallConstraintViolation += solution.constraints()[i] ;
-      }
+    double overallConstraintViolation =
+        (double) solution.attributes().getOrDefault(PRECOMPUTED.OVERALL_CONSTRAINT_VIOLATION, IntStream.range(0, solution.constraints().length)
+            .filter(i -> solution.constraints()[i] < 0.0).mapToDouble(i -> solution.constraints()[i])
+            .sum());
+    if (overallConstraintViolation == 0.0) {
+      overallConstraintViolation = IntStream.range(0, solution.constraints().length)
+          .filter(i -> solution.constraints()[i] < 0.0).mapToDouble(i -> solution.constraints()[i])
+          .sum();
     }
-
-    return overallConstraintViolation ;
+    return overallConstraintViolation;
   }
 
   /**
    * Returns the ratio of feasible solutions in a solution list
+   *
    * @param solutions
    * @return
    */
   public static <S extends Solution<?>> double feasibilityRatio(List<S> solutions) {
     Check.collectionIsNotEmpty(solutions);
-    double result = 0.0 ;
-    for (Solution<?> solution: solutions) {
-      if (isFeasible(solution)) {
-        result +=1 ;
-      }
-    }
+    double result = solutions.stream().filter(ConstraintHandling::isFeasible).count();
 
-    return result/solutions.size() ;
+    return result / solutions.size();
   }
 }
