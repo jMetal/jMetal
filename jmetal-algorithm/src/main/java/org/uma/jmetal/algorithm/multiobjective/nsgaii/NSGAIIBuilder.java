edba5edec524f3a90c7eb047b111779a7ad71c7b
ajnebro <ajnebro@users.noreply.github.com>
2022-03-28 10:58:59 +0200

    Update class NSGAIIBuilder to use by default the DominanceComparator class

--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/nsgaii/NSGAIIBuilder.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/nsgaii/NSGAIIBuilder.java
@@ -9,8 +9,10 @@ import org.uma.jmetal.operator.selection.SelectionOperator;
 import org.uma.jmetal.operator.selection.impl.BinaryTournamentSelection;
 import org.uma.jmetal.problem.Problem;
 import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.comparator.DominanceComparator;
 import org.uma.jmetal.util.comparator.DominanceWithConstraintsComparator;
 import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
+import org.uma.jmetal.util.errorchecking.Check;
 import org.uma.jmetal.util.errorchecking.JMetalException;
 import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
 import org.uma.jmetal.util.evaluator.impl.SequentialSolutionListEvaluator;
@@ -52,7 +54,7 @@ public class NSGAIIBuilder<S extends Solution<?>> implements AlgorithmBuilder<NS
     this.mutationOperator = mutationOperator ;
     selectionOperator = new BinaryTournamentSelection<S>(new RankingAndCrowdingDistanceComparator<S>()) ;
     evaluator = new SequentialSolutionListEvaluator<S>();
-    dominanceComparator = new DominanceWithConstraintsComparator<>()  ;
+    dominanceComparator = new DominanceComparator<>()  ;
 
     this.variant = NSGAIIVariant.NSGAII ;
   }
@@ -104,9 +106,7 @@ public class NSGAIIBuilder<S extends Solution<?>> implements AlgorithmBuilder<NS
   }
 
   public NSGAIIBuilder<S> setDominanceComparator(Comparator<S> dominanceComparator) {
-    if (dominanceComparator == null) {
-      throw new JMetalException("dominanceComparator is null");
-    }
+    Check.notNull(dominanceComparator);
     this.dominanceComparator = dominanceComparator ;
 
     return this;
