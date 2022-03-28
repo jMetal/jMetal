9e0060409e5f78e2f4f5ff242af68389b4c71f42
ajnebro <ajnebro@users.noreply.github.com>
2022-03-28 17:34:54 +0200

    Update class ABYSSConstrainedProblemIT

--- jmetal-algorithm/src/test/java/org/uma/jmetal/algorithm/multiobjective/abyss/ABYSSConstrainedProblemIT.java
+++ jmetal-algorithm/src/test/java/org/uma/jmetal/algorithm/multiobjective/abyss/ABYSSConstrainedProblemIT.java
@@ -2,7 +2,6 @@ package org.uma.jmetal.algorithm.multiobjective.abyss;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import org.junit.Before;
@@ -23,12 +22,14 @@ import org.uma.jmetal.util.SolutionListUtils;
 import org.uma.jmetal.util.VectorUtils;
 import org.uma.jmetal.util.archive.Archive;
 import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
-import org.uma.jmetal.util.comparator.OverallConstraintViolationDegreeComparator;
-import org.uma.jmetal.util.comparator.DominanceComparator;
-import org.uma.jmetal.util.comparator.MultiComparator;
+import org.uma.jmetal.util.comparator.DominanceWithConstraintsComparator;
+import org.uma.jmetal.util.comparator.constraintcomparator.impl.OverallConstraintViolationDegreeComparator;
 
-/** Created by ajnebro on 11/6/15. */
+/**
+ * Created by ajnebro on 11/6/15.
+ */
 public class ABYSSConstrainedProblemIT {
+
   Algorithm<List<DoubleSolution>> algorithm;
   DoubleProblem problem;
   CrossoverOperator<DoubleSolution> crossover;
@@ -50,9 +51,8 @@ public class ABYSSConstrainedProblemIT {
 
     archive = new CrowdingDistanceArchive<>(100);
 
-    Comparator<DoubleSolution> comparator =
-        new MultiComparator<>(
-            Arrays.asList(new OverallConstraintViolationDegreeComparator<>(), new DominanceComparator<>()));
+    Comparator<DoubleSolution> comparator = new DominanceWithConstraintsComparator<>(
+        new OverallConstraintViolationDegreeComparator<>());
 
     localSearchOperator = new BasicLocalSearch<>(1, mutation, comparator, problem);
   }
