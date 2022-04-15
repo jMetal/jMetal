9715978ad207ae8c2a84dff0a19f696b19bed571
ajnebro <ajnebro@users.noreply.github.com>
2022-04-15 10:50:13 +0200

    Refactor class ObjectiveComparator

--- jmetal-core/src/main/java/org/uma/jmetal/util/comparator/ObjectiveComparator.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/comparator/ObjectiveComparator.java
@@ -3,19 +3,20 @@ package org.uma.jmetal.util.comparator;
 import java.io.Serializable;
 import java.util.Comparator;
 import org.uma.jmetal.solution.Solution;
-import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.errorchecking.Check;
 
 /**
  * This class implements a comparator based on a given objective
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
-@SuppressWarnings("serial")
 public class ObjectiveComparator<S extends Solution<?>> implements Comparator<S>, Serializable {
-  public enum Ordering {ASCENDING, DESCENDING} ;
-  private int objectiveId;
 
-  private Ordering order;
+  public enum Ordering {ASCENDING, DESCENDING}
+
+  private final int objectiveId;
+
+  private final Ordering order;
 
   /**
    * Constructor.
@@ -29,12 +30,13 @@ public class ObjectiveComparator<S extends Solution<?>> implements Comparator<S>
 
   /**
    * Comparator.
+   *
    * @param objectiveId The index of the objective to compare
-   * @param order Ascending or descending order
+   * @param order       Ascending or descending order
    */
   public ObjectiveComparator(int objectiveId, Ordering order) {
     this.objectiveId = objectiveId;
-    this.order = order ;
+    this.order = order;
   }
 
   /**
@@ -47,30 +49,23 @@ public class ObjectiveComparator<S extends Solution<?>> implements Comparator<S>
    */
   @Override
   public int compare(S solution1, S solution2) {
-    int result ;
-    if (solution1 == null) {
-      if (solution2 == null) {
-        result = 0;
-      } else {
-        result =  1;
-      }
-    } else if (solution2 == null) {
-      result =  -1;
-    } else if (solution1.objectives().length <= objectiveId) {
-      throw new JMetalException("The solution1 has " + solution1.objectives().length+ " objectives "
-          + "and the objective to sort is " + objectiveId) ;
-    } else if (solution2.objectives().length <= objectiveId) {
-      throw new JMetalException("The solution2 has " + solution2.objectives().length+ " objectives "
-          + "and the objective to sort is " + objectiveId) ;
+    Check.notNull(solution1);
+    Check.notNull(solution2);
+
+    Check.that(solution1.objectives().length > objectiveId, "The solution1 has "
+        + solution1.objectives().length + " objectives and the objective to sort is" + objectiveId);
+    Check.that(solution2.objectives().length > objectiveId, "The solution2 has "
+        + solution2.objectives().length + " objectives and the objective to sort is" + objectiveId);
+
+    double objective1 = solution1.objectives()[this.objectiveId];
+    double objective2 = solution2.objectives()[this.objectiveId];
+    int result;
+    if (order == Ordering.ASCENDING) {
+      result = Double.compare(objective1, objective2);
     } else {
-      double objective1 = solution1.objectives()[this.objectiveId];
-      double objective2 = solution2.objectives()[this.objectiveId];
-      if (order == Ordering.ASCENDING) {
-        result = Double.compare(objective1, objective2);
-      } else {
-        result = Double.compare(objective2, objective1);
-      }
+      result = Double.compare(objective2, objective1);
     }
-    return result ;
+
+    return result;
   }
 }
