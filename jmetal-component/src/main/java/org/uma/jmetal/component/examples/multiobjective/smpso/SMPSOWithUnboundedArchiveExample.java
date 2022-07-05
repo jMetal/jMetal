c3d29bff7d3e5089e5b700615b1f4812140ae35c
ajnebro <ajnebro@users.noreply.github.com>
2022-07-05 09:43:10 +0200

    Add class SMPSOWithUnboundedArchiveExample

---
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/smpso/SMPSOWithUnboundedArchiveExample.java
@@ -0,0 +1,66 @@
+package org.uma.jmetal.component.examples.multiobjective.smpso;
+
+import java.io.IOException;
+import java.util.List;
+import org.uma.jmetal.component.algorithm.ParticleSwarmOptimizationAlgorithm;
+import org.uma.jmetal.component.algorithm.multiobjective.SMPSOBuilder;
+import org.uma.jmetal.component.catalogue.pso.globalbestinitialization.impl.DefaultGlobalBestWithExternalArchiveInitialization;
+import org.uma.jmetal.component.catalogue.pso.globalbestupdate.impl.DefaultGlobalBestWithExternalArchiveUpdate;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
+import org.uma.jmetal.qualityindicator.QualityIndicatorUtils;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.util.SolutionListUtils;
+import org.uma.jmetal.util.VectorUtils;
+import org.uma.jmetal.util.archive.Archive;
+import org.uma.jmetal.util.archive.impl.BestSolutionsArchive;
+import org.uma.jmetal.util.archive.impl.NonDominatedSolutionListArchive;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+import org.uma.jmetal.util.termination.Termination;
+import org.uma.jmetal.util.termination.impl.TerminationByEvaluations;
+
+public class SMPSOWithUnboundedArchiveExample {
+  public static void main(String[] args) throws JMetalException, IOException {
+    String problemName = "org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2";
+    String referenceParetoFront = "resources/referenceFrontsCSV/DTLZ2.3D.csv";
+
+    Problem<DoubleSolution> problem = ProblemUtils.<DoubleSolution>loadProblem(problemName);
+
+    int swarmSize = 100 ;
+    Termination termination = new TerminationByEvaluations(50000);
+
+    Archive<DoubleSolution> externalUnboundedArchive = new BestSolutionsArchive<>(new NonDominatedSolutionListArchive<>(), swarmSize) ;
+
+    ParticleSwarmOptimizationAlgorithm smpso = new SMPSOBuilder(
+        (DoubleProblem) problem,
+        swarmSize)
+        .setTermination(termination)
+        .setGlobalBestInitialization(new DefaultGlobalBestWithExternalArchiveInitialization(externalUnboundedArchive))
+        .setGlobalBestUpdate(new DefaultGlobalBestWithExternalArchiveUpdate(externalUnboundedArchive))
+        .build();
+
+    smpso.run();
+
+    List<DoubleSolution> population = externalUnboundedArchive.getSolutionList();
+    JMetalLogger.logger.info("Total execution time : " + smpso.getTotalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + smpso.getEvaluation());
+
+    new SolutionListOutput(population)
+        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+        .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+
+    QualityIndicatorUtils.printQualityIndicators(
+        SolutionListUtils.getMatrixWithObjectiveValues(population),
+        VectorUtils.readVectors(referenceParetoFront, ","));
+  }
+}
