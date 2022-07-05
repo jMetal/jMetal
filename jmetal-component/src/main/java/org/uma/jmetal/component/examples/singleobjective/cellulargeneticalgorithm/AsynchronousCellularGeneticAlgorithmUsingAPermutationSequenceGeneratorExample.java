5f7a52b805bef238c39351b1d25ecf625a1fc09f
ajnebro <ajnebro@users.noreply.github.com>
2022-07-05 17:59:32 +0200

    Add cellular genetic algorithm examples

---
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/singleobjective/cellulargeneticalgorithm/AsynchronousCellularGeneticAlgorithmUsingAPermutationSequenceGeneratorExample.java
@@ -0,0 +1,98 @@
+package org.uma.jmetal.component.examples.singleobjective.cellulargeneticalgorithm;
+
+import java.util.List;
+import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
+import org.uma.jmetal.component.algorithm.singleobjective.GeneticAlgorithmBuilder;
+import org.uma.jmetal.component.catalogue.ea.selection.impl.NeighborhoodMatingPoolSelection;
+import org.uma.jmetal.component.catalogue.ea.variation.impl.CrossoverAndMutationVariation;
+import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.operator.selection.impl.NaryTournamentSelection;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
+import org.uma.jmetal.problem.singleobjective.Sphere;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.comparator.ObjectiveComparator;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.neighborhood.Neighborhood;
+import org.uma.jmetal.util.neighborhood.impl.C9;
+import org.uma.jmetal.util.observer.impl.FitnessObserver;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+import org.uma.jmetal.util.sequencegenerator.SequenceGenerator;
+import org.uma.jmetal.util.sequencegenerator.impl.IntegerBoundedSequenceGenerator;
+import org.uma.jmetal.util.sequencegenerator.impl.IntegerPermutationGenerator;
+import org.uma.jmetal.util.termination.Termination;
+import org.uma.jmetal.util.termination.impl.TerminationByEvaluations;
+
+/**
+ * Class to configure and run an asynchronous cellular genetic algorithm to solve a {@link DoubleProblem}
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class AsynchronousCellularGeneticAlgorithmUsingAPermutationSequenceGeneratorExample {
+  public static void main(String[] args) throws JMetalException {
+    Problem<DoubleSolution> problem = new Sphere(20) ;
+
+    double crossoverProbability = 0.9;
+    double crossoverDistributionIndex = 20.0;
+    var crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    var mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+
+    int populationSize = 100;
+    int offspringPopulationSize = 1;
+
+    int rows = 10 ;
+    int columns = 10 ;
+    Neighborhood<DoubleSolution> neighborhood = new C9<>(rows, columns) ;
+
+    SequenceGenerator<Integer> solutionIndexGenerator = new IntegerPermutationGenerator(populationSize);
+
+    var variation = new CrossoverAndMutationVariation<>(offspringPopulationSize, crossover, mutation) ;
+
+    var selection =
+        new NeighborhoodMatingPoolSelection<>(
+            variation.getMatingPoolSize(),
+            solutionIndexGenerator,
+            neighborhood,
+            new NaryTournamentSelection<>(2, new ObjectiveComparator<>(0)),
+            true);
+
+    Termination termination = new TerminationByEvaluations(500000);
+
+    EvolutionaryAlgorithm<DoubleSolution> geneticAlgorithm = new GeneticAlgorithmBuilder<>(
+        "acGA",
+                    problem,
+                    populationSize,
+                    offspringPopulationSize,
+                    crossover,
+                    mutation)
+        .setTermination(termination)
+        .setVariation(variation)
+        .setSelection(selection)
+        .build();
+
+    geneticAlgorithm.getObservable().register(new FitnessObserver(5000));
+
+    geneticAlgorithm.run();
+
+    List<DoubleSolution> population = geneticAlgorithm.getResult();
+    JMetalLogger.logger.info("Total execution time : " + geneticAlgorithm.getTotalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + geneticAlgorithm.getNumberOfEvaluations());
+    JMetalLogger.logger.info("Best fitness: " + geneticAlgorithm.getResult().get(0).objectives()[0]);
+
+    new SolutionListOutput(population)
+            .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+            .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+            .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+  }
+}
