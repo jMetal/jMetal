63c9b186fe71408bdf198c3b4413b20cfc18faaa
ajnebro <ajnebro@users.noreply.github.com>
2022-07-07 12:51:38 +0200

    Minor changes

--- jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/parameterfilegeneration/AutoMOPSOIraceFileGenerator.java
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/parameterfilegeneration/AutoMOPSOIraceFileGenerator.java
@@ -1,9 +1,17 @@
 package org.uma.jmetal.auto.irace.parameterfilegeneration;
 
 import org.uma.jmetal.auto.autoconfigurablealgorithm.AutoMOPSO;
+import org.uma.jmetal.auto.autoconfigurablealgorithm.AutoNSGAII;
 
 /**
- * @author Daniel Doblas
+ * Program to generate the irace configuration file for class {@link AutoMOPSO}
+ * Note that the result configuration must be manually modified, replacing the lines of parameters
+ * weightMin and weightMax by these:
+ *
+ * weightMin    "--weightMin "     r   (0.1, 0.5)      | inertiaWeightComputingStrategy %in% c("randomSelectedValue", "linearIncreasingValue", "linearDecreasingValue")
+ * weightMax    "--weightMax "     r    (0.5, 1.0)     | inertiaWeightComputingStrategy %in% c("randomSelectedValue", "linearIncreasingValue", "linearDecreasingValue")
+ *
+ * @author Antonio J. Nebro (ajnebro@uma.es)
  */
 public class AutoMOPSOIraceFileGenerator {
 
---
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/parameterfilegeneration/AutoNSGAIIIraceParameterFileGenerator.java
@@ -0,0 +1,37 @@
+package org.uma.jmetal.auto.irace.parameterfilegeneration;
+
+import org.uma.jmetal.auto.autoconfigurablealgorithm.AutoNSGAII;
+
+/**
+ * Program to generate the irace configuration file for class {@link AutoNSGAII}
+ *
+ * @author Antonio J. Nebro (ajnebro@uma.es)
+ */
+public class AutoNSGAIIIraceParameterFileGenerator {
+
+  public static void main(String[] args) {
+    String[] parameters =
+        ("--problemName org.uma.jmetal.problem.multiobjective.zdt.ZDT1 "
+            + "--referenceFrontFileName ZDT1.csv "
+            + "--maximumNumberOfEvaluations 25000 "
+            + "--algorithmResult population "
+            + "--populationSize 100 "
+            + "--offspringPopulationSize 100 "
+            + "--createInitialSolutions random "
+            + "--variation crossoverAndMutationVariation "
+            + "--selection tournament "
+            + "--selectionTournamentSize 2 "
+            + "--crossover SBX "
+            + "--crossoverProbability 0.9 "
+            + "--crossoverRepairStrategy bounds "
+            + "--sbxDistributionIndex 20.0 "
+            + "--mutation polynomial "
+            + "--mutationProbabilityFactor 1.0 "
+            + "--mutationRepairStrategy bounds "
+            + "--polynomialMutationDistributionIndex 20.0 ")
+            .split("\\s+");
+
+    IraceParameterFileGenerator parameterFileGenerator = new IraceParameterFileGenerator() ;
+    parameterFileGenerator.generateConfigurationFile(new AutoNSGAII(), parameters) ;
+  }
+}
