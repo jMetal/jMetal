d6cf17487983ece429a4f7d3aa195cb05edfce71
ajnebro <ajnebro@users.noreply.github.com>
2022-07-11 13:12:05 +0200

    Add class MOEADWithUnboundedArchiveExample

---
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/moea/MOEADWithUnboundedArchiveExample.java
@@ -0,0 +1,80 @@
+package org.uma.jmetal.component.examples.multiobjective.moea;
+
+import java.io.IOException;
+import java.util.List;
+import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
+import org.uma.jmetal.component.algorithm.multiobjective.MOEADBuilder;
+import org.uma.jmetal.component.catalogue.common.termination.Termination;
+import org.uma.jmetal.component.catalogue.common.termination.impl.TerminationByEvaluations;
+import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemFactory;
+import org.uma.jmetal.util.archive.Archive;
+import org.uma.jmetal.util.archive.impl.BestSolutionsArchive;
+import org.uma.jmetal.util.archive.impl.NonDominatedSolutionListArchive;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+import org.uma.jmetal.util.sequencegenerator.SequenceGenerator;
+import org.uma.jmetal.util.sequencegenerator.impl.IntegerPermutationGenerator;
+
+/**
+ * Class to configure and run the NSGA-II algorithm configured with standard settings.
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class MOEADWithUnboundedArchiveExample {
+  public static void main(String[] args) throws JMetalException, IOException {
+    String problemName = "org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2Minus";
+
+    Problem<DoubleSolution> problem = ProblemFactory.<DoubleSolution>loadProblem(problemName);
+
+    double crossoverProbability = 0.9;
+    double crossoverDistributionIndex = 20.0;
+    var crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    var mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+
+    int populationSize = 91;
+
+    Termination termination = new TerminationByEvaluations(40000);
+
+    String weightVectorDirectory = "resources/weightVectorFiles/moead";
+
+    SequenceGenerator<Integer> sequenceGenerator = new IntegerPermutationGenerator(populationSize) ;
+
+    Archive<DoubleSolution> externalArchive = new BestSolutionsArchive<>(new NonDominatedSolutionListArchive<>(), populationSize) ;
+
+    EvolutionaryAlgorithm<DoubleSolution> moead = new MOEADBuilder<>(
+        problem,
+        populationSize,
+        crossover,
+        mutation,
+        weightVectorDirectory,
+        sequenceGenerator)
+        .setTermination(termination)
+        .setArchive(externalArchive)
+        .build();
+
+    moead.run();
+
+    List<DoubleSolution> population = moead.getResult();
+    JMetalLogger.logger.info("Total execution time : " + moead.getTotalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + moead.getNumberOfEvaluations());
+
+    new SolutionListOutput(population)
+        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+        .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+  }
+}
