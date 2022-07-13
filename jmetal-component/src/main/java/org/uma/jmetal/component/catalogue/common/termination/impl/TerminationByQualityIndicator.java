651bc55c23e8a2cda7e156481d76429a9e8d1752
ajnebro <ajnebro@users.noreply.github.com>
2022-07-13 15:57:41 +0200

    Update class TerminationByQualityIndicator

--- jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/common/termination/impl/TerminationByQualityIndicator.java
+++ jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/common/termination/impl/TerminationByQualityIndicator.java
@@ -11,28 +11,37 @@ import org.uma.jmetal.util.errorchecking.Check;
 
 /**
  * Class that allows to check the termination condition when current front is above a given
- * percentage of the value of a quality indicator applied to a reference front
+ * percentage of the value of a quality indicator applied to a reference front. An evaluations limit
+ * is used to avoid an infinite loop if the value is never gotten.
  *
  * @author Antonio J. Nebro <antonio@lcc.uma.es>
  */
 public class TerminationByQualityIndicator implements Termination {
-  private QualityIndicator qualityIndicator;
-  private double[][] referenceFront;
-  private double percentage;
-  private double referenceFrontIndicatorValue;
-  private int evaluationsLimit ;
-  private int evaluations ;
-  private boolean evaluationsLimitReached ;
-  private double computedIndicatorValue ;
+
+  private final QualityIndicator qualityIndicator;
+  private final double[][] referenceFront;
+  private final double percentage;
+  private final double referenceFrontIndicatorValue;
+  private final int evaluationsLimit;
+  private int evaluations;
+  private boolean evaluationsLimitReached;
+  private double computedIndicatorValue;
 
   public TerminationByQualityIndicator(
-      QualityIndicator qualityIndicator, double[][] referenceFront, double percentage, int evaluationsLimit) {
+      QualityIndicator qualityIndicator, double[][] referenceFront, double percentage,
+      int evaluationsLimit) {
+    Check.notNull(qualityIndicator);
+    Check.notNull(referenceFront);
+    Check.valueIsNotNegative(percentage);
+    Check.valueIsNotNegative(evaluationsLimit);
+    Check.that(referenceFront.length > 1,
+        "The reference front must have at least two points instead of " + referenceFront.length);
+
     this.qualityIndicator = qualityIndicator;
     this.percentage = percentage;
     this.referenceFront = referenceFront;
-    this.evaluationsLimit = evaluationsLimit ;
-    evaluations = 0 ;
-    evaluationsLimitReached = false ;
+    this.evaluationsLimit = evaluationsLimit;
+    evaluationsLimitReached = false;
 
     double[][] normalizedReferenceFront = NormalizeUtils.normalize(referenceFront);
     qualityIndicator.setReferenceFront(normalizedReferenceFront);
@@ -41,10 +50,13 @@ public class TerminationByQualityIndicator implements Termination {
 
   @Override
   public boolean isMet(Map<String, Object> algorithmStatusData) {
-    List<Solution<?>> population = (List<Solution<?>>) algorithmStatusData.get("POPULATION");
-    evaluations = (int) algorithmStatusData.get("EVALUATIONS") ;
+    Check.notNull(algorithmStatusData.get("POPULATION"));
+    Check.notNull(algorithmStatusData.get("EVALUATIONS"));
 
+    List<Solution<?>> population = (List<Solution<?>>) algorithmStatusData.get("POPULATION");
+    evaluations = (int) algorithmStatusData.get("EVALUATIONS");
     Check.notNull(population);
+    Check.collectionIsNotEmpty(population);
 
     double[][] front = SolutionListUtils.getMatrixWithObjectiveValues(population);
     double[][] normalizedFront =
@@ -55,29 +67,38 @@ public class TerminationByQualityIndicator implements Termination {
 
     computedIndicatorValue = qualityIndicator.compute(normalizedFront);
 
-    boolean unsuccessfulStopCondition = evaluationsLimit <= evaluations ;
-    boolean successfulStopCondition = computedIndicatorValue >= percentage * referenceFrontIndicatorValue ;
+    boolean unsuccessfulStopCondition = evaluationsLimit <= evaluations;
+    boolean successfulStopCondition =
+        computedIndicatorValue >= percentage * referenceFrontIndicatorValue;
 
     if (unsuccessfulStopCondition) {
-      evaluationsLimitReached = true ;
+      evaluationsLimitReached = true;
     }
 
     return (successfulStopCondition || unsuccessfulStopCondition);
   }
 
   public double getComputedIndicatorValue() {
-    return computedIndicatorValue ;
+    return computedIndicatorValue;
   }
 
   public double getReferenceFrontIndicatorValue() {
-    return referenceFrontIndicatorValue ;
+    return referenceFrontIndicatorValue;
   }
 
   public double getEvaluations() {
-    return evaluations ;
+    return evaluations;
   }
 
   public boolean evaluationsLimitReached() {
-    return evaluationsLimitReached ;
+    return evaluationsLimitReached;
+  }
+
+  public QualityIndicator getQualityIndicator() {
+    return qualityIndicator;
+  }
+
+  public int getEvaluationsLimit() {
+    return evaluationsLimit ;
   }
 }
