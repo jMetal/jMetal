7deab45cd291d304ad52f3155d52fd27b8766602
ajnebro <ajnebro@users.noreply.github.com>
2022-07-14 16:50:19 +0200

    Minor changes

--- jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/common/termination/impl/TerminationByQualityIndicator.java
+++ jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/common/termination/impl/TerminationByQualityIndicator.java
@@ -77,7 +77,6 @@ public class TerminationByQualityIndicator implements Termination {
       if (successfulStopCondition) {
         stoppingCondition = true ;
       }
-
     }
     return stoppingCondition;
   }
