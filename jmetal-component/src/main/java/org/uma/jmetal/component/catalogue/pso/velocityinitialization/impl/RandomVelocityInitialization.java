83d82173eaea129609dfc21b04f8981e2119d6c0
ajnebro <ajnebro@users.noreply.github.com>
2022-09-07 16:10:10 +0200

    Update class RandomVelocityInitialization

--- jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/pso/velocityinitialization/impl/RandomVelocityInitialization.java
+++ jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/pso/velocityinitialization/impl/RandomVelocityInitialization.java
@@ -3,12 +3,18 @@ package org.uma.jmetal.component.catalogue.pso.velocityinitialization.impl;
 import java.util.List;
 import org.uma.jmetal.component.catalogue.pso.velocityinitialization.VelocityInitialization;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.bounds.Bounds;
 import org.uma.jmetal.util.errorchecking.Check;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+import org.uma.jmetal.util.pseudorandom.PseudoRandomGenerator;
 
 /**
+ * Class that initializes the velocity of the particles randomly according the particle position
+ * bounds.
+ *
  * @author Antonio J. Nebro
  */
-public class DefaultVelocityInitialization implements VelocityInitialization {
+public class RandomVelocityInitialization implements VelocityInitialization {
   @Override
   /**
    * Initialize the velocity of the particles.
@@ -21,10 +27,14 @@ public class DefaultVelocityInitialization implements VelocityInitialization {
 
     int numberOfVariables = swarm.get(0).variables().size() ;
     double[][] speed = new double[swarm.size()][numberOfVariables] ;
+    PseudoRandomGenerator randomGenerator = JMetalRandom.getInstance().getRandomGenerator();
 
     for (int i = 0 ; i < speed.length; i++) {
+      DoubleSolution particle = swarm.get(i) ;
       for (int j = 0; j < numberOfVariables; j++) {
-        speed[i][j] = 0.0 ;
+        Bounds<Double> bounds = particle.getBounds(j) ;
+        speed[i][j] = (randomGenerator.nextDouble(bounds.getLowerBound(), bounds.getUpperBound())
+            - particle.variables().get(j)) / 2.0;
       }
     }
 
