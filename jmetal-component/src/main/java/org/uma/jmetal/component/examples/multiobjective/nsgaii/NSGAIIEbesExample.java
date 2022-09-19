d06f4630610984c2af16ba0ce4d518f273668258
ajnebro <ajnebro@users.noreply.github.com>
2022-09-19 16:07:45 +0200

    Create class NSGAIIEbesExample

---
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/nsgaii/NSGAIIEbesExample.java
@@ -0,0 +1,75 @@
+package org.uma.jmetal.component.examples.multiobjective.nsgaii;
+
+import java.io.IOException;
+import java.util.List;
+import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
+import org.uma.jmetal.component.algorithm.multiobjective.NSGAIIBuilder;
+import org.uma.jmetal.component.catalogue.common.termination.Termination;
+import org.uma.jmetal.component.catalogue.common.termination.impl.TerminationByEvaluations;
+import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
+import org.uma.jmetal.problem.multiobjective.ebes.Ebes;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.observer.impl.EvaluationObserver;
+import org.uma.jmetal.util.observer.impl.RunTimeChartObserver;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+/**
+ * Class to configure and run the NSGA-II algorithm showing the population while the algorithm is running
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class NSGAIIEbesExample {
+  public static void main(String[] args) throws JMetalException, IOException {
+    DoubleProblem problem = new Ebes("resources/ebes/Bridge_Stayed_Cable_33G_133N_221B_93ZXY.ebe", new String[]{"W", "D"}) ;
+
+    double crossoverProbability = 0.9;
+    double crossoverDistributionIndex = 20.0;
+    var crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    var mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+
+    int populationSize = 100;
+    int offspringPopulationSize = populationSize;
+
+    Termination termination = new TerminationByEvaluations(25000);
+
+    EvolutionaryAlgorithm<DoubleSolution> nsgaii = new NSGAIIBuilder<>(
+                    problem,
+                    populationSize,
+                    offspringPopulationSize,
+                    crossover,
+                    mutation)
+        .setTermination(termination)
+        .build();
+
+    EvaluationObserver evaluationObserver = new EvaluationObserver(1000);
+    RunTimeChartObserver<DoubleSolution> runTimeChartObserver =
+        new RunTimeChartObserver<>("NSGA-II", 80, 100, null);
+
+    nsgaii.getObservable().register(evaluationObserver);
+    nsgaii.getObservable().register(runTimeChartObserver);
+
+    nsgaii.run();
+
+    List<DoubleSolution> population = nsgaii.getResult();
+    JMetalLogger.logger.info("Total execution time : " + nsgaii.getTotalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + nsgaii.getNumberOfEvaluations());
+
+    new SolutionListOutput(population)
+            .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+            .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+            .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+  }
+}
