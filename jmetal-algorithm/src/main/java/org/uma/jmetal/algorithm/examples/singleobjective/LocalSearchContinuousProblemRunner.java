ec2366ac6f400a6c7a27a51db3eda44cf6719065
ajnebro <ajnebro@users.noreply.github.com>
2022-09-28 15:41:23 +0200

    Add local search runner for continuous problems

---
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/examples/singleobjective/LocalSearchContinuousProblemRunner.java
@@ -0,0 +1,45 @@
+package org.uma.jmetal.algorithm.examples.singleobjective;
+
+import java.util.Comparator;
+import org.uma.jmetal.algorithm.impl.DefaultLocalSearch;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.singleobjective.Sphere;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.comparator.dominanceComparator.impl.DefaultDominanceComparator;
+
+/**
+ * Class to configure and run a single objective local search. The target problem is Sphere.
+ *
+ * @author Antonio J. Nebro
+ */
+public class LocalSearchContinuousProblemRunner {
+  public static void main(String[] args)  {
+    var problem = new Sphere(20) ;
+
+    var mutationOperator =
+        new PolynomialMutation(1.0 / problem.getNumberOfVariables(), 20.0) ;
+
+    int improvementRounds = 500000 ;
+
+    Comparator<DoubleSolution> comparator = new DefaultDominanceComparator<>() ;
+
+    DefaultLocalSearch<DoubleSolution> localSearch = new DefaultLocalSearch<>(
+            improvementRounds,
+            problem,
+            mutationOperator,
+            comparator) ;
+
+
+    long startTime = System.currentTimeMillis() ;
+    localSearch.run();
+    long endTime = System.currentTimeMillis() ;
+
+    DoubleSolution foundSolution = localSearch.getResult() ;
+
+    String fitnessMessage = "Fitness: " + foundSolution.objectives()[0] ;
+    JMetalLogger.logger.info(fitnessMessage) ;
+    JMetalLogger.logger.info("Solution: " + foundSolution.variables().get(0)) ;
+    JMetalLogger.logger.info("Computing time: " + (endTime - startTime)) ;
+  }
+}
---
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/AutoMOEADIraceHV.java
@@ -0,0 +1,37 @@
+package org.uma.jmetal.auto.irace;
+
+import static org.uma.jmetal.util.SolutionListUtils.getMatrixWithObjectiveValues;
+
+import java.io.IOException;
+import org.uma.jmetal.auto.autoconfigurablealgorithm.AutoNSGAII;
+import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.impl.PISAHypervolume;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.NormalizeUtils;
+import org.uma.jmetal.util.VectorUtils;
+
+public class AutoNSGAIIIraceHV {
+  public static void main(String[] args) throws IOException {
+    AutoNSGAII nsgaiiWithParameters = new AutoNSGAII();
+    nsgaiiWithParameters.parseAndCheckParameters(args);
+
+    EvolutionaryAlgorithm<DoubleSolution> nsgaII = nsgaiiWithParameters.create();
+    nsgaII.run();
+
+    String referenceFrontFile =
+        "resources/referenceFrontsCSV/" + nsgaiiWithParameters.referenceFrontFilename.getValue();
+
+    double[][] referenceFront = VectorUtils.readVectors(referenceFrontFile, ",");
+    double[][] front = getMatrixWithObjectiveValues(nsgaII.getResult()) ;
+
+    double[][] normalizedReferenceFront = NormalizeUtils.normalize(referenceFront);
+    double[][] normalizedFront =
+            NormalizeUtils.normalize(
+                    front,
+                    NormalizeUtils.getMinValuesOfTheColumnsOfAMatrix(referenceFront),
+                    NormalizeUtils.getMaxValuesOfTheColumnsOfAMatrix(referenceFront));
+
+    var qualityIndicator = new PISAHypervolume(normalizedReferenceFront) ;
+    System.out.println(qualityIndicator.compute(normalizedFront) * -1.0) ;
+  }
+}
