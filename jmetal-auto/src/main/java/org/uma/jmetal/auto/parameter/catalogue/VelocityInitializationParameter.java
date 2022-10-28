23371c2e1ee4ebd4cac62b29252ed93f98e7242e
ajnebro <ajnebro@users.noreply.github.com>
2022-10-28 09:16:39 +0200

    Minor changes

--- jmetal-auto/src/main/java/org/uma/jmetal/auto/parameter/catalogue/VelocityInitializationParameter.java
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/parameter/catalogue/VelocityInitializationParameter.java
@@ -32,6 +32,6 @@ public class VelocityInitializationParameter extends CategoricalParameter {
 
   @Override
   public String getName() {
-    return "velocity initialization";
+    return "velocityInitialization";
   }
 }
