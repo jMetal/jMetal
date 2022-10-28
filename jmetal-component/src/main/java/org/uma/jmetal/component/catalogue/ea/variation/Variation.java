25b0130874312205434fe793912a8b54aac0b7d7
ajnebro <ajnebro@users.noreply.github.com>
2022-10-28 09:13:43 +0200

    Reformat code

--- jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/ea/variation/Variation.java
+++ jmetal-component/src/main/java/org/uma/jmetal/component/catalogue/ea/variation/Variation.java
@@ -5,7 +5,6 @@ import org.uma.jmetal.solution.Solution;
 
 public interface Variation<S extends Solution<?>> {
   List<S> variate(List<S> solutionList, List<S> matingPool) ;
-
   int getMatingPoolSize() ;
   int getOffspringPopulationSize() ;
 }
