5af824d2789b7de4c162d6f69acbc00ea50515fb
ajnebro <ajnebro@users.noreply.github.com>
2022-12-12 12:22:24 +0100

    Refactor class PolynomialMutation

--- jmetal-core/src/main/java/org/uma/jmetal/operator/mutation/impl/PolynomialMutation.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/mutation/impl/PolynomialMutation.java
@@ -115,41 +115,6 @@ public class PolynomialMutation implements MutationOperator<DoubleSolution> {
     return solution;
   }
 
-  public double[] execute(double[] x, double[] lowerBound, double[] upperBound) {
-    double rnd, delta1, delta2, mutPow, deltaq;
-    double y, yl, yu, val, xy;
-
-    for (int i = 0; i < x.length; i++) {
-      if (randomGenerator.getRandomValue() <= mutationProbability) {
-        y = x[i];
-        yl = lowerBound[i];
-        yu = upperBound[i];
-        if (yl == yu) {
-          y = yl;
-        } else {
-          delta1 = (y - yl) / (yu - yl);
-          delta2 = (yu - y) / (yu - yl);
-          rnd = randomGenerator.getRandomValue();
-          mutPow = 1.0 / (distributionIndex + 1.0);
-          if (rnd <= 0.5) {
-            xy = 1.0 - delta1;
-            val = 2.0 * rnd + (1.0 - 2.0 * rnd) * (Math.pow(xy, distributionIndex + 1.0));
-            deltaq = Math.pow(val, mutPow) - 1.0;
-          } else {
-            xy = 1.0 - delta2;
-            val = 2.0 * (1.0 - rnd) + 2.0 * (rnd - 0.5) * (Math.pow(xy, distributionIndex + 1.0));
-            deltaq = 1.0 - Math.pow(val, mutPow);
-          }
-          y = y + deltaq * (yu - yl);
-          y = solutionRepair.repairSolutionVariableValue(y, yl, yu);
-        }
-        x[i] = y ;
-      }
-    }
-
-    return x ;
-  }
-
   /** Perform the mutation operation */
   private void doMutation(DoubleSolution solution) {
 
