7f10370766b8231ef6f1955c6ddd89bf504caa1b
ajnebro <ajnebro@users.noreply.github.com>
2023-02-07 13:12:21 +0100

    Update documentation

--- jmetal-core/src/main/java/org/uma/jmetal/util/aggregationfunction/impl/Tschebyscheff.java
+++ jmetal-core/src/main/java/org/uma/jmetal/util/aggregationfunction/impl/Tschebyscheff.java
@@ -3,14 +3,32 @@ package org.uma.jmetal.util.aggregationfunction.impl;
 import org.uma.jmetal.util.aggregationfunction.AggregationFunction;
 import org.uma.jmetal.util.point.impl.IdealPoint;
 
-public class Tschebyscheff implements AggregationFunction {
+/**
+ * Class that implements the Tschebyscheff aggregation function for multi-objective optimization problems.
+ *
+ * @author Antonio J. Nebro
+ */
+
+ public class Tschebyscheff implements AggregationFunction {
 
   private IdealPoint idealPoint;
 
+  /**
+   * Constructor for the Tschebyscheff class, initializing the idealPoint property to null.
+   */
   public Tschebyscheff() {
     this.idealPoint = null;
   }
 
+  /**
+   * Computes the Tschebyscheff aggregation function using the formula:
+   * max(|vector[n] - idealPoint.value(n)| * weightVector[n]).
+   * If weightVector[n] is equal to 0, feval is set to 0.000001 * diff.
+   *
+   * @param vector The input vector to be aggregated.
+   * @param weightVector The weight vector to be used in the aggregation.
+   * @return The result of the Tschebyscheff aggregation function.
+   */
   @Override
   public double compute(double[] vector, double[] weightVector) {
     double maxFun = -1.0e+30;
@@ -32,7 +50,12 @@ public class Tschebyscheff implements AggregationFunction {
     return maxFun;
   }
 
-
+  /**
+   * Updates the idealPoint property with the values in the input vector.
+   * If idealPoint is null, a new IdealPoint instance is created with the length of the vector input.
+   *
+   * @param vector The input vector to be used for updating the idealPoint.
+   */
   @Override
   public void update(double[] vector) {
     if (idealPoint == null) {
