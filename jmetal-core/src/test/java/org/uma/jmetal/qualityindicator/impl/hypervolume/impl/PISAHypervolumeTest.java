43ac6e697dc4b43790a5ffc6e2fd324b4f2a7d09
ajnebro <ajnebro@users.noreply.github.com>
2023-02-15 09:23:27 +0100

    Add three problems

--- jmetal-core/src/test/java/org/uma/jmetal/qualityindicator/impl/hypervolume/impl/PISAHypervolumeTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/qualityindicator/impl/hypervolume/impl/PISAHypervolumeTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Test;
 import org.uma.jmetal.util.VectorUtils;
@@ -60,6 +61,61 @@ public class PISAHypervolumeTest {
         Assert.assertEquals(0.6661, result, 0.0001);
     }
 
+
+    @Test
+    public void chatgptCodeTest() throws IOException {
+
+      class Hypervolume {
+        private final int numberOfObjectives;
+        private final double[] referencePoint;
+
+        public Hypervolume(int numberOfObjectives, double[] referencePoint) {
+          this.numberOfObjectives = numberOfObjectives;
+          this.referencePoint = referencePoint;
+        }
+
+        public double calculate(double[][] solutions) {
+          double hypervolume = 0.0;
+
+          int numberOfSolutions = solutions.length;
+          double[][] normalizedSolutions = new double[numberOfSolutions][numberOfObjectives];
+          for (int i = 0; i < numberOfSolutions; i++) {
+            for (int j = 0; j < numberOfObjectives; j++) {
+              normalizedSolutions[i][j] = (solutions[i][j] - referencePoint[j]) / (1.0 - referencePoint[j]);
+            }
+          }
+
+          Arrays.sort(normalizedSolutions, (a, b) -> {
+            for (int i = 0; i < numberOfObjectives; i++) {
+              int comparison = Double.compare(b[i], a[i]);
+              if (comparison != 0) {
+                return comparison;
+              }
+            }
+            return 0;
+          });
+
+          for (int i = 0; i < numberOfSolutions; i++) {
+            double volume = 1.0;
+            for (int j = 0; j < numberOfObjectives; j++) {
+              volume *= normalizedSolutions[i][j];
+            }
+            hypervolume += volume;
+          }
+
+          return hypervolume;
+        }
+      }
+
+      double[][] storedFront =
+          VectorUtils.readVectors("../resources/referenceFrontsCSV/ZDT1.csv", ",");
+
+      var hv = new Hypervolume(2, new double[]{1.1, 1.1}) ;
+      double result = hv.calculate(storedFront) ;
+
+      Assert.assertEquals(0.6661, result, 0.0001);
+    }
+
     /**
      * CASE 3: The hypervolume of a front composed of a point equals to the reference point is zero
      *
@@ -67,7 +123,7 @@ public class PISAHypervolumeTest {
      */
     @Test
     public void shouldEvaluateWorkProperlyCase3()  {
-        double[][] referenceFront = new double[][] {{1.0, 0.1}, {0.0, 1.0}};
+        double[][] referenceFront = {{1.0, 0.1}, {0.0, 1.0}};
 
         double[][] front = new double[][] {{1.0, 0.0},{0.0, 1.0}} ;
 
