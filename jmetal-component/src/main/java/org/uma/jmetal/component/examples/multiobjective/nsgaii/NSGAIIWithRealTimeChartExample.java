27c345a969b35de0b687b0d444281af3fad041f2
ajnebro <ajnebro@users.noreply.github.com>
2023-05-10 09:38:46 +0200

    Minor changes

--- jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/nsgaii/NSGAIIWithRealTimeChartExample.java
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/nsgaii/NSGAIIWithRealTimeChartExample.java
@@ -32,7 +32,7 @@ import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 public class NSGAIIWithRealTimeChartExample {
 
   public static void main(String[] args) throws JMetalException, IOException {
-    String problemName = "org.uma.jmetal.problem.multiobjective.UF.UF9";
+    String problemName = "org.uma.jmetal.problem.multiobjective.uf.UF9";
     String referenceParetoFront = "resources/referenceFrontsCSV/UF9.csv";
 
     Problem<DoubleSolution> problem = ProblemFactory.<DoubleSolution>loadProblem(problemName);
