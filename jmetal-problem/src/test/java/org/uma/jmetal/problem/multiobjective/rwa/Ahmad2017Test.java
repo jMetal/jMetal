293b51ec1d1adb669de8556c26df8a7613d3ee53
ajnebro <ajnebro@users.noreply.github.com>
2023-07-03 17:40:39 +0200

    Refactor class Ahmad2017Test

--- jmetal-problem/src/test/java/org/uma/jmetal/problem/multiobjective/rwa/Ahmad2017Test.java
+++ jmetal-problem/src/test/java/org/uma/jmetal/problem/multiobjective/rwa/Ahmad2017Test.java
@@ -2,26 +2,75 @@ package org.uma.jmetal.problem.multiobjective.rwa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
 
+@DisplayName("Class Ahmad2017")
 class Ahmad2017Test {
-  @Test
-  void constructorWorksProperly() {
-    var problem = new Ahmad2017() ;
+  private DoubleProblem problem ;
 
-    assertEquals(3, problem.numberOfVariables()) ;
-    assertEquals(7, problem.numberOfObjectives()) ;
-    assertEquals(0, problem.numberOfConstraints()) ;
-
-    assertEquals(10.0, problem.variableBounds().get(0).getLowerBound()) ;
-    assertEquals(10.0, problem.variableBounds().get(1).getLowerBound()) ;
-    assertEquals(150.0, problem.variableBounds().get(2).getLowerBound()) ;
-
-    assertEquals(50.0, problem.variableBounds().get(0).getUpperBound()) ;
-    assertEquals(50.0, problem.variableBounds().get(1).getUpperBound()) ;
-    assertEquals(170.0, problem.variableBounds().get(2).getUpperBound()) ;
-
-    assertEquals("Ahmad2017", problem.name()) ;
+  @BeforeEach
+  void setup() {
+    problem = new Ahmad2017() ;
   }
 
+  @DisplayName("Its main properties are:")
+  @Nested
+  class MainProperties {
+
+    @Test
+    @DisplayName("Variables: 3")
+    void theNumberOfVariablesIsCorrect() {
+      assertEquals(3, problem.numberOfVariables());
+    }
+
+    @Test
+    @DisplayName("Objectives: 7")
+    void theNumberOfObjectivesIsCorrect() {
+      assertEquals(7, problem.numberOfObjectives());
+    }
+
+    @Test
+    @DisplayName("Constraints: 0")
+    void theNumberOfConstraintsIsCorrect() {
+      assertEquals(0, problem.numberOfConstraints());
+    }
+
+    @Test
+    @DisplayName("Name: Ahmad2017")
+    void theNameIsCorrect() {
+      assertEquals("Ahmad2017", problem.name()) ;
+    }
+  }
+
+  @Nested
+  @DisplayName("Its bounds are:")
+  class Bounds {
+    @ParameterizedTest
+    @DisplayName("Lower bounds: ")
+    @CsvSource({
+        "0, 10.0",
+        "1, 10.0",
+        "2, 150.0"
+    })
+    void checkLowerBounds(int boundIndex, double lowerBound) {
+      assertEquals(lowerBound, problem.variableBounds().get(boundIndex).getLowerBound());
+    }
+    @ParameterizedTest
+    @DisplayName("Upper bounds: ")
+    @CsvSource({
+        "0, 50.0",
+        "1, 50.0",
+        "2, 170.0"
+    })
+    void checkUpperBounds(int boundIndex, double upperBound) {
+      assertEquals(upperBound, problem.variableBounds().get(boundIndex).getUpperBound());
+    }
+  }
 }
+
