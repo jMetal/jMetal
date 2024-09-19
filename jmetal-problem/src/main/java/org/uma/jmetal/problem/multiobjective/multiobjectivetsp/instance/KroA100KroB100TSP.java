c643b846da42dcca7a5d59a417bce09ae16e1395
ajnebro <ajnebro@users.noreply.github.com>
2024-09-19 15:01:55 +0200

    Add class KroA100KroB100TSP problem

---
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/multiobjectivetsp/instance/KroA100KroB100TSP.java
@@ -0,0 +1,16 @@
+package org.uma.jmetal.problem.multiobjective.multiobjectivetsp.instance;
+
+import org.uma.jmetal.problem.multiobjective.multiobjectivetsp.MultiObjectiveTSP;
+
+import java.io.IOException;
+
+public class KroA100KroB100TSP extends MultiObjectiveTSP {
+
+  /**
+   * Creates a new MultiobjectiveTSP problem instance
+   *
+   */
+  public KroA100KroB100TSP() throws IOException {
+    super("resources/tspInstances/kroA100.tsp", "resources/tspInstances/kroB100.tsp");
+  }
+}
