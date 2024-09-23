59441c6f62a3b9cef7d6fe695cbb0d05acd9263a
ajnebro <ajnebro@users.noreply.github.com>
2024-09-23 12:14:30 +0200

    Add example GSMSEMOAExample

---
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/smsemoa/GSMSEMOAExample.java
@@ -0,0 +1,81 @@
+package org.uma.jmetal.component.examples.multiobjective.smsemoa;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import org.uma.jmetal.component.algorithm.multiobjective.SMSEMOABuilder;
+import org.uma.jmetal.component.catalogue.common.termination.Termination;
+import org.uma.jmetal.component.catalogue.common.termination.impl.TerminationByEvaluations;
+import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.ProblemFactory;
+import org.uma.jmetal.qualityindicator.QualityIndicatorUtils;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.SolutionListUtils;
+import org.uma.jmetal.util.VectorUtils;
+import org.uma.jmetal.util.comparator.dominanceComparator.impl.GDominanceComparator;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+import org.uma.jmetal.util.ranking.Ranking;
+import org.uma.jmetal.util.ranking.impl.FastNonDominatedSortRanking;
+
+/**
+ * Class to configure and run the SMSEMOA algorithm using g-dominance
+ *
+ * @author Antonio J. Nebro
+ */
+public class GSMSEMOAExample extends AbstractAlgorithmRunner {
+  public static void main(String[] args) throws JMetalException, IOException {
+    String problemName = "org.uma.jmetal.problem.multiobjective.zdt.ZDT4";
+    String referenceParetoFront = "resources/referenceFrontsCSV/ZDT4.csv";
+
+    Problem<DoubleSolution> problem = ProblemFactory.<DoubleSolution>loadProblem(problemName);
+
+    double crossoverProbability = 0.9;
+    double crossoverDistributionIndex = 20.0;
+    var crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+
+    double mutationProbability = 1.0 / problem.numberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    var mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+
+    int populationSize = 100;
+
+    Termination termination = new TerminationByEvaluations(25000);
+
+    List<Double> referencePoint = Arrays.asList(0.1, 0.5);
+    Comparator<DoubleSolution> dominanceComparator = new GDominanceComparator<>(referencePoint);
+    Ranking<DoubleSolution> ranking = new FastNonDominatedSortRanking<>(dominanceComparator);
+
+    var algorithm =
+        new SMSEMOABuilder<>(problem, populationSize, crossover, mutation)
+            .setTermination(termination)
+            .setRanking(ranking)
+            .build();
+
+    algorithm.run();
+
+    List<DoubleSolution> population = algorithm.result();
+    JMetalLogger.logger.info("Total execution time : " + algorithm.totalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + algorithm.numberOfEvaluations());
+
+    new SolutionListOutput(population)
+        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+        .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+
+    QualityIndicatorUtils.printQualityIndicators(
+        SolutionListUtils.getMatrixWithObjectiveValues(population),
+        VectorUtils.readVectors(referenceParetoFront, ","));
+  }
+}
