dcfddfea859d24581e9f052be34cd2a3c1ed36c4
ajnebro <ajnebro@users.noreply.github.com>
2024-10-16 16:43:34 +0200

    Refactor TSP problems

--- jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/multiobjectivetsp/instance/EuclidAB300.java
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/multiobjectivetsp/instance/EuclidAB300.java
@@ -1,20 +1,20 @@
 package org.uma.jmetal.problem.multiobjective.multiobjectivetsp.instance;
 
 import java.io.IOException;
-import org.uma.jmetal.problem.multiobjective.multiobjectivetsp.MultiObjectiveTSP;
+import org.uma.jmetal.problem.multiobjective.multiobjectivetsp.BiObjectiveTSP;
 
-public class EuclidA300EuclidB300 extends MultiObjectiveTSP {
+public class EuclidAB300 extends BiObjectiveTSP {
 
   /**
    * Creates a new MultiobjectiveTSP problem instance
    *
    */
-  public EuclidA300EuclidB300() throws IOException {
+  public EuclidAB300() throws IOException {
     super("resources/tspInstances/euclidA300.tsp", "resources/tspInstances/euclidB300.tsp");
   }
 
   @Override
   public String name() {
-    return "EuclidA300EuclidB300" ;
+    return "EuclidAB300" ;
   }
 }
