9747f9f40d007ef0996b0394298b51915fefebcf
ajnebro <ajnebro@users.noreply.github.com>
2024-11-14 10:35:07 +0100

    Add new irace runner

---
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/irace/AutoMOEADIraceNHV.java
@@ -0,0 +1,37 @@
+package org.uma.jmetal.auto.irace;
+
+import static org.uma.jmetal.util.SolutionListUtils.getMatrixWithObjectiveValues;
+
+import java.io.IOException;
+import org.uma.jmetal.auto.autoconfigurablealgorithm.AutoMOEAD;
+import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.impl.PISAHypervolume;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.NormalizeUtils;
+import org.uma.jmetal.util.VectorUtils;
+
+public class AutoMOEADIraceHV {
+  public static void main(String[] args) throws IOException {
+    AutoMOEAD autoMOEAD = new AutoMOEAD();
+    autoMOEAD.parse(args);
+
+    EvolutionaryAlgorithm<DoubleSolution> mopso = autoMOEAD.create();
+    mopso.run();
+
+    String referenceFrontFile = "resources/referenceFrontsCSV/"
+        + autoMOEAD.referenceFrontFilenameParameter.value();
+
+    double[][] referenceFront = VectorUtils.readVectors(referenceFrontFile, ",");
+    double[][] front = getMatrixWithObjectiveValues(mopso.result()) ;
+
+    double[][] normalizedReferenceFront = NormalizeUtils.normalize(referenceFront);
+    double[][] normalizedFront =
+        NormalizeUtils.normalize(
+            front,
+            NormalizeUtils.getMinValuesOfTheColumnsOfAMatrix(referenceFront),
+            NormalizeUtils.getMaxValuesOfTheColumnsOfAMatrix(referenceFront));
+
+    var qualityIndicator = new PISAHypervolume(normalizedReferenceFront) ;
+    System.out.println(qualityIndicator.compute(normalizedFront) * -1.0) ;
+  }
+}
