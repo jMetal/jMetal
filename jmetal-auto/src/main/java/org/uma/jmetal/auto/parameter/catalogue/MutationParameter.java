c967d44604466897f101fce2d91d4ff548309aec
ajnebro <ajnebro@users.noreply.github.com>
2024-11-15 12:37:42 +0100

    Update classes CrossoverParameter and MutationParameter

--- jmetal-auto/src/main/java/org/uma/jmetal/auto/parameter/catalogue/MutationParameter.java
+++ jmetal-auto/src/main/java/org/uma/jmetal/auto/parameter/catalogue/MutationParameter.java
@@ -18,8 +18,9 @@ public class MutationParameter extends CategoricalParameter {
   public MutationOperator<DoubleSolution> getDoubleSolutionParameter() {
     MutationOperator<DoubleSolution> result;
     int numberOfProblemVariables = (int) getNonConfigurableParameter("numberOfProblemVariables");
-    double mutationProbability = (double) findGlobalParameter(
-        "mutationProbabilityFactor").value() / numberOfProblemVariables;
+    double mutationProbability =
+        (double) findGlobalParameter("mutationProbabilityFactor").value()
+            / numberOfProblemVariables;
     RepairDoubleSolutionStrategyParameter repairDoubleSolution =
         (RepairDoubleSolutionStrategyParameter) findGlobalParameter("mutationRepairStrategy");
 
@@ -39,17 +40,19 @@ public class MutationParameter extends CategoricalParameter {
                 mutationProbability, distributionIndex, repairDoubleSolution.getParameter());
         break;
       case "uniform":
-        Double perturbation = (Double) findSpecificParameter(
-            "uniformMutationPerturbation").value();
+        Double perturbation = (Double) findSpecificParameter("uniformMutationPerturbation").value();
         result =
-            new UniformMutation(mutationProbability, perturbation,
-                repairDoubleSolution.getParameter());
+            new UniformMutation(
+                mutationProbability, perturbation, repairDoubleSolution.getParameter());
         break;
       case "nonUniform":
         perturbation = (Double) findSpecificParameter("nonUniformMutationPerturbation").value();
         int maxIterations = (Integer) getNonConfigurableParameter("maxIterations");
         result =
-            new NonUniformMutation(mutationProbability, perturbation, maxIterations,
+            new NonUniformMutation(
+                mutationProbability,
+                perturbation,
+                maxIterations,
                 repairDoubleSolution.getParameter());
         break;
       default:
@@ -61,8 +64,8 @@ public class MutationParameter extends CategoricalParameter {
   public MutationOperator<BinarySolution> getBinarySolutionParameter() {
     MutationOperator<BinarySolution> result;
     int numberOfBitsInASolution = (int) getNonConfigurableParameter("numberOfBitsInASolution");
-    double mutationProbability = (double) findGlobalParameter(
-        "mutationProbabilityFactor").value() / numberOfBitsInASolution;
+    double mutationProbability =
+        (double) findGlobalParameter("mutationProbabilityFactor").value() / numberOfBitsInASolution;
 
     if ("bitFlip".equals(value())) {
       result = new BitFlipMutation<>(mutationProbability);
@@ -76,16 +79,30 @@ public class MutationParameter extends CategoricalParameter {
     MutationOperator<PermutationSolution<Integer>> result;
 
     int permutationLength = (int) getNonConfigurableParameter("permutationLength");
-    double mutationProbability = (double) findGlobalParameter(
-            "mutationProbabilityFactor").value() / permutationLength;
+    double mutationProbability =
+        (double) findGlobalParameter("mutationProbabilityFactor").value() / permutationLength;
 
-    if ("swap".equals(value())) {
-      result = new PermutationSwapMutation<>(mutationProbability) ;
-    } else {
-      throw new JMetalException("Mutation operator does not exist: " + name());
+    switch (value()) {
+      case "swap":
+        result = new PermutationSwapMutation<>(mutationProbability);
+        break;
+      case "displacement":
+        result = new DisplacementMutation<>(mutationProbability);
+        break;
+      case "insert":
+        result = new InsertMutation<>(mutationProbability);
+        break;
+      case "scramble":
+        result = new ScrambleMutation<>(mutationProbability);
+        break;
+      case "inversion":
+        result = new InversionMutation<>(mutationProbability);
+        break;
+      default:
+        throw new JMetalException("Mutation operator does not exist: " + name());
     }
 
-    return result ;
+    return result;
   }
 
   @Override
