1893b4f64793834e48ae192f03d7f0b7c67dee05
ajnebro <ajnebro@users.noreply.github.com>
2024-11-22 11:29:35 +0100

    Refactor NPointCrosover and TwoPointCrossover

--- jmetal-core/src/main/java/org/uma/jmetal/operator/crossover/impl/NPointCrossover.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/crossover/impl/NPointCrossover.java
@@ -10,20 +10,20 @@ import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
 /** Created by FlapKap on 23-03-2017. */
 @SuppressWarnings("serial")
-public class NPointCrossover<T> implements CrossoverOperator<Solution<T>> {
+public class NPointCrossover<T extends Solution<S>, S extends Number> implements CrossoverOperator<T> {
   private final JMetalRandom randomNumberGenerator = JMetalRandom.getInstance();
   private final double probability;
-  private final int crossovers;
+  private final int numberOfCuttingPoints;
 
-  public NPointCrossover(double probability, int crossovers) {
+  public NPointCrossover(double probability, int numberOfCuttingPoints) {
     Check.probabilityIsValid(probability);
-    Check.that(crossovers >=  1, "Number of crossovers is less than one");
+    Check.that(numberOfCuttingPoints >=  1, "Number of crossovers is less than one");
     this.probability = probability;
-    this.crossovers = crossovers;
+    this.numberOfCuttingPoints = numberOfCuttingPoints;
   }
 
   public NPointCrossover(int crossovers) {
-    this.crossovers = crossovers;
+    this.numberOfCuttingPoints = crossovers;
     this.probability = 1.0;
   }
 
@@ -33,38 +33,38 @@ public class NPointCrossover<T> implements CrossoverOperator<Solution<T>> {
   }
 
   @Override
-  public List<Solution<T>> execute(List<Solution<T>> s) {
+  public List<T> execute(List<T> solutions) {
     Check.that(
-        numberOfRequiredParents() == s.size(),
+        numberOfRequiredParents() == solutions.size(),
         "Point Crossover requires + "
             + numberOfRequiredParents()
             + " parents, but got "
-            + s.size());
+            + solutions.size());
 
     if (randomNumberGenerator.nextDouble() < probability) {
-      return doCrossover(s);
+      return doCrossover(solutions);
     } else {
-      return s;
+      return solutions;
     }
   }
 
-  private List<Solution<T>> doCrossover(List<Solution<T>> s) {
-    Solution<T> mom = s.get(0);
-    Solution<T> dad = s.get(1);
+  private List<T> doCrossover(List<T> s) {
+    T mom = s.get(0);
+    T dad = s.get(1);
 
     Check.that(
         mom.variables().size() == dad.variables().size(),
         "The 2 parents doesn't have the same number of variables");
     Check.that(
-        mom.variables().size() >= crossovers,
+        mom.variables().size() >= numberOfCuttingPoints,
         "The number of crossovers is higher than the number of variables");
 
-    int[] crossoverPoints = new int[crossovers];
+    int[] crossoverPoints = new int[numberOfCuttingPoints];
     for (int i = 0; i < crossoverPoints.length; i++) {
       crossoverPoints[i] = randomNumberGenerator.nextInt(0, mom.variables().size() - 1);
     }
-    Solution<T> girl = mom.copy();
-    Solution<T> boy = dad.copy();
+    T girl = (T) mom.copy();
+    T boy = (T) dad.copy();
     boolean swap = false;
 
     for (int i = 0; i < mom.variables().size(); i++) {
@@ -77,7 +77,7 @@ public class NPointCrossover<T> implements CrossoverOperator<Solution<T>> {
         swap = !swap;
       }
     }
-    List<Solution<T>> result = new ArrayList<>();
+    List<T> result = new ArrayList<>();
     result.add(girl);
     result.add(boy);
     return result;
