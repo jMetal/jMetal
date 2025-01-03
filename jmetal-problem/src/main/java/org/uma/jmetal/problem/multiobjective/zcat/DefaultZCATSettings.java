ca608b95e4641d24e0246eb17270fac6094fb7ba
ajnebro <ajnebro@users.noreply.github.com>
2025-01-03 17:22:43 +0100

    Add configuration class for ZCAT problems

---
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/zcat/DefaultZCATSettings.java
@@ -0,0 +1,10 @@
+package org.uma.jmetal.problem.multiobjective.zcat;
+
+public class DefaultZCATSettings {
+    public static int numberOfObjectives = 2 ;
+    public static int numberOfVariables = 30 ;
+    public static boolean complicatedParetoSet = false ;
+    public static int level = 1 ;
+    public static boolean bias = false ;
+    public static boolean imbalance = false ;
+}
