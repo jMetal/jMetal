6b860c12f90eecf29ca672c820abc7843b675395
ajnebro <ajnebro@users.noreply.github.com>
2026-01-26 12:47:14 +0100

    Fix: Actualizar frentes de referencia RWA con datos correctos
    Regenerar archivos de frentes de referencia usando los POF originales:
    - RWA1 (2 obj) -> Subasi2016
    - RWA2 (3 obj) -> Liao2008
    - RWA3 (3 obj) -> Ganesan2013
    - RWA4 (3 obj) -> Padhi2016
    - RWA5 (3 obj) -> Gao2020
    - RWA6 (3 obj) -> Xu2020
    - RWA7 (3 obj) -> Goel2007
    - RWA8 (4 obj) -> Vaidyanathan2004
    - RWA9 (5 obj) -> Chen2015
    - RWA10 (7 obj) -> Ahmad2017

---
+++ jmetal-component/src/main/java/org/uma/jmetal/component/examples/multiobjective/nsgaii/NSGAIIWithCosineSimilarityArchiveExample.java
@@ -0,0 +1,83 @@
+package org.uma.jmetal.component.examples.multiobjective.nsgaii;
+
+import java.io.IOException;
+import java.util.List;
+import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
+import org.uma.jmetal.component.algorithm.multiobjective.NSGAIIBuilder;
+import org.uma.jmetal.component.catalogue.common.evaluation.impl.SequentialEvaluationWithArchive;
+import org.uma.jmetal.component.catalogue.common.termination.Termination;
+import org.uma.jmetal.component.catalogue.common.termination.impl.TerminationByEvaluations;
+import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.ProblemFactory;
+import org.uma.jmetal.qualityindicator.QualityIndicatorUtils;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.SolutionListUtils;
+import org.uma.jmetal.util.VectorUtils;
+import org.uma.jmetal.util.archive.Archive;
+import org.uma.jmetal.util.archive.impl.CrowdingDistanceArchive;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+/**
+ * Class to configure and run the NSGA-II algorithm configured a bounded external archive that uses
+ * the crowding distance to remove solutions when the archive gets full.
+ *
+ * @author Antonio J. Nebro
+ */
+public class NSGAIIWithCosineDistanceArchiveExample {
+  public static void main(String[] args) throws JMetalException, IOException {
+    String problemName = "org.uma.jmetal.problem.multiobjective.zdt.ZDT1";
+    String referenceParetoFront = "resources/referenceFrontsCSV/ZDT1.csv";
+
+    Problem<DoubleSolution> problem = ProblemFactory.<DoubleSolution>loadProblem(problemName);
+
+    double crossoverProbability = 0.9;
+    double crossoverDistributionIndex = 20.0;
+    var crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+
+    double mutationProbability = 1.0 / problem.numberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    var mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+
+    int populationSize = 100;
+    int offspringPopulationSize = populationSize;
+
+    Termination termination = new TerminationByEvaluations(25000);
+
+    Archive<DoubleSolution> externalArchive = new CrowdingDistanceArchive<>(populationSize) ;
+
+    EvolutionaryAlgorithm<DoubleSolution> nsgaii = new NSGAIIBuilder<>(
+        problem,
+        populationSize,
+        offspringPopulationSize,
+        crossover,
+        mutation)
+        .setTermination(termination)
+        .setEvaluation(new SequentialEvaluationWithArchive<>(problem, externalArchive))
+        .build();
+
+    nsgaii.run();
+
+    List<DoubleSolution> population = externalArchive.solutions();
+    JMetalLogger.logger.info("Total execution time : " + nsgaii.totalComputingTime() + "ms");
+    JMetalLogger.logger.info("Number of evaluations: " + nsgaii.numberOfEvaluations());
+
+    new SolutionListOutput(population)
+        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+        .print();
+
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+
+    QualityIndicatorUtils.printQualityIndicators(
+        SolutionListUtils.getMatrixWithObjectiveValues(population),
+        VectorUtils.readVectors(referenceParetoFront, ","));
+  }
+}
