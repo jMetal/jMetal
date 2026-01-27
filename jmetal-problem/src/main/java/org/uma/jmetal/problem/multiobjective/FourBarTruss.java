f299e6a286863b35d1e4ca36754bdf2d89c18f16
ajnebro <ajnebro@users.noreply.github.com>
2026-01-27 09:06:23 +0100

    Update pom.xml

--- jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/FourBarTruss.java
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/multiobjective/FourBarTruss.java
@@ -1,68 +1,52 @@
 package org.uma.jmetal.problem.multiobjective;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.uma.jmetal.problem.doubleproblem.impl.AbstractDoubleProblem;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
 
 /**
  * Class representing problem FourBarTruss
- *
- * 	Measures:
- * 	f = 10kN
- *  e = 200000 kN/cm2
- *  l = 200 cm
- *  sigma = 10kN/cm2
  */
 @SuppressWarnings("serial")
 public class FourBarTruss extends AbstractDoubleProblem {
-  private double f = 10   ;
-  private double e = 200000;
-  private double l = 200  ;
-  private double sigma = 10 ;
-	
-  /**
-   * Constructor
-   * Creates a default instance of the FourBarTruss problem
-   */
+  private static final double SQRT_2 = Math.sqrt(2.0);
+
+  private final double appliedLoad = 10;
+  private final double elasticityModulus = 200000;
+  private final double length = 200;
+  private final double maximumStress = 10;
+
   public FourBarTruss() {
-    int numberOfVariables = 4 ;
     numberOfObjectives(2);
-    numberOfConstraints(0);
     name("FourBarTruss");
 
-    List<Double> lowerLimit = new ArrayList<>(numberOfVariables) ;
-    List<Double> upperLimit = new ArrayList<>(numberOfVariables) ;
+    double lowerBase = appliedLoad / maximumStress;
+    double upperBase = 3.0 * lowerBase;
 
-    lowerLimit.add(f / sigma) ;
-    lowerLimit.add(Math.sqrt(2.0)*(f / sigma));
-    lowerLimit.add(lowerLimit.get(1)) ;
-    lowerLimit.add(lowerLimit.get(0)) ;
+    List<Double> lowerLimit = Arrays.asList(
+        lowerBase,
+        lowerBase * SQRT_2,
+        lowerBase * SQRT_2,
+        lowerBase);
 
-    upperLimit.add(3*(f / sigma)) ;
-    upperLimit.add(upperLimit.get(0)) ;
-    upperLimit.add(upperLimit.get(0)) ;
-    upperLimit.add(upperLimit.get(0)) ;
+    List<Double> upperLimit = Arrays.asList(
+        upperBase, upperBase, upperBase, upperBase);
 
     variableBounds(lowerLimit, upperLimit);
   }
 
-  /**
-   * Evaluate() method
-   */
   @Override
   public DoubleSolution evaluate(DoubleSolution solution) {
-    double [] fx = new double[2] ;
-    double [] x = new double[numberOfVariables()] ;
-    for (int i = 0 ; i < numberOfVariables(); i++)
-    	x[i] = solution.variables().get(i) ;
+    List<Double> x = solution.variables();
 
-    fx[0] = l * (2 * x[0] + Math.sqrt(2.0) * x[1] + Math.sqrt(2.0) * x[2] + x[3]);
-    fx[1] = (f * l / e) * (2.0 / x[0] + 2.0 * Math.sqrt(2.0) / x[1] - 2.0 * Math.sqrt(2.0) / x[2] + 2.0 / x[3]);
+    double volume = length * (2 * x.get(0) + SQRT_2 * x.get(1) + SQRT_2 * x.get(2) + x.get(3));
+    double displacement = (appliedLoad * length / elasticityModulus) *
+        (2.0 / x.get(0) + 2.0 * SQRT_2 / x.get(1) - 2.0 * SQRT_2 / x.get(2) + 2.0 / x.get(3));
 
-    solution.objectives()[0] = fx[0];
-    solution.objectives()[1] = fx[1];
+    solution.objectives()[0] = volume;
+    solution.objectives()[1] = displacement;
 
-    return solution ;
+    return solution;
   }
 }
