0a24602ac88c77bd99ec3dfc926ce9ec1fe7f97e
ajnebro <ajnebro@users.noreply.github.com>
2026-03-17 11:01:40 +0100

    Add runner for AGE-MOEA

---
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/examples/multiobjective/agemoea/AGEMOEABiObjectiveTSPRunner.java
@@ -0,0 +1,68 @@
+package org.uma.jmetal.algorithm.examples.multiobjective.agemoea;
+
+import java.io.IOException;
+import java.util.List;
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.examples.AlgorithmRunner;
+import org.uma.jmetal.algorithm.multiobjective.agemoea.AGEMOEABuilder;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
+import org.uma.jmetal.operator.crossover.impl.PMXCrossover;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.operator.mutation.impl.PermutationSwapMutation;
+import org.uma.jmetal.problem.multiobjective.multiobjectivetsp.BiObjectiveTSP;
+import org.uma.jmetal.problem.permutationproblem.PermutationProblem;
+import org.uma.jmetal.solution.permutationsolution.PermutationSolution;
+import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.errorchecking.JMetalException;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+/**
+ * Class for configuring and running the AGE-MOEA algorithm to solve the bi-objective TSP
+ *
+ * @author Antonio J. Nebro
+ */
+public class AGEMOEABiObjectiveTSPRunner extends AbstractAlgorithmRunner {
+
+  /**
+   * @param args Command line arguments.
+   */
+  public static void main(String[] args) throws JMetalException, IOException {
+
+    PermutationProblem<PermutationSolution<Integer>> problem = new BiObjectiveTSP(
+        "resources/tspInstances/kroA100.tsp", "resources/tspInstances/kroB100.tsp");
+
+    CrossoverOperator<PermutationSolution<Integer>> crossover = new PMXCrossover(0.9);
+
+    double mutationProbability = 0.2;
+    MutationOperator<PermutationSolution<Integer>> mutation = new PermutationSwapMutation<>(
+        mutationProbability);
+
+    int populationSize = 100;
+    
+    Algorithm<List<PermutationSolution<Integer>>> algorithm =
+        new AGEMOEABuilder<>(problem)
+            .setCrossoverOperator(crossover)
+            .setMutationOperator(mutation)
+            .setMaxIterations(100)
+            .setPopulationSize(populationSize)
+            .build();
+
+    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm).execute();
+
+    List<PermutationSolution<Integer>> population = algorithm.result();
+    long computingTime = algorithmRunner.getComputingTime();
+
+    new SolutionListOutput(population)
+        .setVarFileOutputContext(new DefaultFileOutputContext("VAR.csv", ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext("FUN.csv", ","))
+        .print();
+
+    JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+    JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.csv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.csv");
+  }
+}
